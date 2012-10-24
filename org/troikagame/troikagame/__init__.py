7bf59143c11c96687fdce07dbe3a021facda03fe
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2012-10-24 14:31:54 +0300

    Added licence texts to source files

--- org/troikagame/troikagame/__init__.py
+++ org/troikagame/troikagame/__init__.py
@@ -1,3 +1,21 @@
+#  Copyright © 2012 Olli Parviainen, Petro Poutanen, Salla Laaksonen and 
+#  Filosofian Akatemia. [http://troikagame.org]
+#
+#  This file is part of Troika Game.
+#
+#  Troika Game is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program; if not, see http://www.gnu.org/licenses
+#
 from flask import Flask
 from troikagame import session
 import os
