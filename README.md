703dddbc90a3160ea18521bf59fc1549eb3a6503
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2013-05-05 11:37:23 +0300

    Set html5mode to true. Requires nginx.conf rewrite to work.

---
+++ README.md
@@ -0,0 +1,17 @@
+Extended Mind
+=============
+
+This is the root of the Extended Mind code repository.
+
+Nginx configuration
+-------------------
+
+AngularJS is set to use 'html5mode' so you have to configure a rewrite such as this to your 'nginx.conf':
+
+	location / {
+	    root  [extendedmind_home]/frontend/core/src/main/webapp;
+        index  index_devel.html;
+        rewrite ^/(?!static) /index_devel.html break;
+    }
+
+After starting Nginx, the development version of extended mind should be running on 'localhost'.
--- frontend/core/src/main/webapp/static/js/zapp.js
+++ frontend/core/src/main/webapp/static/js/zapp.js
@@ -2,91 +2,85 @@
 (function() {
   "use strict";
 
-  angular.module('em', ['em.filters', 'em.services', 'em.directives']).config([
-    '$routeProvider', function($routeProvider) {
-      /* Use this at some point: http://docs.angularjs.org/guide/dev_guide.services.$location
-        History API support required: http://caniuse.com/#search=history
-
-        ['$locationProvider', ($locationProvider) ->
-        $locationProvider.html5Mode(true)],
-      */
+  angular.module('em', ['em.filters', 'em.services', 'em.directives']).config(
+    function($routeProvider, $locationProvider) {
+      $locationProvider.html5Mode(true);
       $routeProvider.when('/', {
-        templateUrl: 'static/partials/home.html',
+        templateUrl: '/static/partials/home.html',
         controller: ContentCtrl
       });
       $routeProvider.when('/about', {
-        templateUrl: 'static/partials/about.html',
+        templateUrl: '/static/partials/about.html',
         controller: AboutCtrl
       });
       $routeProvider.when('/people', {
-        templateUrl: 'static/partials/people.html',
+        templateUrl: '/static/partials/people.html',
         controller: PeopleCtrl
       });
       $routeProvider.when('/people/:personId', {
-        templateUrl: 'static/partials/person.html',
+        templateUrl: '/static/partials/person.html',
         controller: PersonCtrl
       });
       $routeProvider.when('/categories', {
-        templateUrl: 'static/partials/categories.html',
+        templateUrl: '/static/partials/categories.html',
         controller: CategoriesCtrl
       });
       $routeProvider.when('/search', {
-        templateUrl: 'static/partials/search.html',
+        templateUrl: '/static/partials/search.html',
         controller: SearchCtrl
       });
       $routeProvider.when('/search/:searchString', {
-        templateUrl: 'static/partials/search.html',
+        templateUrl: '/static/partials/search.html',
         controller: SearchCtrl
       });
       $routeProvider.when('/login', {
-        templateUrl: 'static/partials/login.html',
+        templateUrl: '/static/partials/login.html',
         controller: LoginCtrl
       });
       $routeProvider.when('/my', {
-        templateUrl: 'static/partials/my.html',
+        templateUrl: '/static/partials/my.html',
         controller: MyCtrl
       });
       $routeProvider.when('/my/notes', {
-        templateUrl: 'static/partials/notes.html',
+        templateUrl: '/static/partials/notes.html',
         controller: NotesCtrl
       });
       $routeProvider.when('/my/tasks', {
-        templateUrl: 'static/partials/tasks.html',
+        templateUrl: '/static/partials/tasks.html',
         controller: TasksCtrl
       });
       $routeProvider.when('/my/email', {
-        templateUrl: 'static/partials/email.html',
+        templateUrl: '/static/partials/email.html',
         controller: EmailCtrl
       });
       $routeProvider.when('/people/timo-tiuraniemi/on-philosophy-of-technology', {
-        templateUrl: 'static/partials/test/tech.html',
+        templateUrl: '/static/partials/test/tech.html',
         controller: NoteCtrl
       });
       $routeProvider.when('/people/henri-ylikotila/essential-firefox-keyboard-shortcuts', {
-        templateUrl: 'static/partials/test/firefox.html',
+        templateUrl: '/static/partials/test/firefox.html',
         controller: NoteCtrl
       });
       $routeProvider.when('/people/antti-takalahti/avocado-pasta', {
-        templateUrl: 'static/partials/test/pasta.html',
+        templateUrl: '/static/partials/test/pasta.html',
         controller: NoteCtrl
       });
       $routeProvider.when('/people/timo-tiuraniemi/notes-on-productivity', {
-        templateUrl: 'static/partials/test/productivity.html',
+        templateUrl: '/static/partials/test/productivity.html',
         controller: NoteCtrl
       });
       $routeProvider.when('/people/lauri-jarvilehto/why-its-great-to-be-a-nerd', {
-        templateUrl: 'static/partials/test/nerd.html',
+        templateUrl: '/static/partials/test/nerd.html',
         controller: NoteCtrl
       });
       $routeProvider.when('/people/timo-tiuraniemi/how-to-break-out-of-facebook', {
-        templateUrl: 'static/partials/test/nofacebook.html',
+        templateUrl: '/static/partials/test/nofacebook.html',
         controller: NoteCtrl
       });
       return $routeProvider.otherwise({
         redirectTo: '/'
       });
-    }
-  ]);
+    });
 
   angular.bootstrap(document, ['em']);
 
