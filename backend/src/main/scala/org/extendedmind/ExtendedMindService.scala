ff0856a52bd339634998985c79e0128861a5eaa9
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2013-05-21 21:37:09 +0300

    Changed Scala version to 2.9.3 and BlueEyes to the latest stable version 0.6.1-SNAPSHOT

---
+++ backend/src/main/scala/org/extendedmind/ExtendedMindService.scala
@@ -0,0 +1,56 @@
+import akka.dispatch.Future
+import blueeyes.{BlueEyesServiceBuilder, BlueEyesServer}
+import blueeyes.core.http.{HttpRequest, HttpResponse, HttpStatus}
+import blueeyes.core.http.HttpStatusCodes._
+import blueeyes.core.data.{ByteChunk, BijectionsChunkString}
+
+trait ExtendedMindService extends BlueEyesServiceBuilder with BijectionsChunkString {
+  val calculatorService = service("calculatorService", "1.0.0") {
+    context =>
+      startup {
+        Future { () }
+      } ->
+      request { config: Unit =>
+        path("/add" / 'number1 / 'number2) {
+          (request: HttpRequest[ByteChunk]) =>
+            try {
+              val number1 = request.parameters('number1).toInt
+              val number2 = request.parameters('number2).toInt
+              val sum = (number1 + number2).toString
+
+              Future {
+                HttpResponse[ByteChunk](content = Some(sum.toString))
+              }
+            } catch {
+                case e: NumberFormatException =>
+                  Future {
+                    HttpResponse[ByteChunk](status = HttpStatus(BadRequest))
+                  }
+            }
+        } ~
+        path("/multiply" / 'number1 / 'number2) {
+          (request: HttpRequest[ByteChunk]) =>
+            try {
+              val number1 = request.parameters('number1).toInt
+              val number2 = request.parameters('number2).toInt
+              val product = (number1 * number2).toString
+
+              Future {
+                HttpResponse[ByteChunk](content = Some(product.toString))
+              }
+            } catch {
+                case e: NumberFormatException =>
+                  Future {
+                    HttpResponse[ByteChunk](status = HttpStatus(BadRequest))
+                  }
+            }
+        }
+      } ->
+      shutdown { config =>
+        println("Shutting down")
+        Future { () }
+      }
+  }
+}
+
+object ExtendedMindServer extends BlueEyesServer with ExtendedMindService
