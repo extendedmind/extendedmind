2c661125cfa2b49c67c4f733ca367f2b20ad1f0c
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2013-05-24 13:24:57 +0300

    Switched to Spray.io as it is much better supported and more mature than BlueEyes

--- backend/src/main/scala/org/extendedmind/ExtendedMindServer.scala
+++ backend/src/main/scala/org/extendedmind/ExtendedMindServer.scala
@@ -1,5 +1,13 @@
-package org.extendedmind
+import akka.actor.{ActorSystem, Props}
+import akka.io.IO
+import spray.can.Http
 
-import blueeyes.BlueEyesServer
+object ExtendedMindServer extends App {
 
-object ExtendedMindServer extends BlueEyesServer with ExtendedMindService
+  implicit val system = ActorSystem()
+
+  // the handler actor replies to incoming HttpRequests
+  val handler = system.actorOf(Props[ExtendedMindService], name = "handler")
+
+  IO(Http) ! Http.Bind(handler, interface = "localhost", port = 8080)
+}
