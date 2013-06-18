7174e06cd6bfb57219c8ae3824e97cb9051bc336
JP Salo <jpsalo@zoho.com>
2013-06-18 14:12:28 +0300

    Removed mockLatest.js

--- frontend/core/src/test/webapp/static/js/mockLatest.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('mockedLatest', []).value('latestJSON', {
-    query : {
-        "id" : 101,
-        "title" : "Why it's great to be a nerd",
-        "author" : "Lauri Järvilehto"
-    }
-});
