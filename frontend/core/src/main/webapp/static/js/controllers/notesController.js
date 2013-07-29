1a62bda70d4e95bb2baaa5c079b0aa16f3d92ba8
JP Salo <jpsalo@zoho.com>
2013-07-29 12:10:47 +0300

    Checkbox for tasks.

--- frontend/core/src/main/webapp/static/js/controllers/notesController.js
+++ frontend/core/src/main/webapp/static/js/controllers/notesController.js
@@ -1,6 +1,12 @@
 "use strict";
 
-emControllers.controller('NotesController', ['$scope',
-function($scope) {
-  $scope.notes = 'notes';
+emControllers.controller('NotesController', ['$scope', 'Item', 'Items', 'User',
+function($scope, Item, Items, User) {
+  $scope.filterArgument = 'NOTE';
+
+  Item.getItems(User.getUserUUID(), function() {
+    $scope.items = Items.getUserItems();
+    $scope.newItems = Items.getUserNewItems();
+  }, function(error) {
+  });
 }]);
