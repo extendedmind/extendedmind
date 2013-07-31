00c92b31aef420288373507dd0e73ab88936d367
JP Salo <jpsalo@zoho.com>
2013-07-31 14:09:15 +0300

    Fixed cookies and sessionStorage usage in user authorization. Using JQuery cookies instead of AngularJS because it is able to set expiration date to cookie. Http backend json mock data is fetched with Jasmine getJsonFixture instead of AngularJS $resource http wrapper. Updated AngularJS sources and cleaned index html files.

--- frontend/core/src/main/webapp/static/js/userAuthenticate.js
+++ frontend/core/src/main/webapp/static/js/userAuthenticate.js
@@ -1,38 +1,92 @@
+// AngularJS does not support cookie expiration:
+// https://github.com/angular/angular.js/pull/2459.
+//
+// Using JQUery cookie instead
+// http://stackoverflow.com/questions/1458724/how-to-set-unset-cookie-with-jquery
+// http://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.3.1/jquery.cookie.min.js
+// }
+
 "use strict";
 
-var emAuthenticate = angular.module('em.userAuthenticate', ['ngCookies']);
+var emAuthenticate = angular.module('em.userAuthenticate', ['em.base64']);
 
-emAuthenticate.factory('UserAuthenticate', ['$http', 'User',function($http, User) {
+emAuthenticate.factory('UserAuthenticate', ['$http', 'Auth', 'User',function($http, Auth, User) {
+
   return {
-    userLogin : function(user, success, error) {
-      $http.post('/api/authenticate', user).success(function(authenticateResponse) {
-        User.setUser(authenticateResponse);
-        success();
-      }).error(error);
+    userLogin : function(success, error) {
+      $http({
+        method : 'POST',
+        url : '/api/authenticate'
+      }).success(success).error(error);
     },
     userLogout : function() {
-      User.setUser();
+      // Auth.clearCredentials();
+      // User.clearUser();
     }
   };
 }]);
 
-emAuthenticate.factory('User', ['$cookieStore',
-function($cookieStore) {
+emAuthenticate.factory('Resu', ['Auth', 'User', 'UserAuthenticate',
+function(Auth, User, UserAuthenticate) {
   return {
-    setUser : function(user) {
-      if (user == undefined) {
-        $cookieStore.remove('user');
-      } else
-        $cookieStore.put('user', user);
+    loglog : function() {
+      Auth.setCredentials('token', User.getUserToken());
+      UserAuthenticate.userLogin(function(authenticateResponse) {
+        User.setUser(authenticateResponse.token.toString(), true);
+        Auth.setCredentials('token', authenticateResponse.token);
+      }, function(error) {
+      });
+    }
+  };
+}]);
+
+emAuthenticate.factory('User', [
+function() {
+  return {
+    setUser : function(token, remember) {
+      if (remember) {
+        console.log('remember: ' + remember + ', token: ' + token);
+        $.cookie('token', token, {
+          expires : 7
+        });
+      }
+      sessionStorage.setItem('token', token);
     },
-    getUser : function() {
-      return $cookieStore.get('user');
+    clearUser : function() {
+      $.removeCookie('token');
+      sessionStorage.removeItem('token');
+    },
+    getUserToken : function() {
+      return $.cookie('token');
     },
     getUserUUID : function() {
-      return $cookieStore.get('user').userUUID;
+      return sessionStorage.getItem('userUUID');
+    },
+    isUserRemembered : function() {
+      return $.cookie('token') != null;
     },
     isUserAuthenticated : function() {
-      return $cookieStore.get('user') != undefined;
+      return sessionStorage.getItem('token') != null;
+    }
+  };
+}]);
+
+emAuthenticate.factory('Auth', ['$http', 'Base64',
+function($http, Base64) {
+  // initialize to whatever is in the cookie, if anything
+  $http.defaults.headers.common['Authorization'] = 'Basic ' + sessionStorage.getItem('token');
+  // $http.defaults.headers.common['Authorization'] = '';
+
+  return {
+    setCredentials : function(username, password) {
+      var encoded = Base64.encode(username + ':' + password);
+      $http.defaults.headers.common.Authorization = 'Basic ' + encoded;
+      sessionStorage.setItem('token', encoded);
+    },
+    clearCredentials : function() {
+      document.execCommand("ClearAuthenticationCache");
+      sessionStorage.removeItem('token');
+      $http.defaults.headers.common.Authorization = 'Basic ';
     }
   };
 }]);
