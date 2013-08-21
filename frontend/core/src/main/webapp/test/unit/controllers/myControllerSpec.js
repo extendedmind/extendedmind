da90acfbd58dc534501bd8e57446f56ed4764d34
JP Salo <jpsalo@zoho.com>
2013-08-21 10:53:30 +0300

    Unit tests.

--- frontend/core/src/main/webapp/test/unit/controllers/myControllerSpec.js
+++ frontend/core/src/main/webapp/test/unit/controllers/myControllerSpec.js
@@ -6,10 +6,11 @@ describe('em.controllers', function() {
   describe('MyController', function() {
     beforeEach(module('em.services', 'em.mockHelpers'));
 
-    var $controller, $scope;
+    var $controller, $httpBackend, $scope;
     var mockHttpBackendResponse;
 
-    beforeEach(inject(function(_$controller_, _$rootScope_, _mockHttpBackendResponse_) {
+    beforeEach(inject(function(_$controller_, _$httpBackend_, _$rootScope_, _mockHttpBackendResponse_) {
+      $httpBackend = _$httpBackend_;
       mockHttpBackendResponse = _mockHttpBackendResponse_;
 
       $scope = _$rootScope_.$new();
@@ -18,10 +19,38 @@ describe('em.controllers', function() {
       });
     }));
 
+    afterEach(function() {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+      mockHttpBackendResponse.clearSessionStorage();
+    });
+
     it('should return logged user\'s items', inject(function() {
       expect($scope.items).toBe(undefined);
       $scope.items = mockHttpBackendResponse.getItemsResponse();
       expect($scope.items.length).toBe(3);
+    }));
+
+    it('should add new item', inject(function(_httpBasicAuth_, _itemsFactory_, _userSessionStorage_) {
+      var httpBasicAuth = _httpBasicAuth_;
+      httpBasicAuth.setCredentials('timo@ext.md', 'timopwd');
+
+      var itemsFactory = _itemsFactory_;
+      var userSessionStorage = _userSessionStorage_;
+
+      userSessionStorage.setUserUUID(mockHttpBackendResponse.getAuthenticateResponse().userUUID);
+      $scope.newItems = itemsFactory.getUserNewItems();
+      expect($scope.newItems.length).toBe(0);
+
+      $scope.item = {
+        title : 'Buy more milk'
+      };
+
+      $httpBackend.expectPUT('/api/' + mockHttpBackendResponse.getAuthenticateResponse().userUUID + '/item');
+
+      $scope.putItem();
+      $httpBackend.flush();
+      expect($scope.newItems[0].title).toBe('Buy more milk');
     }));
   });
 });
--- frontend/core/src/main/webapp/test/unit/services/userAuthenticateServiceSpec.js
+++ frontend/core/src/main/webapp/test/unit/services/userAuthenticateServiceSpec.js
@@ -10,40 +10,15 @@ describe('em.service', function() {
       var $rootScope, userAuthenticate;
 
       beforeEach(inject(function(_$rootScope_, _userAuthenticate_) {
-
         $rootScope = _$rootScope_;
-        spyOn($rootScope, "$broadcast");
-
         userAuthenticate = _userAuthenticate_;
       }));
 
       it('should broadcast \'event:loginRequired\' on invalid user', inject(function() {
+        spyOn($rootScope, "$broadcast");
+
         userAuthenticate.authenticate();
         expect($rootScope.$broadcast).toHaveBeenCalledWith('event:loginRequired');
-      }));
-    });
-
-    describe('httpBasicAuth', function() {
-      var httpBasicAuth;
-
-      beforeEach(inject(function(_httpBasicAuth_) {
-        httpBasicAuth = _httpBasicAuth_;
-      }));
-
-      it('should set credentials', inject(function() {
-        httpBasicAuth.setCredentials('timo@ext.md', 'timopwd');
-        expect(httpBasicAuth.getCredentials()).toBeDefined();
-      }));
-
-      it('should call setter for credentials', inject(function() {
-        spyOn(httpBasicAuth, 'setCredentials');
-        httpBasicAuth.setCredentials('timo@ext.md', 'timopwd');
-        expect(httpBasicAuth.setCredentials).toHaveBeenCalledWith('timo@ext.md', 'timopwd');
-      }));
-
-      it('should get new credentials', inject(function() {
-        httpBasicAuth.setCredentials(['timo@ext.md', 'timopwd']);
-        expect(httpBasicAuth.getCredentials()).toEqual('dGltb0BleHQubWQsdGltb3B3ZDp1bmRlZmluZWQ=');
       }));
     });
   });
