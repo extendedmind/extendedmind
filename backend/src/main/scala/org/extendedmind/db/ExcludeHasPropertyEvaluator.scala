5dfe71d810aa1be1e46e9f4ba1b092d77605476b
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2013-08-26 16:32:43 +0300

    Added missing Evaluator

---
+++ backend/src/main/scala/org/extendedmind/db/ExcludeHasPropertyEvaluator.scala
@@ -0,0 +1,19 @@
+package org.extendedmind.db
+
+import org.neo4j.graphdb.traversal.Evaluator
+import org.neo4j.graphdb.Node
+import org.neo4j.graphdb.Path
+import org.neo4j.graphdb.traversal.Evaluation
+import java.util.UUID
+import org.extendedmind.security.UUIDUtils
+
+case class ExcludeHasPropertyEvaluator(label: Label, property: String) extends Evaluator{
+
+  override def evaluate(path: Path): Evaluation = {
+    val currentNode: Node = path.endNode();
+    if (currentNode.hasLabel(label) && currentNode.hasProperty(property)){
+      return Evaluation.EXCLUDE_AND_CONTINUE
+    }
+    return Evaluation.INCLUDE_AND_CONTINUE
+  }
+}
