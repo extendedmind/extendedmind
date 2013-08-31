6af8eeeb8ca33f0bb82ef7dd1622f7be9eef196a
JP Salo <jpsalo@zoho.com>
2013-08-31 10:03:41 +0300

    Added getItems http request into notes controller. Fixed note and task templates' list filter paramater.

--- frontend/core/src/main/webapp/static/js/controllers/notes.js
+++ frontend/core/src/main/webapp/static/js/controllers/notes.js
@@ -2,12 +2,18 @@
 
 ( function() {'use strict';
 
-    angular.module('em.app').controller('NotesController', ['$scope', 'errorHandler', 'notesArray',
-    function($scope, errorHandler, notesArray) {
+    angular.module('em.app').controller('NotesController', ['$scope', 'errorHandler', 'itemsRequest', 'notesArray',
+    function($scope, errorHandler, itemsRequest, notesArray) {
 
       $scope.errorHandler = errorHandler;
 
+      itemsRequest.getItems(function(itemsResponse) {
+        notesArray.setNotes(itemsResponse.notes);
+
+        $scope.notes = notesArray.getNotes();
+      }, function(error) {
+      });
+
       $scope.notesListFilter = true;
-      $scope.notes = notesArray.getNotes();
     }]);
   }());
