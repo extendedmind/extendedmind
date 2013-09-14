182877b5da9530d3baf6a259bab8fa67bdbea9a1
JP Salo <jpsalo@zoho.com>
2013-09-15 01:29:00 +0300

    Cleanup.

--- frontend/core/src/main/webapp/static/js/app.js
+++ frontend/core/src/main/webapp/static/js/app.js
@@ -107,6 +107,12 @@
 
     angular.module('em.app').run(['$document', '$location', '$rootScope', 'userAuthenticate',
     function($document, $location, $rootScope, userAuthenticate) {
+
+      $rootScope.pageAnimation = {
+        enter : 'fade-show',
+        hide : 'fade-hide'
+      };
+
       $rootScope.$on('event:authenticationRequired', function() {
         userAuthenticate.authenticate();
       });
