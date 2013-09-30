c3ac86de51ad80e04bbe4b862e429fa6ed975126
JP Salo <jpsalo@zoho.com>
2013-09-30 11:59:52 +0300

    Carousel slide data loading fixes.

--- frontend/core/src/main/webapp/static/js/controllers/my.js
+++ frontend/core/src/main/webapp/static/js/controllers/my.js
@@ -6,10 +6,20 @@
     angular.module('em.app').controller('MyController', ['$scope', 'activeItem', 'itemsArray', 'itemsRequest', 'itemsResponse', 'notesArray', 'tagsArray', 'tasksArray',
     function($scope, activeItem, itemsArray, itemsRequest, itemsResponse, notesArray, tagsArray, tasksArray) {
 
-      $scope.items = itemsArray.getItems();
-      $scope.notes = notesArray.getNotes();
-      $scope.tags = tagsArray.getTags();
-      $scope.tasks = tasksArray.getTasks();
+      itemsRequest.getItems(function(itemsResponse) {
+
+        itemsArray.setItems(itemsResponse.items);
+        notesArray.setNotes(itemsResponse.notes);
+        tagsArray.setTags(itemsResponse.tags);
+        tasksArray.setTasks(itemsResponse.tasks);
+
+        $scope.items = itemsArray.getItems();
+        $scope.notes = notesArray.getNotes();
+        $scope.tags = tagsArray.getTags();
+        $scope.tasks = tasksArray.getTasks();
+
+      }, function(error) {
+      });
 
       $scope.addNewItem = function() {
 
