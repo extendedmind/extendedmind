4e1b4b20b31cf196edcb4f2f9bb2aabe9e527f53
JP Salo <jpsalo@zoho.com>
2013-10-01 10:50:14 +0300

    Added AngularUI Bootstrap.

--- frontend/core/src/main/webapp/static/js/app.js
+++ frontend/core/src/main/webapp/static/js/app.js
@@ -2,7 +2,7 @@
 
 ( function() {'use strict';
 
-    angular.module('em.app', ['ngRoute', 'ngAnimate', 'ngTouch', 'angular-carousel', 'em.directives', 'em.filters', 'em.services']);
+    angular.module('em.app', ['ngRoute', 'ngAnimate', 'ngTouch', 'ui.bootstrap', 'angular-carousel', 'em.directives', 'em.filters', 'em.services']);
     angular.module('em.directives', []);
     angular.module('em.filters', []);
     angular.module('em.services', ['em.base64']);
