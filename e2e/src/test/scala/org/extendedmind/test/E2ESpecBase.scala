75417a1ba5f5280419de4519c760450504093329
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2013-12-02 16:43:32 +0200

    Added Phonegap project to main build. Fixed PhantomJS fetch from repo

--- e2e/src/test/scala/org/extendedmind/test/E2ESpecBase.scala
+++ e2e/src/test/scala/org/extendedmind/test/E2ESpecBase.scala
@@ -31,9 +31,11 @@ abstract class E2ESpecBase extends FunSpec
   }
 
   if (System.getProperty("os.name") == "Mac OS X") {
-    System.setProperty(PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"target/phantomjs-1.9.2/mac/bin/phantomjs")  
+    
+System.setProperty(PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"target/phantomjs-1.0-SNAPSHOT/mac/bin/phantomjs")  
   } else {
-    System.setProperty(PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"target/phantomjs-1.9.2/linux/bin/phantomjs")  
+    
+System.setProperty(PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"target/phantomjs-1.0-SNAPSHOT/linux/bin/phantomjs")  
   }
   implicit val webDriver: WebDriver = new PhantomJSDriver
 
