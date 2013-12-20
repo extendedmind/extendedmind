df0122568e212b61ddfc6c33bc78528c9022c263
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2013-12-20 21:24:19 +0200

    Reverted refresh on focus as focus event is fired 7 times per focus => too heavy and unnecessary as check is run on location change

--- frontend/core/src/main/webapp/static/js/controllers/navbar.js
+++ frontend/core/src/main/webapp/static/js/controllers/navbar.js
@@ -1,7 +1,7 @@
 /*global angular */
 'use strict';
 
-function NavbarController($location, $scope, $rootScope, $window, auth, authenticateRequest, emSwiper, Enum, userPrefix, userSessionStorage) {
+function NavbarController($location, $scope, $window, auth, authenticateRequest, emSwiper, Enum, userPrefix, userSessionStorage) {
 
   $scope.user = userSessionStorage.getUserUUID();
   $scope.collectives = userSessionStorage.getCollectives();
@@ -46,27 +46,7 @@ function NavbarController($location, $scope, $rootScope, $window, auth, authenti
     emSwiper.setSlideIndex(Enum.PROJECTS, index);
   };
 
-  $rootScope.lastCheck = new Date();
-  $rootScope.checking = false;
-
-  // Get items on focus if  10 seconds passed since last focus
-  angular.element($window).bind('focus',function(){
-    var referenceTime = new Date();
-    var referenceTime = new Date(referenceTime .getTime() - 1000*10);
-    // Time after check is retried even if checking for the last time is still going on,
-    // or check failed for some reason
-    var retryReferenceTime = new Date(referenceTime .getTime() - 1000*20);
-    if ($rootScope.lastCheck < referenceTime){
-      if (!$rootScope.checking || $rootScope.lastCheck < retryReferenceTime){
-        $rootScope.checking = true;
-        $rootScope.lastCheck = new Date();
-        auth.check();
-        $rootScope.checking = false;
-      }
-    }
-  });
-
 }
 
-NavbarController.$inject = ['$location', '$scope', '$rootScope', '$window', 'auth', 'authenticateRequest', 'emSwiper', 'Enum', 'userPrefix', 'userSessionStorage'];
+NavbarController.$inject = ['$location', '$scope', '$window', 'auth', 'authenticateRequest', 'emSwiper', 'Enum', 'userPrefix', 'userSessionStorage'];
 angular.module('em.app').controller('NavbarController', NavbarController);
