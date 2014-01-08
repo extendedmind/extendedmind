bf0c552a87101549ae1e0d46ba253af5c15f6bbd
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2014-01-08 10:44:21 +0200

    Better documentation

--- frontend/core/src/main/webapp/static/app/main/MainController.js
+++ frontend/core/src/main/webapp/static/app/main/MainController.js
@@ -2,8 +2,9 @@
 'use strict';
 
 // Controller for all main slides
-// Holds a reference to all the item arrays, these are needed anyway to get the
-// home and inbox to work.
+// Holds a reference to all the item arrays. There is no sense in limiting
+// the arrays because everything is needed anyway to get home and inbox to work,
+// which are part of every main slide collection. 
 function MainController($scope, DateService, itemsArray, TagsService, tasksArray, OwnerService, FilterService) {
   $scope.items = itemsArray.getItems();
   $scope.tasks = tasksArray.getTasks();
@@ -12,7 +13,6 @@ function MainController($scope, DateService, itemsArray, TagsService, tasksArray
   $scope.filterService = FilterService;
   $scope.dates = DateService.week();
   $scope.date = DateService.today();
-
 }
 
 MainController.$inject = ['$scope', 'DateService', 'itemsArray', 'TagsService', 'tasksArray', 'OwnerService', 'FilterService'];
