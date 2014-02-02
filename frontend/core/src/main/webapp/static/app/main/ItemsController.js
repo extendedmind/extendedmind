2471a88b22d0f18cacdb59f9393021acdbb89a56
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2014-02-02 22:41:45 +0200

    Removed logging

--- frontend/core/src/main/webapp/static/app/main/ItemsController.js
+++ frontend/core/src/main/webapp/static/app/main/ItemsController.js
@@ -6,7 +6,6 @@ function ItemsController($scope, $location, $routeParams, ItemsService) {
   if ($location.path().indexOf('/edit/' != -1) || $location.path().indexOf('/new' != -1)){
     if ($routeParams.uuid) {
       $scope.item = ItemsService.getItemByUUID($routeParams.uuid);
-      console.log($scope.item);
     }else{
       $scope.item = {};
     }
