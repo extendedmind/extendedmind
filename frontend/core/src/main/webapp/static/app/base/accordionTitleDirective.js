82f66c4b7ba0f0b2fcbd62de11304780beda8e30
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2014-02-04 10:06:45 +0200

    Fixed very subtle bug with Phonegap accordion clicking in inbox being slow. The reason was that there wasn't an unused div above accordion. Reason for this is unknown, but probably has to do with element.parent() or parents() for some reason having poor/unstandard performance.

--- frontend/core/src/main/webapp/static/app/base/accordionTitleDirective.js
+++ frontend/core/src/main/webapp/static/app/base/accordionTitleDirective.js
@@ -18,79 +18,79 @@ function accordionTitleDirective($document){
       hasComplete:'=',
       toggleComplete:'&'
     },  // Create an isolated scope
-    link: function(scope, element, attrs, accordionCtrl) {
-      accordionCtrl.addItem(scope);
-      scope.isOpen = false;
-      scope.oldTitle = scope.item.title;
+    link: function($scope, $element, $attrs, accordionCtrl) {
+      accordionCtrl.addItem($scope);
+      $scope.isOpen = false;
+      $scope.oldTitle = $scope.item.title;
 
-      scope.toggleOpen = function() {
-        if (!scope.isOpen){
-          scope.isOpen = true;
-          element.parent().addClass("accordion-item-active");
+      $scope.toggleOpen = function() {
+        if (!$scope.isOpen){
+          $scope.isOpen = true;
+          $element.parent().addClass("accordion-item-active");
         }else{
-          scope.closeItem();          
+          $scope.closeItem();          
         }
-        return scope.isOpen;
+        return $scope.isOpen;
       }
 
-      scope.closeItem = function() {
-        if (scope.isOpen){
-          scope.endTitleEdit();
-          element.parent().removeClass("accordion-item-active");
-          scope.isOpen = false;
+      $scope.closeItem = function() {
+        if ($scope.isOpen){
+          $scope.endTitleEdit();
+          $element.parent().removeClass("accordion-item-active");
+          $scope.isOpen = false;
           return true;
         }
         return false;
       }
 
-      scope.clickTitle = function() {
-        if (scope.isOpen){
-          scope.toggleOpen();
-          accordionCtrl.closeOthers(scope);
+      $scope.clickTitle = function() {
+        if ($scope.isOpen){
+          $scope.toggleOpen();
+          accordionCtrl.closeOthers($scope);
         }else{
           // Not open, don't open unless nothing else was closed
-          if (!accordionCtrl.closeOthers(scope)){
-            scope.toggleOpen();
+          if (!accordionCtrl.closeOthers($scope)){
+            $scope.toggleOpen();
           }
         }
       };
 
-      scope.endTitleEdit = function(){
+      $scope.endTitleEdit = function(){
         // Programmatically blur the input
-        element.find('input#accordionTitleInput')[0].blur();
-        if (scope.oldTitle !== scope.item.title){
+        $element.find('input#accordionTitleInput')[0].blur();
+        if ($scope.oldTitle !== $scope.item.title){
           // Title has changed, call edit title method with new title
-          scope.editItemTitle({item: scope.item});
-          scope.oldTitle = scope.item.title;
+          $scope.editItemTitle({item: $scope.item});
+          $scope.oldTitle = $scope.item.title;
         }
       };
 
-      scope.pressItemEdit = function(){
-        scope.editItem({item: scope.item});
+      $scope.pressItemEdit = function(){
+        $scope.editItem({item: $scope.item});
       };
 
-      scope.startTitleEdit = function($event) {  
+      $scope.startTitleEdit = function($event) {  
         $event.stopPropagation();
       };
 
-      scope.evaluateKeyPress = function($event) {
+      $scope.evaluateKeyPress = function($event) {
         // Enter key
         if(event.which === 13) {
-          scope.endTitleEdit();
+          $scope.endTitleEdit();
           event.preventDefault();
         }
       };
 
-      scope.getTitleInputClass = function() {
-        if (scope.hasComplete){
+      $scope.getTitleInputClass = function() {
+        if ($scope.hasComplete){
           return 'center-input-wrapper';
         }
         return 'left-input-wrapper';
       }
 
-      scope.itemChecked = function() {
-        if (scope.toggleComplete){
-          scope.toggleComplete({item: scope.item});
+      $scope.itemChecked = function() {
+        if ($scope.toggleComplete){
+          $scope.toggleComplete({item: $scope.item});
         }
       };
     }
