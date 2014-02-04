5e32e5e46111a4dc1a0b16297bee82cd10e586f8
JP Salo <jpsalo@zoho.com>
2014-02-04 16:57:10 +0200

    Started implementing new version of datepicker/datebar: DateService refactoring, new HTML layout.

--- frontend/core/src/main/webapp/static/common/DateService.js
+++ frontend/core/src/main/webapp/static/common/DateService.js
@@ -8,58 +8,76 @@ angular.module('common').factory('DateService', [
     'may', 'jun', 'jul', 'aug',
     'sep', 'oct', 'nov', 'dec'
     ];
+    var weekdays = ['s', 'm', 't', 'w', 't', 'f', 's'];
+    var numberOfDays = 7;
 
-    return{
-      yyyymmdd: function(d) {
-        var yyyy, mm, dd;
+    // date object with shared data (prototype/this)
 
-        yyyy = d.getFullYear().toString();
-        mm = (d.getMonth() + 1).toString(); // getMonth() is zero-based
-        dd  = d.getDate().toString();
+    function nextDate(date) {
+      // compare current date with number of days in month
+      // http://stackoverflow.com/a/315767
+      if (date.getDate() < new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()) {
+        date.setDate(date.getDate() + 1);
+      } else {
+        // reset date
+        date.setDate(1);
+        // compare current month with number of months in year
+        if (date.getMonth() < 11) { //getMonth() is zero-based
+          date.setMonth(date.getMonth() + 1);
+        } else {
+        // reset month and year
+        date.setMonth(0);
+        date.setYear(date.getYear() + 1);
+      }
+    }
+    return date;
+  }
 
-        return yyyy +'-'+ (mm[1]?mm:'0'+mm[0]) +'-'+ (dd[1]?dd:'0'+dd[0]); // padding
-      },
-      today: function() {
-        return today;
-      },
-      week: function() {
-        var date, day, week, i;
-        date = new Date();
-        week = [];
-        i = 0;
+  // http://stackoverflow.com/a/3067896
+  function yyyymmdd(date) {
+    var yyyy, mm, dd;
 
-        for (i = 0; i < 7; i++) {
-          day = {};
-          day.date = date.getDate();
+    yyyy = date.getFullYear().toString();
+    mm = (date.getMonth() + 1).toString(); // getMonth() is zero-based
+    dd  = date.getDate().toString();
 
-          day.month = {};
-          day.month.name = monthNames[date.getMonth()];
-          day.month.day = date.getMonth();
+    return yyyy +'-'+ (mm[1]?mm:'0'+mm[0]) +'-'+ (dd[1]?dd:'0'+dd[0]); // padding
+  }
+
+  return {
+    today: function() {
+      return today;
+    },
+    week: function() {
+      var date, day, week, i;
+      date = new Date();
+      week = [];
+      i = 0;
+
+      for (i = 0; i < numberOfDays; i++) {
+        day = {};
+        day.date = date.getDate();
+        day.weekday = weekdays[date.getDay()];
+
+        day.month = {};
+        day.month.name = monthNames[date.getMonth()];
+
+          // is this needed?
+          day.month.number = date.getMonth() + 1;
 
           day.year = date.getFullYear();
-          day.yyyymmdd = this.yyyymmdd(date);
+          day.yyyymmdd = yyyymmdd(date);
           week.push(day);
 
-          if (i === 0){
+          if (i === 0) {
             day.displayDate = 'today';
-            day.displayDateShort = day.displayDate;
             today = day;
           } else {
             day.displayDate = day.month.name + ' ' + day.date;
-            day.displayDateShort = day.date;
           }
+          day.displayDateShort = day.date;
 
-          if (date.getDate() < new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()) {
-            date.setDate(date.getDate() + 1);
-          } else {
-            date.setDate(1);
-            if (date.getMonth() < 11){
-              date.setMonth(date.getMonth() + 1);
-            } else {
-              date.setMonth(0);
-              date.setYear(date.getYear() + 1);
-            }
-          }
+          date = nextDate(date);
         }
         return week;
       }
