6c861cca0bc6b16e6debdcd9ba4c8f61cfc310db
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2014-02-04 16:18:47 +0200

    Fixed omnibar click elsewhere

--- frontend/core/src/main/webapp/static/app/main/HomeController.js
+++ frontend/core/src/main/webapp/static/app/main/HomeController.js
@@ -1,7 +1,7 @@
 /*jshint sub:true*/
 'use strict';
 
-function HomeController($scope, $location, ItemsService, UserSessionService, AuthenticationService) {
+function HomeController($scope, $location, $document, $element, ItemsService, UserSessionService, AuthenticationService) {
 
   $scope.omniBarActive = false;
   $scope.menuActive = false;
@@ -37,11 +37,13 @@ function HomeController($scope, $location, ItemsService, UserSessionService, Aut
 
   $scope.addNewItem = function(omnibarText) {
     if ($scope.omnibarText && $scope.omnibarText.title) {
-      $scope.focusOmnibar = true;
       ItemsService.saveItem(omnibarText).then(function(/*item*/) {
-        // TODO: Highlight new item
+        // TODO: Highlight new item instead of closing omnibar
         $scope.omnibarText = {};
+        $element.find('input#omniItem')[0].focus();
+        $scope.bindElsewhereEvents();
       });
+
     }else{
       $location.path($scope.prefix + '/items/new');
     }
@@ -50,13 +52,49 @@ function HomeController($scope, $location, ItemsService, UserSessionService, Aut
   $scope.omniBarFocus = function(focus) {
     if (focus) {
       $scope.omniBarActive = true;
+      $scope.bindElsewhereEvents();
     } else {
+      $scope.unbindElsewhereEvents();
       if ($scope.omnibarText == null || $scope.omnibarText.title == null || $scope.omnibarText.title.length === 0) {
         $scope.omniBarActive = false;
       }
     }
   };
+
+  // "Click elsewhere to lose omnibar focus"
+
+  $scope.$on('$destroy', function() {
+    $scope.unbindElsewhereEvents();
+  });
+
+  $scope.eventsBound = false;
+  $scope.unbindElsewhereEvents = function() {
+    if ($scope.eventsBound){
+      $document.unbind('click', $scope.elsewhereCallback);
+    }
+    $scope.eventsBound = false;
+  };
+
+  $scope.bindElsewhereEvents = function () {
+    if (!$scope.eventsBound){
+      $document.bind('click', $scope.elsewhereCallback);
+      $scope.eventsBound = true;
+    }
+  };
+
+  $scope.elsewhereCallback = function(event) {
+    // First rule out clicking on omnibar text itself
+    if (event.target.id !== 'omniItem' && event.target.id !== 'omnibarPlus') {
+      $scope.$apply(function() {
+        $scope.unbindElsewhereEvents();
+        $scope.omniBarFocus(false);
+        // Programmatically blur the omnibar
+        $element.find('input#omniItem')[0].blur();
+      });
+    }
+  };
+
 }
 
 angular.module('em.app').controller('HomeController', HomeController);
-HomeController['$inject'] = ['$scope', '$location','ItemsService', 'UserSessionService', 'AuthenticationService'];
+HomeController['$inject'] = ['$scope', '$location', '$document', '$element', 'ItemsService', 'UserSessionService', 'AuthenticationService'];
