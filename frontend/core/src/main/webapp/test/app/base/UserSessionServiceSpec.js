30d3eb533c314db27626b89d6ad68caa23c0ac19
JP Salo <jpsalo@zoho.com>
2014-02-13 09:14:38 +0200

    UserSessionService unit tests.

--- frontend/core/src/main/webapp/test/app/base/UserSessionServiceSpec.js
+++ frontend/core/src/main/webapp/test/app/base/UserSessionServiceSpec.js
@@ -5,24 +5,6 @@ describe('UserSessionService', function() {
   var LocalStorageService, SessionStorageService, UserSessionService;
   var testOwnerUUID = '6be16f46-7b35-4b2d-b875-e13d19681e77';
 
-  // http://stackoverflow.com/a/9428694
-  var mockStorage = function() {
-    var table = {};
-    return {
-      getItem: function(key) {
-        return table[key];
-      },
-      setItem: function(key, value) {
-        table[key] = value.toString();
-      },
-      clear: function() {
-        table = {};
-      }
-    };
-  }();
-  Object.defineProperty(window, 'sessionStorage', {value: mockStorage});
-  Object.defineProperty(window, 'localStorage', {value: mockStorage});
-
   beforeEach(function() {
     module('em.appTest');
 
@@ -31,14 +13,38 @@ describe('UserSessionService', function() {
       SessionStorageService = _SessionStorageService_;
       UserSessionService = _UserSessionService_;
     });
+
+    // http://stackoverflow.com/a/14381941
+    var sessionStore = {};
+    var localStore = {};
+
+    spyOn(sessionStorage, 'getItem').andCallFake(function(key) {
+      return sessionStore[key];
+    });
+    spyOn(sessionStorage, 'setItem').andCallFake(function(key, value) {
+      sessionStore[key] = value + '';
+    });
+    spyOn(sessionStorage, 'clear').andCallFake(function () {
+      sessionStore = {};
+    });
+
+    spyOn(localStorage, 'getItem').andCallFake(function(key) {
+      return localStore[key];
+    });
+    spyOn(localStorage, 'setItem').andCallFake(function(key, value) {
+      localStore[key] = value + '';
+    });
+    spyOn(localStorage, 'clear').andCallFake(function() {
+      localStore = {};
+    });
   });
 
-  afterEach(function() {
-    localStorage.clear();
-    sessionStorage.clear();
-  });
+afterEach(function() {
+  localStorage.clear();
+  sessionStorage.clear();
+});
 
-  it('should be authenticated with uuid in web storage', function() {
+it('should be authenticated with uuid in web storage', function() {
     // Session Storage
     SessionStorageService.setUserUUID(testOwnerUUID);
     expect(UserSessionService.isAuthenticated()).toBeTruthy();
@@ -49,7 +55,7 @@ describe('UserSessionService', function() {
     expect(UserSessionService.isAuthenticated()).toBeTruthy();
   });
 
-  it('should return valid authentication with non expired authentication', function() {
+it('should return valid authentication with non expired authentication', function() {
     // Session Storage
     SessionStorageService.setUserUUID(testOwnerUUID);
     SessionStorageService.setExpires(Date.now());
@@ -62,8 +68,8 @@ describe('UserSessionService', function() {
     expect(UserSessionService.isAuthenticateValid()).toBe(true);
   });
 
-  it('should not return valid authentication with expired authentication', function() {
-    var swapTokenBufferTimeReached = 10*60*1000 + 1;
+it('should not return valid authentication with expired authentication', function() {
+  var swapTokenBufferTimeReached = 10*60*1000 + 1;
 
     // Session Storage
     SessionStorageService.setUserUUID(testOwnerUUID);
@@ -76,20 +82,20 @@ describe('UserSessionService', function() {
     expect(UserSessionService.isAuthenticateValid()).toBeUndefined();
   });
 
-  it('should return true with replaceable authentication', function() {
-    LocalStorageService.setReplaceable(Date.now());
-    expect(UserSessionService.isAuthenticateReplaceable()).toBe(true);
-  });
+it('should return true with replaceable authentication', function() {
+  LocalStorageService.setReplaceable(Date.now());
+  expect(UserSessionService.isAuthenticateReplaceable()).toBe(true);
+});
 
-  it('should return undefined with unreplaceable authentication', function() {
-    var swapTokenBufferTimeReached = 10*60*1000 + 1;
-    LocalStorageService.setReplaceable(Date.now() - swapTokenBufferTimeReached);
-    expect(UserSessionService.isAuthenticateReplaceable()).toBeUndefined();
-  });
+it('should return undefined with unreplaceable authentication', function() {
+  var swapTokenBufferTimeReached = 10*60*1000 + 1;
+  LocalStorageService.setReplaceable(Date.now() - swapTokenBufferTimeReached);
+  expect(UserSessionService.isAuthenticateReplaceable()).toBeUndefined();
+});
 
-  it('should set and get items synchronized timestamp', function() {
-    UserSessionService.setItemsSynchronized(testOwnerUUID);
-    var itemsSynchronized = UserSessionService.getItemsSynchronized(testOwnerUUID);
-    expect(isNaN(itemsSynchronized)).toBe(false);
-  });
+it('should set and get items synchronized timestamp', function() {
+  UserSessionService.setItemsSynchronized(testOwnerUUID);
+  var itemsSynchronized = UserSessionService.getItemsSynchronized(testOwnerUUID);
+  expect(isNaN(itemsSynchronized)).toBe(false);
+});
 });
