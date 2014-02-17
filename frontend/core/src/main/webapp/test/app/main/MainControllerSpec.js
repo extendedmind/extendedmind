1da4196c7b1eaa761da34d68237c10acb6fd0b8e
JP Salo <jpsalo@zoho.com>
2014-02-17 18:21:40 +0200

    MainControllerSpec.

--- frontend/core/src/main/webapp/test/app/main/MainControllerSpec.js
+++ frontend/core/src/main/webapp/test/app/main/MainControllerSpec.js
@@ -1,16 +1,37 @@
 'use strict';
 
 describe('MainController', function() {
-	var $scope, MainController;
+	var $q, $scope, MainController;
+	var UserSessionService;
+
+	var MockItemsService = {
+		synchronize: function() {
+			var deferred = $q.defer();
+			deferred.resolve();
+			return deferred.promise;
+		},
+		getItems: function() {
+			return;
+		}
+	};
 
 	beforeEach(function() {
 		module('em.appTest');
 
-		inject(function($controller, $rootScope) {
+		module('em.services', function ($provide){
+			$provide.value('ItemsService', MockItemsService);
+		});
+
+		inject(function($controller, _$q_, $rootScope, _UserSessionService_) {
+			$q = _$q_;
 			$scope = $rootScope.$new();
 			MainController = $controller('MainController', {
 				$scope: $scope
 			});
+			UserSessionService = _UserSessionService_;
 		});
+	});
+
+	it('should synchronize items', function() {
 	});
 });
