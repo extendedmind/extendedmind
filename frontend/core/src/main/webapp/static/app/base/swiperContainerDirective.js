8c12f9f2d3e4bb7d0e277bf7e32de8e7a70febfb
JP Salo <jpsalo@zoho.com>
2014-02-20 11:34:17 +0200

    Swiper click directive and touch listener functions detecting swipe to prevent clicks when swiping.

--- frontend/core/src/main/webapp/static/app/base/swiperContainerDirective.js
+++ frontend/core/src/main/webapp/static/app/base/swiperContainerDirective.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function swiperContainerDirective(SwiperService) {
+function swiperContainerDirective(SwiperService, $rootScope) {
 
   return {
     restrict: 'A',
@@ -39,6 +39,11 @@ function swiperContainerDirective(SwiperService) {
           element[0].firstElementChild.firstElementChild.addEventListener('touchstart', slideTouchStart, false);
           element[0].firstElementChild.firstElementChild.addEventListener('touchmove', slideTouchMove, false);
         }
+
+        // Listen touch events on slide and set swiping flag on to prevent clickable elements' click event.
+        element[0].addEventListener('touchstart', swipeTouchStart, false);
+        element[0].addEventListener('touchmove', swipeTouchMove, false);
+        element[0].addEventListener('touchend', swipeTouchEnd, false);
         
         // (Re)inializes the swiper after the digest to make sure the whole
         // DOM is ready before this is done. Otherwise Swiper does not register
@@ -82,19 +87,76 @@ function swiperContainerDirective(SwiperService) {
         SwiperService.onSlideChangeEnd($scope, $scope.swiperPath);
       }
 
+      var swipeUp = false;
+      var swipeDown = false;
+      var swipeLeft = false;
+      var swipeRight = false;
+      var swipeStartX, swipeStartY, swipeDistX, swipeDistY;
+      var horizontalRestraint = 1;
+      var verticalRestraint = 1;
+
+      function swipeTouchStart(event) {
+        $rootScope.swiping = false;
+        swipeLeft = false;
+        swipeRight = false;
+        swipeDown = false;
+        swipeUp = false;
+
+        var touchobj = event.changedTouches[0];
+        swipeStartX = touchobj.pageX;
+        swipeStartY = touchobj.pageY;
+      }
+      function swipeTouchMove(event) {
+        swipeLeft = false;
+        swipeRight = false;
+        swipeDown = false;
+        swipeUp = false;
+        /*jshint validthis: true */
+        var touchobj = event.changedTouches[0];
+        swipeDistX = touchobj.pageX - swipeStartX;
+        swipeDistY = touchobj.pageY - swipeStartY;
+
+        // http://www.javascriptkit.com/javatutors/touchevents2.shtml
+        if (Math.abs(swipeDistX) >= horizontalRestraint && Math.abs(swipeDistY) <= horizontalRestraint) { // horizontal
+          if (swipeDistX < 0) {
+            swipeLeft = true;
+            swipeRight = false;
+          } else {
+            swipeLeft = false;
+            swipeRight = true;
+          }
+        } else if (Math.abs(swipeDistY) >= verticalRestraint && Math.abs(swipeDistX) <= verticalRestraint) { // vertical
+          if (swipeDistY < 0) {
+            swipeDown = true;
+            swipeUp = false;
+          } else {
+            swipeDown = false;
+            swipeUp = true;
+          }
+        }
+        swipeStartX = touchobj.pageX;
+        swipeStartY = touchobj.pageY;
+      }
+      function swipeTouchEnd() {
+        // Slide is swiping to some direction.
+        if (swipeUp || swipeDown || swipeRight || swipeRight) {
+          $rootScope.swiping = true;
+        }
+      }
+
       // Overlapping swipers, should stopPropagation be called?
 
       var up = false;
       var down = false;
       var startX, startY, distX, distY;
 
-      function slideTouchStart() {
+      function slideTouchStart(event) {
         var touchobj = event.changedTouches[0];
         startX = touchobj.pageX;
         startY = touchobj.pageY;
       }
 
-      function slideTouchMove() {
+      function slideTouchMove(event) {
         /*jshint validthis: true */
         var touchobj = event.changedTouches[0];
         distX = touchobj.pageX - startX;
@@ -125,5 +187,5 @@ function swiperContainerDirective(SwiperService) {
     }
   };
 }
-swiperContainerDirective['$inject'] = ['SwiperService'];
+swiperContainerDirective['$inject'] = ['SwiperService', '$rootScope'];
 angular.module('em.directives').directive('swiperContainer', swiperContainerDirective);
