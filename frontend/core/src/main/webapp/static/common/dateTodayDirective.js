2e02012e123f53755e3cbb1261207b938c6adb8f
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2014-03-06 21:22:20 +0200

    Changed missing task.due to task.date

--- frontend/core/src/main/webapp/static/common/dateTodayDirective.js
+++ frontend/core/src/main/webapp/static/common/dateTodayDirective.js
@@ -4,7 +4,7 @@ function dateToday() {
   return {
     restrict: 'A',
     link: function(scope, element) {
-      if (!scope.task.due) {
+      if (!scope.task.date) {
         element[0].focus();
         element[0].value = new Date().toISOString().substring(0, 10);
       }
