f4c70fc9400523aa720c3f7aff7c547d058f4860
JP Salo <jukapeka@gmail.com>
2014-03-14 10:20:04 +0200

    Added comment to includeReplaceDirective and removed unused 'attributes' parameter from link function.

--- frontend/core/src/main/webapp/static/common/includeReplaceDirective.js
+++ frontend/core/src/main/webapp/static/common/includeReplaceDirective.js
@@ -1,10 +1,11 @@
 'use strict';
 
+// http://stackoverflow.com/a/20912566
 function includeReplaceDirective() {
   return {
     require: 'ngInclude',
     restrict: 'A', /* optional */
-    link: function (scope, el, attrs) {
+    link: function (scope, el) {
       el.replaceWith(el.children());
     }
   };
