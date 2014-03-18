0bb2cb72f8d0ed7420597815c27fe68f1f04831a
JP Salo <jukapeka@gmail.com>
2014-03-18 21:44:31 +0200

    Access drawer menu with right swipe from leftmost slide. Implemented into tasks slides.

--- frontend/core/src/main/webapp/static/app/base/snapDirective.js
+++ frontend/core/src/main/webapp/static/app/base/snapDirective.js
@@ -1,33 +1,58 @@
 'use strict';
 
-function snapDirective($rootScope, SnapService) {
+function snapDirective($rootScope, SnapService, SwiperService) {
   return {
     restrict: 'A',
     link: function($scope, $element) {
+      var dragElement;
+
+      $scope.setDrawerDragElement = function setDrawerDragElement() {
+        dragElement = document.getElementById('drawer-dragger');
+        var settings = {dragger: dragElement};
+        SnapService.updateSettings(settings);
+
+        // TODO set initial resistance in SwiperService
+        setTimeout(function() {
+          SwiperService.setSwiperResistance('tasks', '100%');
+        }, 0);
+      };
+
       SnapService.createSnapper($element[0]);
-      SnapService.disableSliding();
       $rootScope.noSwiping = false;
-      $rootScope.isSnapVisible = false;
 
       SnapService.registerOpenCallback(snapperOpened);
       SnapService.registerCloseCallback(snapperClosed);
       SnapService.registerAnimatedCallback(snapperAnimated);
 
+      SwiperService.registerSlideChangeCallback(mainSlideChanged, 'tasks', 'snapDirective');
+      // SwiperService.registerSlideChangeCallback(mainSlideChanged, 'notes', 'NavbarController');
+
+      function mainSlideChanged(path, activeIndex) {
+        if (activeIndex === 0) {
+          SwiperService.setSwiperResistance('tasks', '100%');
+        } else {
+          SwiperService.setSwiperResistance('tasks', true);
+        }
+      }
+
       function snapperOpened() {
-        SnapService.enableSliding().then(function() {
-          $rootScope.isSnapVisible = true;
-          $rootScope.noSwiping = true;
-        });
+        if ($rootScope.noSwiping) {
+          $scope.$apply(setNoSwipingClass(true));
+        }
       }
       function snapperClosed() {
-        SnapService.disableSliding().then(function() {
-          $rootScope.noSwiping = false;
-        });
+        if (!$rootScope.noSwiping) {
+          $scope.$apply(setNoSwipingClass(false));
+        }
+      }
+      function setNoSwipingClass(swiping) {
+        $rootScope.noSwiping = swiping;
       }
       function snapperAnimated(snap) {
-        if(snap.state().state === 'closed') {
-          $rootScope.isSnapVisible = false;
-          snapperClosed();
+        if (snap.state().state === 'closed') {
+          $scope.$apply(setNoSwipingClass(false));
+        } else if (snap.state().state === 'left') {
+          $scope.$apply(setNoSwipingClass(true));
         }
       }
       $scope.$on('$destroy', function() {
@@ -37,5 +62,5 @@ function snapDirective($rootScope, SnapService) {
     }
   };
 }
-snapDirective.$inject = ['$rootScope', 'SnapService'];
+snapDirective.$inject = ['$rootScope', 'SnapService', 'SwiperService'];
 angular.module('em.directives').directive('snapDirective', snapDirective);
