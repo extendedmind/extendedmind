e5bf6f17212210a76faf0279d31c2cd4c923c8e1
Joonas Jokela <joonas.jokela@meshtactics.com>
2014-04-15 10:26:15 +0300

    Removed android platform update and few unnecessary build files

--- frontend/phonegap/build-android.sh
+++ frontend/phonegap/build-android.sh
@@ -1,5 +1,4 @@
 # variables
-ANDROID_ROOT="app/platforms/android"
 RELEASE_BUILDDIR="bin"
 RELEASE_UNSIGNED="em-release-unsigned.apk" # produced by "ant release" command
 RELEASE_BUILD_APK="em-release.apk"
@@ -12,8 +11,7 @@ cp ~/.phonegap/android/HelveticaNeueLight.otf app/www/static/fonts/HelveticaNeue
 cp ~/.phonegap/android/HelveticaNeueRoman.ttf app/www/static/fonts/HelveticaNeueRoman.ttf
 
 # new dir
-cd ${ANDROID_ROOT}
-pwd
+cd app
 
 # clean
 if [ -d ${RELEASE_BUILDDIR} ]; then
@@ -21,21 +19,8 @@ if [ -d ${RELEASE_BUILDDIR} ]; then
     rm -r ${RELEASE_BUILDDIR}
 fi
 
-# make sure cordova assets are updated
-# phonegap platform update android
-
 # run phonegap command locally
-cordova build android | grep -A 5 error
+cordova build android
 
 # make new build using ant release configuration
 ant release
-
-# sign the build with keystore file, there is a prompt for password here (ant release does this)
-# jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ${KEYSTORE_FILE} ${RELEASE_BUILDDIR}/${RELEASE_UNSIGNED} extendedmind_beta
-
-# verify signing succeeded
-# TODO: enable again once the maven script is polished
-# jarsigner -verify ${RELEASE_BUILDDIR}/${RELEASE_BUILD_APK}
-
-# align the package (source, destination) (ant relase does this)
-# zipalign -v 4 ${RELEASE_BUILDDIR}/${RELEASE_UNSIGNED} ${RELEASE_BUILDDIR}/${RELEASE_BUILD_APK}
