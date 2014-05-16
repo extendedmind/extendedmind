06a978887bd571789c61ef32f9bafc0dec642757
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2014-05-16 20:40:14 +0300

    Experimental adding of splash hiding to Android

--- frontend/cordova/app/hooks/after_platform_add/add_plugins.js
+++ frontend/cordova/app/hooks/after_platform_add/add_plugins.js
@@ -1,22 +1,23 @@
 #!/usr/bin/env node
- 
+
 //this hook installs all your plugins
- 
+
 var pluginlist = [
-    "https://github.com/driftyco/ionic-plugins-keyboard"
+    "https://github.com/driftyco/ionic-plugins-keyboard",
+    "org.apache.cordova.splashscreen"
 ];
- 
+
 // no need to configure below
- 
+
 var fs = require('fs');
 var path = require('path');
 var sys = require('sys')
 var exec = require('child_process').exec;
- 
+
 function puts(error, stdout, stderr) {
     sys.puts(stdout)
 }
- 
+
 pluginlist.forEach(function(plug) {
     exec("cordova plugin add " + plug, puts);
 });
