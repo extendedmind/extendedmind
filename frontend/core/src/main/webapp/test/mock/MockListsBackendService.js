2092313ddb2402820bcdd7cb3367639b469fc4c8
JP Salo <jukapeka@gmail.com>
2014-05-22 13:21:48 +0300

    Added orderBy filters to ng-repeats in archive to sort items descending by latest completed/archived value.

--- frontend/core/src/main/webapp/test/mock/MockListsBackendService.js
+++ frontend/core/src/main/webapp/test/mock/MockListsBackendService.js
@@ -1,53 +1,54 @@
-/*global angular, getJSONFixture, sessionStorage */
+/*global angular, getJSONFixture */
 'use strict';
 
-function MockListsBackendService($httpBackend, ListsService, UUIDService) {  
+function MockListsBackendService($httpBackend, ListsService, UUIDService) {
 
   function mockPutNewList(expectResponse){
     $httpBackend.whenPUT(ListsService.putNewListRegex)
-      .respond(function(method, url, data, headers) {
-        var putNewListResponse = getJSONFixture('putListResponse.json');
-        putNewListResponse.modified = (new Date()).getTime();
-        putNewListResponse.uuid = UUIDService.randomUUID();
-        return expectResponse(method, url, data, headers, putNewListResponse);
-      });
-  };
-  
+    .respond(function(method, url, data, headers) {
+      var putNewListResponse = getJSONFixture('putListResponse.json');
+      putNewListResponse.modified = (new Date()).getTime();
+      putNewListResponse.uuid = UUIDService.randomUUID();
+      return expectResponse(method, url, data, headers, putNewListResponse);
+    });
+  }
+
   function mockPutExistingList(expectResponse){
     $httpBackend.whenPUT(ListsService.putExistingListRegex)
-      .respond(function(method, url, data, headers) {
-        var putExistingListResponse = getJSONFixture('putExistingListResponse.json');
-        putExistingListResponse.modified = (new Date()).getTime();
-        return expectResponse(method, url, data, headers, putExistingListResponse);
-      });
-  };
+    .respond(function(method, url, data, headers) {
+      var putExistingListResponse = getJSONFixture('putExistingListResponse.json');
+      putExistingListResponse.modified = (new Date()).getTime();
+      return expectResponse(method, url, data, headers, putExistingListResponse);
+    });
+  }
 
   function mockDeleteList(expectResponse){
     $httpBackend.whenDELETE(ListsService.deleteListRegex)
-      .respond(function(method, url, data, headers) {
-        var deleteListResponse = getJSONFixture('deleteListResponse.json');
-        deleteListResponse.result.modified = (new Date()).getTime();
-        return expectResponse(method, url, data, headers, deleteListResponse);
-      });    
-  };
+    .respond(function(method, url, data, headers) {
+      var deleteListResponse = getJSONFixture('deleteListResponse.json');
+      deleteListResponse.result.modified = (new Date()).getTime();
+      return expectResponse(method, url, data, headers, deleteListResponse);
+    });
+  }
 
   function mockUndeleteList(expectResponse){
     $httpBackend.whenPOST(ListsService.undeleteListRegex)
-      .respond(function(method, url, data, headers) {
-        var undeleteListResponse = getJSONFixture('undeleteListResponse.json');
-        undeleteListResponse.modified = (new Date()).getTime();
-        return expectResponse(method, url, data, headers, undeleteListResponse);
-      });    
-  };
+    .respond(function(method, url, data, headers) {
+      var undeleteListResponse = getJSONFixture('undeleteListResponse.json');
+      undeleteListResponse.modified = (new Date()).getTime();
+      return expectResponse(method, url, data, headers, undeleteListResponse);
+    });
+  }
 
   function mockArchiveList(expectResponse){
     $httpBackend.whenPOST(ListsService.archiveListRegex)
-      .respond(function(method, url, data, headers) {
-        var archiveListResponse = getJSONFixture('archiveListResponse.json');
-        archiveListResponse.result.modified = (new Date()).getTime();
-        return expectResponse(method, url, data, headers, archiveListResponse);
-      });    
-  };
+    .respond(function(method, url, data, headers) {
+      var archiveListResponse = getJSONFixture('archiveListResponse.json');
+      archiveListResponse.result.modified = (new Date()).getTime();
+      archiveListResponse.archived = Date.now();
+      return expectResponse(method, url, data, headers, archiveListResponse);
+    });
+  }
 
   return {
     mockListsBackend: function(expectResponse) {
@@ -58,7 +59,7 @@ function MockListsBackendService($httpBackend, ListsService, UUIDService) {
       mockArchiveList(expectResponse);
     }
   };
-};
+}
 
 MockListsBackendService.$inject = ['$httpBackend', 'ListsService', 'UUIDService'];
 angular.module('em.appTest').factory('MockListsBackendService', MockListsBackendService);
