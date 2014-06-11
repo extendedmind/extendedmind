aa1572a756903ca8e872e82d9e8289f41301f97d
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2014-06-11 13:57:48 +0300

    Added feature fast switching

--- frontend/core/src/main/webapp/static/app/main/HeaderController.js
+++ frontend/core/src/main/webapp/static/app/main/HeaderController.js
@@ -2,8 +2,18 @@
 
 function HeaderController($scope, $rootScope, UISessionService) {
 
-
+  $scope.switchFeature = function(){
+    var activeFeature = $scope.getActiveFeature();
+    if (activeFeature === 'inbox'){
+      UISessionService.changeFeature('tasks');
+    }else if (activeFeature === 'tasks'){
+      UISessionService.changeFeature('notes');
+    }else if (activeFeature === 'notes'){
+      UISessionService.changeFeature('archive');
+    }else if (activeFeature === 'archive'){
+      UISessionService.changeFeature('inbox');
+    }
+  };
 }
-
 HeaderController['$inject'] = ['$scope', '$rootScope', 'UISessionService'];
 angular.module('em.app').controller('HeaderController', HeaderController);
