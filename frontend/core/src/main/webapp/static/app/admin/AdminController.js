dc284417cb85b8aa1ea1bbdd8dfe9fbc8485cffa
JP Salo <jukapeka@gmail.com>
2014-07-28 15:35:51 +0300

    Admin module cleaning:  * added apache licence preamble  * renamed anonymous function expressions to named function expressions  * organized dependecy injections alpabetically  * refactoring

--- frontend/core/src/main/webapp/static/app/admin/AdminController.js
+++ frontend/core/src/main/webapp/static/app/admin/AdminController.js
@@ -1,6 +1,20 @@
-'use strict';
+/* Copyright 2013-2014 Extended Mind Technologies Oy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 'use strict';
 
-function AdminController($scope, DateService, AdminService, AuthenticationService, AnalyticsService) {
+ function AdminController($scope, AdminService, AuthenticationService, AnalyticsService, DateService) {
 
   AnalyticsService.visit('admin');
 
@@ -13,56 +27,56 @@ function AdminController($scope, DateService, AdminService, AuthenticationServic
     }
   });
 
-  $scope.gotoUsers = function(){
-    AdminService.getUsers().then(function(usersResponse){
-      if (usersResponse.data){
+  $scope.gotoUsers = function gotoUsers() {
+    AdminService.getUsers().then(function(usersResponse) {
+      if (usersResponse.data) {
         $scope.users = usersResponse.data.users;
         $scope.adminMode = 'users';
       }
     });
   };
 
-  $scope.gotoInvites = function(){
-    AdminService.getInvites().then(function(invitesResponse){
-      if (invitesResponse.data){
+  $scope.gotoInvites = function gotoInvites() {
+    AdminService.getInvites().then(function(invitesResponse) {
+      if (invitesResponse.data) {
         $scope.invites = invitesResponse.data.invites;
         $scope.adminMode = 'invites';
       }
     });
   };
 
-  $scope.resendInvite = function(invite){
-    AuthenticationService.resendInvite(invite.uuid, invite.email).then(function(resendResponse){
-      if (resendResponse.data){
+  $scope.resendInvite = function resendInvite(invite) {
+    AuthenticationService.resendInvite(invite.uuid, invite.email).then(function(resendResponse) {
+      if (resendResponse.data) {
         invite.resent = true;
       }
     });
   };
 
-  $scope.gotoInviteRequests = function(){
-    AdminService.getInviteRequests().then(function(inviteRequestsResponse){
-      if (inviteRequestsResponse.data){
+  $scope.gotoInviteRequests = function gotoInviteRequests() {
+    AdminService.getInviteRequests().then(function(inviteRequestsResponse) {
+      if (inviteRequestsResponse.data) {
         $scope.inviteRequests = inviteRequestsResponse.data.inviteRequests;
         $scope.adminMode = 'inviteRequests';
       }
     });
   };
 
-  $scope.acceptInviteRequest = function(inviteRequest){
-    AdminService.acceptInviteRequest(inviteRequest).then(function(){
-      if (removeInviteRequest(inviteRequest)){
+  $scope.acceptInviteRequest = function acceptInviteRequest(inviteRequest) {
+    AdminService.acceptInviteRequest(inviteRequest).then(function() {
+      if (removeInviteRequest(inviteRequest)) {
         $scope.inviteCount += 1;
       }
     });
   };
 
-  $scope.deleteInviteRequest = function(inviteRequest){
-    AdminService.deleteInviteRequest(inviteRequest).then(function(){
+  $scope.deleteInviteRequest = function deleteInviteRequest(inviteRequest) {
+    AdminService.deleteInviteRequest(inviteRequest).then(function() {
       removeInviteRequest(inviteRequest);
     });
   };
 
-  function removeInviteRequest(inviteRequest){
+  function removeInviteRequest(inviteRequest) {
     var index = $scope.inviteRequests.indexOf(inviteRequest);
     if (index > -1) {
       $scope.inviteRequests.splice(index, 1);
@@ -72,14 +86,14 @@ function AdminController($scope, DateService, AdminService, AuthenticationServic
     }
   }
 
-  $scope.getDateString = function(date){
+  $scope.getDateString = function getDateString(date) {
     return DateService.getYYYYMMDD(new Date(date));
   };
 
-  $scope.gotoStatistics = function(){
+  $scope.gotoStatistics = function gotoStatistics() {
     $scope.adminMode = undefined;
   };
 }
 
-AdminController['$inject'] = ['$scope', 'DateService', 'AdminService', 'AuthenticationService', 'AnalyticsService'];
+AdminController['$inject'] = ['$scope', 'AdminService', 'AuthenticationService', 'AnalyticsService', 'DateService'];
 angular.module('em.app').controller('AdminController', AdminController);
