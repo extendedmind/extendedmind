784999372a555bf74ba47a3049668921bbd622ac
JP Salo <jukapeka@gmail.com>
2014-07-28 15:41:48 +0300

    Archive module cleaning:  * added apache licence preamble  * renamed anonymous function expressions to named function expressions  * refactoring

--- frontend/core/src/main/webapp/static/app/archive/ArchiveController.js
+++ frontend/core/src/main/webapp/static/app/archive/ArchiveController.js
@@ -1,10 +1,24 @@
-'use strict';
+/* Copyright 2013-2014 Extended Mind Technologies Oy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 'use strict';
 
-function ArchiveController($scope, ListsService, NotesService, SwiperService, TasksService, SynchronizeService, UISessionService) {
+ function ArchiveController($scope, ListsService, NotesService, SwiperService, TasksService, SynchronizeService, UISessionService) {
   $scope.archiveSlides = [];
 
-  function setCompletedTaskLimit(fullCompleteTasksSize){
-    if (fullCompleteTasksSize < 25){
+  function setCompletedTaskLimit(fullCompleteTasksSize) {
+    if (fullCompleteTasksSize < 25) {
       $scope.completedTasksLimit = fullCompleteTasksSize;
     }
   }
@@ -31,60 +45,60 @@ function ArchiveController($scope, ListsService, NotesService, SwiperService, Ta
   $scope.isCompletedAndArchivedLoading = true;
   $scope.completedTasksLimit = 0;
 
-  SynchronizeService.synchronizeCompletedAndArchived(UISessionService.getActiveUUID()).then(function(){
+  SynchronizeService.synchronizeCompletedAndArchived(UISessionService.getActiveUUID()).then(function() {
     $scope.isCompletedAndArchivedLoading = false;
   });
 
-  $scope.getCompletedTasksLimit = function(){
+  $scope.getCompletedTasksLimit = function getCompletedTasksLimit() {
     return $scope.completedTasksLimit;
   };
 
-  $scope.addMoreCompleted = function(){
-    if ($scope.completedTasksLimit !== $scope.fullCompletedTasks.length){
+  $scope.addMoreCompleted = function addMoreCompleted() {
+    if ($scope.completedTasksLimit !== $scope.fullCompletedTasks.length) {
       // There is still more to add, add in batches of 25
-      if ($scope.completedTasksLimit + 25 < $scope.fullCompletedTasks.length){
+      if ($scope.completedTasksLimit + 25 < $scope.fullCompletedTasks.length) {
         $scope.completedTasksLimit += 25;
-      }else{
+      } else {
         $scope.completedTasksLimit = $scope.fullCompletedTasks.length;
       }
     }
   };
 
-  function getTodayMidnight(){
+  function getTodayMidnight() {
     var today = new Date();
-    today.setHours(0,0,0,0);
+    today.setHours(0, 0, 0, 0);
     return today;
   }
 
-  function getPreviousDay(numberOfDays){
+  function getPreviousDay(numberOfDays) {
     var today = getTodayMidnight();
-    return new Date(today.setDate(today.getDate()-numberOfDays));
+    return new Date(today.setDate(today.getDate() - numberOfDays));
   }
 
-  function getMondayThisWeek(){
+  function getMondayThisWeek() {
     var today = getTodayMidnight();
     var day = today.getDay();
-    var diff = today.getDate() - day + (day == 0 ? -6:1); // adjust when day is sunday
+    var diff = today.getDate() - day + (day === 0 ? -6 : 1); // adjust when day is sunday
     return new Date(today.setDate(diff));
   }
 
-  function getFirstOfPreviousMonth(numberOfMonths){
+  function getFirstOfPreviousMonth(numberOfMonths) {
     var today = getTodayMidnight();
     if (!numberOfMonths) numberOfMonths = 0;
     return new Date(today.getFullYear(), today.getMonth() - numberOfMonths, 1);
   }
 
-  function getFirstOfThisYear(){
+  function getFirstOfThisYear() {
     var today = getTodayMidnight();
     return new Date(today.getFullYear(), 0, 1);
   }
 
 
   var todayShown = false;
-  $scope.showToday = function(completedTask, first){
+  $scope.showToday = function(completedTask, first) {
     if (first) todayShown = false;
-    if (!todayShown){
-      if (completedTask.completed > getTodayMidnight().getTime()){
+    if (!todayShown) {
+      if (completedTask.completed > getTodayMidnight().getTime()) {
         todayShown = true;
         return true;
       }
@@ -92,12 +106,12 @@ function ArchiveController($scope, ListsService, NotesService, SwiperService, Ta
   };
 
   var yesterdayShown = false;
-  $scope.showYesterday = function(completedTask, first){
+  $scope.showYesterday = function showYesterday(completedTask, first) {
     if (first) yesterdayShown = false;
-    if (!yesterdayShown){
+    if (!yesterdayShown) {
       // Today is the upper border
-      if (completedTask.completed < getTodayMidnight().getTime()){
-        if (completedTask.completed > getPreviousDay(1).getTime()){
+      if (completedTask.completed < getTodayMidnight().getTime()) {
+        if (completedTask.completed > getPreviousDay(1).getTime()) {
           yesterdayShown = true;
           return true;
         }
@@ -107,13 +121,13 @@ function ArchiveController($scope, ListsService, NotesService, SwiperService, Ta
 
 
   var thisWeekShown = false;
-  $scope.showThisWeek = function(completedTask, first){
+  $scope.showThisWeek = function showThisWeek(completedTask, first) {
     if (first) thisWeekShown = false;
-    if (!thisWeekShown){
+    if (!thisWeekShown) {
       // Yesterday is the upper border
-      if (completedTask.completed < getPreviousDay(1).getTime()){
+      if (completedTask.completed < getPreviousDay(1).getTime()) {
         // Get Monday of this week as the other border
-        if (completedTask.completed > getMondayThisWeek().getTime()){
+        if (completedTask.completed > getMondayThisWeek().getTime()) {
           thisWeekShown = true;
           return true;
         }
@@ -122,13 +136,13 @@ function ArchiveController($scope, ListsService, NotesService, SwiperService, Ta
   };
 
   var thisMonthShown = false;
-  $scope.showThisMonth = function(completedTask, first){
+  $scope.showThisMonth = function showThisMonth(completedTask, first) {
     if (first) thisMonthShown = false;
-    if (!thisMonthShown){
+    if (!thisMonthShown) {
       // Get Monday of this week as the lower border
-      if (completedTask.completed < getMondayThisWeek().getTime()){
+      if (completedTask.completed < getMondayThisWeek().getTime()) {
         // Get Monday of this week as the other border
-        if (completedTask.completed > getFirstOfPreviousMonth().getTime()){
+        if (completedTask.completed > getFirstOfPreviousMonth().getTime()) {
           thisMonthShown = true;
           return true;
         }
@@ -137,11 +151,11 @@ function ArchiveController($scope, ListsService, NotesService, SwiperService, Ta
   };
 
   var lastMonthShown = false;
-  $scope.showLastMonth = function(completedTask, first){
+  $scope.showLastMonth = function showLastMonth(completedTask, first) {
     if (first) lastMonthShown = false;
-    if (!lastMonthShown){
-      if (completedTask.completed < getFirstOfPreviousMonth().getTime()){
-        if (completedTask.completed > getFirstOfPreviousMonth(1).getTime()){
+    if (!lastMonthShown) {
+      if (completedTask.completed < getFirstOfPreviousMonth().getTime()) {
+        if (completedTask.completed > getFirstOfPreviousMonth(1).getTime()) {
           lastMonthShown = true;
           return true;
         }
@@ -150,11 +164,11 @@ function ArchiveController($scope, ListsService, NotesService, SwiperService, Ta
   };
 
   var thisYearShown = false;
-  $scope.showThisYear = function(completedTask, first){
+  $scope.showThisYear = function showThisYear(completedTask, first) {
     if (first) thisYearShown = false;
-    if (!thisYearShown){
-      if (completedTask.completed < getFirstOfPreviousMonth(1).getTime()){
-        if (completedTask.completed > getFirstOfThisYear().getTime()){
+    if (!thisYearShown) {
+      if (completedTask.completed < getFirstOfPreviousMonth(1).getTime()) {
+        if (completedTask.completed > getFirstOfThisYear().getTime()) {
           thisYearShown = true;
           return true;
         }
@@ -164,17 +178,17 @@ function ArchiveController($scope, ListsService, NotesService, SwiperService, Ta
 
 
   var lastYearShown = false;
-  $scope.showLastYear = function(completedTask, first){
+  $scope.showLastYear = function showLastYear(completedTask, first) {
     if (first) lastYearShown = false;
-    if (!lastYearShown){
-      if (completedTask.completed < getFirstOfThisYear().getTime()){
+    if (!lastYearShown) {
+      if (completedTask.completed < getFirstOfThisYear().getTime()) {
         lastYearShown = true;
         return true;
       }
     }
   };
 
-  $scope.getFirstShown = function(){
+  $scope.getFirstShown = function getFirstShown() {
     if (todayShown) return 'today';
     if (yesterdayShown) return 'yesterday';
     if (thisWeekShown) return 'thisWeek';
@@ -182,8 +196,7 @@ function ArchiveController($scope, ListsService, NotesService, SwiperService, Ta
     if (lastMonthShown) return 'lastMonth';
     if (thisYearShown) return 'thisYear';
     if (lastYearShown) return 'lastYear';
-  }
-
+  };
 
   function swiperCreatedCallback() {
     // push rest of the horizontal slides to archiveSlides
@@ -199,18 +212,16 @@ function ArchiveController($scope, ListsService, NotesService, SwiperService, Ta
 
   // Navigation
 
-  $scope.showArchivedListDetails = function(selectedList) {
+  $scope.showArchivedListDetails = function showArchivedListDetails(selectedList) {
     $scope.archivedList = selectedList;
     SwiperService.swipeTo('archive/details');
   };
 
-  $scope.deleteListAndShowArchivedLists = function(list) {
+  $scope.deleteListAndShowArchivedLists = function deleteListAndShowArchivedLists(list) {
     SwiperService.swipeTo('archive/lists');
     $scope.deleteList(list);
     $scope.archivedList = undefined;
   };
-
-
 }
 
 ArchiveController['$inject'] = ['$scope', 'ListsService', 'NotesService', 'SwiperService', 'TasksService', 'SynchronizeService', 'UISessionService'];
