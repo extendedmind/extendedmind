2fe05dfe7582b50fca2e7932b81cd88384bf5299
JP Salo <jukapeka@gmail.com>
2014-07-29 15:36:36 +0300

    Added added apache licence preamble to app.js.

--- frontend/core/src/main/webapp/static/app/app.js
+++ frontend/core/src/main/webapp/static/app/app.js
@@ -1,12 +1,27 @@
-/* global $, angular, html5Mode, FastClick, packaging */
-'use strict';
+/* Copyright 2013-2014 Extended Mind Technologies Oy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-angular.module('em.app', ['ngAnimate', 'ngRoute', 'infinite-scroll', 'monospaced.elastic', 'common', 'em.directives', 'em.filters', 'em.services']);
-angular.module('em.directives', ['common']);
-angular.module('em.filters', ['common']);
-angular.module('em.services', ['common', 'em.base64']);
+ /* global $, angular, html5Mode, FastClick, packaging */
+ 'use strict';
 
-angular.module('em.app').config(['$locationProvider', '$routeProvider',
+ angular.module('em.app', ['ngAnimate', 'ngRoute', 'infinite-scroll', 'monospaced.elastic', 'common', 'em.directives', 'em.filters', 'em.services']);
+ angular.module('em.directives', ['common']);
+ angular.module('em.filters', ['common']);
+ angular.module('em.services', ['common', 'em.base64']);
+
+ angular.module('em.app').config(['$locationProvider', '$routeProvider',
   function($locationProvider, $routeProvider) {
 
     // Global variable "html5Mode" is defined in index.html
