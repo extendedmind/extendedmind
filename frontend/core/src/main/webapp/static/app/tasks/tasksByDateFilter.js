ff027ab88660929a9eb8823142011ef75be4d79a
JP Salo <jukapeka@gmail.com>
2014-07-30 10:12:22 +0300

    Added apache licence preamble to missing JS files. Removed deleted visibleTaskContentFilter declaration from index_devel.html. Refactoring.

--- frontend/core/src/main/webapp/static/app/tasks/tasksByDateFilter.js
+++ frontend/core/src/main/webapp/static/app/tasks/tasksByDateFilter.js
@@ -1,21 +1,35 @@
-'use strict';
+/* Copyright 2013-2014 Extended Mind Technologies Oy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 'use strict';
 
-angular.module('em.filters').filter('tasksByDate', ['DateService',
-  function(DateService) {
-    return function(tasks, date) {
-      function isDueDate(task) {
-        return task.due === date;
+ function tasksByDate(DateService) {
+  return function(tasks, date) {
+    function isDueDate(task) {
+      return task.due === date;
+    }
+    function isToday() {
+      return DateService.getTodayYYYYMMDD() === date;
+    }
+    function isOverdue(task) {
+      return DateService.getTodayYYYYMMDD() > task.due;
+    }
+    return tasks.filter(function(task) {
+      if (task.due) {
+        return isDueDate(task) || (isToday() && isOverdue(task));
       }
-      function isToday() {
-        return DateService.getTodayYYYYMMDD() === date;
-      }
-      function isOverdue(task) {
-        return DateService.getTodayYYYYMMDD() > task.due;
-      }
-      return tasks.filter(function(task) {
-        if (task.due) {
-          return isDueDate(task) || (isToday() && isOverdue(task));
-        }
-      });
-    };
-  }]);
+    });
+  };
+}
+angular.module('em.filters').filter('tasksByDate', tasksByDate);
