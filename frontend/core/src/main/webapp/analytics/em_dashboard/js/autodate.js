8b2dbe64c0e8b4aff6503c14a1db89e2d3528500
Joonas Jokela <joonas.jokela@meshtactics.com>
2014-08-06 15:32:46 +0300

    Analytics d3 dashboard code, initial commit

---
+++ frontend/core/src/main/webapp/analytics/em_dashboard/js/autodate.js
@@ -0,0 +1,16 @@
+// automatic date setting
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd='0'+dd
+    } 
+
+    if(mm<10) {
+        mm='0'+mm
+    } 
+
+    today = yyyy+'-'+mm+'-'+dd;
+    console.log("Today is: "+today);
---
+++ frontend/core/src/main/webapp/analytics/em_dashboard/js/barchart.js
@@ -0,0 +1,89 @@
+function initChartToElement(elementId, dataSource) {
+
+var margin = {top: 20, right: 20, bottom: 180, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10, "d");
+
+var svg = d3.select(elementId).append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// parse string to date object
+var parseDate = d3.time.format("%Y-%m-%d_%H:%M:%S+0000");
+
+d3.json(dataSource, function (data) {
+  console.log("data loaded");
+  console.log(data);
+
+  /*data.forEach(function(d) {
+        d.time = parseDate(d.date);
+  });*/
+
+  x.domain(data.map(function(d) { return d.time; }));
+  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.55em")
+      .attr("transform", "rotate(-90)" );
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Data");
+
+  svg.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.time); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return height - y(d.value); });
+
+  svg.selectAll(".bar")
+      .data(data)
+    .enter().append("text")
+      .attr("x", function(d) { return x(d.time); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("dy", ".35em")
+      .text(function(d) { return d.value; });
+
+});
+
+function type(d) {
+    d.value = +d.value;
+
+    var parseDate = d3.time.format("%Y-%m-%d_%H:%M:%S+0000");
+    d.time = parseDate(d.time);
+    return d;
+}
+
+};
