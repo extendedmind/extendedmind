extendedmind/extendedmind

commit af3b634b38960e4a4653ee4ea1d9dcd03a0c1fca

Bugfixes: Editor title had binding to previous item and previous save method was still registered. Clear $scope variables and aboutToClose callback in editorClosed callback.

frontend/core/src/main/webapp/static/app/main/EditorController.js
@@ -64,6 +64,14 @@
     $scope.mode = undefined;
     $scope.editorVisible = false;
     titleBarInputFocusCallbackFunction = titleBarInputBlurCallbackFunction = undefined;
+    featureEditorAboutToCloseCallback = undefined;
+
+    // Reset $scope variables
+    $scope.task = undefined;
+    $scope.note = undefined;
+    $scope.list = undefined;
+    $scope.item = undefined;
+    $scope.tag = undefined;
   }
 
   if (angular.isFunction($scope.registerEditorAboutToOpenCallback))

commit 58eea8ecedd3df347513ac45cf46da3047c29494

Commit af3b634b38960e4a4653ee4ea1d9dcd03a0c1fca broke saving in editor. Fixed saving by clearing previous $scope variables when editor is opening.

frontend/core/src/main/webapp/static/app/main/EditorController.js
@@ -20,8 +20,14 @@
 
   // OPENING, INITIALIZING, CLOSING
 
-
   $scope.initializeEditor = function(editorType, item, mode){
+    // Reset $scope variables. These may exist from previous editor.
+    $scope.task = undefined;
+    $scope.note = undefined;
+    $scope.list = undefined;
+    $scope.item = undefined;
+    $scope.tag = undefined;
+
     $scope.editorType = editorType;
     $scope.editorVisible = true;
     $scope.mode = mode;
@@ -65,13 +71,6 @@
     $scope.editorVisible = false;
     titleBarInputFocusCallbackFunction = titleBarInputBlurCallbackFunction = undefined;
     featureEditorAboutToCloseCallback = undefined;
-
-    // Reset $scope variables
-    $scope.task = undefined;
-    $scope.note = undefined;
-    $scope.list = undefined;
-    $scope.item = undefined;
-    $scope.tag = undefined;
   }
 
   if (angular.isFunction($scope.registerEditorAboutToOpenCallback))