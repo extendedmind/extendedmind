extendedmind/extendedmind

commit 02aa17ca79901c6df3a40d12eb6c9d4a1230dd3d

Nicer layout for context heading

frontend/core/src/main/webapp/static/app/tasks/taskListCompleted.html
@@ -2,3 +2,4 @@
 ng-click="toggleShowCompletedTasks()">
   <span class="text-toggle-completed"></span>
 </button>
+<span style="position:absolute;" ng-if="isCompletedDisabled() || !containsCompleted()">&nbsp;</span>

frontend/core/src/main/webapp/static/less/layout.less
@@ -515,6 +515,11 @@ NOTE: Implementation depends on the removing of --left|--right DOM nodes if they
   margin-bottom: @margin-bottom-heading-justified;
 }
 
+.group-heading.long {
+  font-size: @size-font-not-so-large;
+  margin-bottom: 0;
+}
+
 /*
 `.group-error` groups together multiple error messages.
 */

frontend/core/src/main/webapp/static/app/base/TagsController.js
@@ -41,8 +41,6 @@
   // CONTEXTS
 
   $scope.saveContext = function(context) {
-    console.log("calling save context")
-    console.log(context)
     return TagsService.saveTag(context, UISessionService.getActiveUUID());
   };
 

frontend/core/src/main/webapp/static/app/base/listFooterDirective.js
@@ -28,6 +28,12 @@
         controllers[0].activateAddListItem();
       }
 
+      function trimNavigationText(text){
+        if (text.length > 12){
+          return text.substring(0, 10) + '...';
+        }else return text;
+      }
+
 
       if (controllers[1]){
         var currentSlidePath = controllers[1].getSlidePath();
@@ -48,9 +54,9 @@
           scope.leftNavigationText = scope.featureInfo.slides.left.heading;
           scope.leftNavigationActionParameter = scope.featureInfo.slides.left.path;
 
-          scope.rightNavigationText = scope.featureInfo.slides.right.heading;
+          scope.rightNavigationText = trimNavigationText(scope.featureInfo.slides.right.heading);
           scope.$watch('featureInfo.slides.right.heading', function(newValue) {
-            scope.rightNavigationText = newValue;
+            scope.rightNavigationText = trimNavigationText(newValue);
           });
           scope.rightNavigationActionParameter = scope.featureInfo.slides.right.path;
 

frontend/core/src/main/webapp/static/app/tasks/tasksContext.html
@@ -1,6 +1,13 @@
 <div class="container-content--outer" ng-if="isFeatureActive('tasks')" list-container drawer-handle="left">
   <section class="container-content" list="tasks | itemsFilter:{name:'byTagUUID', filterBy: context ? context.uuid : undefined}" list-order="top">
-    <h2 class="group-heading justified row" ng-bind="context ? '@' + context.title : 'no context'"></h2>
+    <div class="group-mixed heading">
+      <div class="group-mixed--middle">
+        <h2 class="group-heading long justified">
+          <span class="text-heading" ng-bind="context ? '@' + context.title : 'no context'" clamp=2></span>
+        </h2>
+      </div>
+      <div class="group-mixed--right" task-list-completed></div>
+    </div>
     <div list-item-add="getNewTaskWithContext(context)" list-item-add-save="saveTask(newItem)"
     ng-include="'static/app/tasks/taskListItemAdd.html'"></div>
     <div class="animate-list-item-leave"

frontend/core/src/main/webapp/static/less/typography.less
@@ -21,6 +21,7 @@ Fonts in use:
 
 @size-font-jumbotron: 70px;
 @size-font-large: 48px;
+@size-font-not-so-large: 40px;
 @size-font-ingress: 25px;
 @size-font-content: 18px;
 @size-font-medium: 15px;
@@ -84,6 +85,11 @@ h2 {
 # Texts.
 */
 
+[class^="text-"], [class*=" text-"] {
+  .break-words();
+}
+
+
 /*
 Use `.text-details` to separate details from body text.
 */
@@ -105,7 +111,6 @@ Use `.text-details` to separate details from body text.
 .text-teaser, .text-link {
   display: inline-block;
   line-height: @grid-vertical-half;
-  .break-words();
 }
 
 .text-teaser {

commit 96b217cb683fd4bd066762c56a031edb03b664bb

Fixed commit 02aa17ca79901c6df3a40d12eb6c9d4a1230dd3d broke horizontal grid on slides: Removed element with non breaking space (&nbsp;) in taskListCompleted and added .inactive class to its parent to preserve correct dimensions.

frontend/core/src/main/webapp/static/app/tasks/taskListCompleted.html
@@ -2,4 +2,3 @@
 ng-click="toggleShowCompletedTasks()">
   <span class="text-toggle-completed"></span>
 </button>
-<span style="position:absolute;" ng-if="isCompletedDisabled() || !containsCompleted()">&nbsp;</span>

frontend/core/src/main/webapp/static/app/tasks/taskListCompletedDirective.js
@@ -31,20 +31,46 @@
 
           var disableCompleted;
           scope.isCompletedDisabled = function() {
+            tryToChangeElementActivityStatus();
             return disableCompleted;
           };
+
+          var containsCompleted, elementInActive;
+
+          /*
+          * Set element inactive/non-inactive.
+          *
+          * This mimics the functionality of :empty CSS selector which does not seem to work with Angularjs.
+          */
+          function tryToChangeElementActivityStatus() {
+            if (!disableCompleted && !containsCompleted && !elementInActive) {
+              // Set non-inactive element inactive :) Some negation jargon, but this way element is initially
+              // set inactive here when needed, and there is no need to add class immediately.
+              elementInActive = true;
+              element[0].classList.add('inactive');
+            } else if (!disableCompleted && containsCompleted && elementInActive) {
+              // Set inactive element non-inactive. It has a subtle difference with being active.
+              elementInActive = false;
+              element[0].classList.remove('inactive');
+            }
+          }
 
           scope.containsCompleted = function(){
             var taskArray = scope.getList();
             if (taskArray && taskArray.length){
               for (var i = 0; i < taskArray.length;i++){
                 if (taskArray[i].completed && !scope.isTaskFrozen(taskArray[i])){
+                  containsCompleted = true;
+                  tryToChangeElementActivityStatus();
                   return true;
                 }
               }
             }
+            containsCompleted = false;
             showCompletedTasks = false;
+            tryToChangeElementActivityStatus();
           };
+
           function filterCompletedTasks(task, pastDate){
             if (pastDate) {
               disableCompleted = true;

frontend/core/src/main/webapp/static/less/layout.less
@@ -485,6 +485,13 @@ NOTE: Implementation depends on the removing of --left|--right DOM nodes if they
   }
 }
 
+// 1. Set padding-left to default padding for inactive.
+//    NOTE: It would be better to use :empty selector but it did not work with AngularJS 1.3.0 on element with
+//          ng-if so use .inactive instead.
+.group-mixed--right.inactive {
+  padding-left: @grid-horizontal; // [1]
+}
+
 // 1. .group-mixed--middle can be used directly in <a> elements for increased clicking area.
 .group-mixed--middle:first-child {
   padding-left: @grid-horizontal;   /* [1] */