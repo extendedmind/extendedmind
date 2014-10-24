0bd26a5e6bafc6f55b74dbfa0dbbd9f847c12db8
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2014-10-24 12:36:17 +0300

    Settings for disable vibration and hide footer

--- frontend/core/src/main/webapp/static/app/user/UserController.js
+++ frontend/core/src/main/webapp/static/app/user/UserController.js
@@ -17,14 +17,6 @@
  function UserController($http, $location, $q, $rootScope, $scope, $window, AnalyticsService, AuthenticationService, SwiperService,
                          UISessionService, UserService, UserSessionService) {
 
-  $scope.isUserVerified = false;
-  AnalyticsService.visit('account');
-
-  UserService.getAccount().then(function(accountResponse) {
-    $scope.isUserVerified = accountResponse.emailVerified ? true : false;
-    $scope.email = accountResponse.email;
-  });
-
   $scope.isAdmin = function isAdmin() {
     return UserSessionService.getUserType() === 0;
   };
@@ -76,51 +68,20 @@
     });
   };
 
-
   // SETTINGS
-
-  $scope.isUserVerified = false;
-  AnalyticsService.visit('settings');
 
   $scope.settings = {};
 
-  UserService.getAccount().then(function(/*accountResponse*/) {
-    var userPreferences = UserSessionService.getPreferences();
-    if (userPreferences.ui) {
-      if (userPreferences.ui.hideFooter !== undefined) {
-        $scope.settings.hideFooter = userPreferences.ui.hideFooter;
-      }
+  $scope.settingsCheckboxClicked = function(preference) {
+    if ($scope.settings[preference] !== undefined){
+      UserSessionService.setUIPreference(preference, $scope.settings[preference]);
+      UserService.saveAccountPreferences();
     }
-  });
-
-  $scope.onboardingChecked = function onboardingChecked() {
-    UserSessionService.getPreferences();
-    if ($scope.settings.showOnboarding) {
-      UserSessionService.setPreference('onboarded', undefined);
-    } else {
-      UserSessionService.setPreference('onboarded', packaging);
-    }
-    UserService.saveAccountPreferences();
   };
 
+  // TODO: reset onboarding!
   $scope.showOnboardingCheckbox = function showOnboardingCheckbox() {
     return UserSessionService.getUserType() === 0 || UserSessionService.getUserType() === 1;
-  };
-
-  function updateHideSetting(name, hideValue) {
-    var userPreferences = UserSessionService.getPreferences();
-    if (!userPreferences.ui) userPreferences.ui = {};
-    if (hideValue) {
-      userPreferences.ui[name] = true;
-    } else {
-      userPreferences.ui[name] = false;
-    }
-    UserSessionService.setPreferences(userPreferences);
-    UserService.saveAccountPreferences();
-  }
-
-  $scope.hideFooter = function hideFooter() {
-    updateHideSetting('hideFooter', $scope.settings.hideFooter);
   };
 
   // TERMS AND PRIVACY
@@ -139,72 +100,6 @@
       user.privacy = privacyResponse.data;
       $scope.openEditor('user', user, 'privacy');
     });
-  }
-
-  var policyModalElement, contentHeight, footerHeight;
-  $scope.openTermsOfServiceModal = function openTermsOfServiceModal() {
-    AnalyticsService.visit('terms');
-    constructPolicyModal('http://ext.md/terms.html');
-  };
-
-  $scope.openPrivacyPolicyModal = function openPrivacyPolicyModal() {
-    AnalyticsService.visit('privacy');
-    constructPolicyModal('http://ext.md/privacy.html');
-  };
-
-  $scope.closePolicyModal = function closePolicyModal() {
-    $scope.$modalSuccess();
-  };
-
-  $scope.$on('$destroy', tearDown);
-  function tearDown() {
-    angular.element($window).unbind('resize', adjustModalMaxHeightAndPosition);
-  }
-
-  function constructPolicyModal(policyModalTemplateUrl) {
-    var policyModalOptions = {
-      scope: $scope,
-      id: 'policy-modal',
-      showHeaderCloseButton: false
-    };
-
-    $q.all([
-      $http.get(policyModalTemplateUrl),
-      $http.get('static/app/base/policyModalFooter.html')
-      ]).then(initializePolicyModal);
-
-    function initializePolicyModal(policyModalElements) {
-      policyModalOptions.template = policyModalElements[0].data;
-      policyModalOptions.footerTemplate = policyModalElements[1].data;
-
-      ModalService.createDialog(null, policyModalOptions);
-      policyModalElement = document.getElementById('policy-modal');
-
-      angular.element(policyModalElement).bind('show.bs.modal', adjustModalMaxHeightAndPosition);
-      angular.element($window).bind('resize', adjustModalMaxHeightAndPosition);
-    }
-  }
-
-  function adjustModalMaxHeightAndPosition() {
-    if (!policyModalElement.classList.contains('in')) {
-      policyModalElement.style.display = 'block';
-    }
-
-    contentHeight = $window.innerHeight - 20;
-    footerHeight = policyModalElement.getElementsByClassName('modal-footer')[0].offsetHeight;
-
-    policyModalElement.getElementsByClassName('modal-content')[0].style.maxHeight = contentHeight;
-    policyModalElement.getElementsByClassName('modal-body')[0].style.maxHeight = (contentHeight -
-                                                                                  footerHeight) + 'px';
-
-    var modalDialogElement = policyModalElement.getElementsByClassName('modal-dialog')[0];
-
-    modalDialogElement.style.marginTop = -(modalDialogElement.offsetHeight / 2) + 'px';
-    modalDialogElement.style.marginLeft = -(modalDialogElement.offsetWidth / 2) + 'px';
-
-    if (!policyModalElement.classList.contains('in')) {
-      policyModalElement.style.display = '';
-    }
   }
 }
 UserController['$inject'] = ['$http', '$location', '$q', '$rootScope', '$scope', '$window',
