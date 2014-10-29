2e7b2b8364d8ccf42874487a9ac3268a76f13110
Tuomas Saarela <tuomas@ext.md>
2014-10-29 11:19:55 +0200

    android splash script update

--- frontend/cordova/app/hooks/after_platform_add/setup_android_assets.js
+++ frontend/cordova/app/hooks/after_platform_add/setup_android_assets.js
@@ -9,16 +9,18 @@ var path = require( "path" ),
     buildroot = rootdir + "/assets/build/android";
 
 if (shell.test('-d', androidroot)){
-  [ "-hdpi", "-mdpi", "-xhdpi", "-xxhdpi" ].forEach( function( item ) {
+  [ "-hdpi", "-mdpi", "-xhdpi", "-xxhdpi", "-xxxhdpi" ].forEach( function( item ) {
       // copy icons to Cordova Android directories and filenames
       shell.exec( "cp -f " + iconroot + "/*" + item + ".png " + androidroot + "/res/drawable" + item + "/icon.png", {silent:false} );
       // copy splash screens to Cordova Android directory
       shell.exec( "cp -f " + screenroot + "/*" + item + ".png " + androidroot + "/res/drawable" + item + "/splash.9.png", {silent:false} );
-
   });
-  shell.exec( "cp -f " + iconroot + "/icon.png " + androidroot + "/res/drawable/icon.png", {silent:false} );
 
-  shell.exec( "cp -f " + screenroot + "/screen.png " + androidroot + "/res/drawable/splash.9.png", {silent:false} );
+  // Using XHDPI as default size
+
+  shell.exec( "cp -f " + iconroot + "/icon-96-xhdpi.png " + androidroot + "/res/drawable/icon.png", {silent:false} );
+
+  shell.exec( "cp -f " + screenroot + "/launch-xhdpi.png " + androidroot + "/res/drawable/splash.9.png", {silent:false} );
 
   // copy ant.properties
   shell.exec( "cp -f " + buildroot + "/ant.properties " + androidroot, {silent:false} );
