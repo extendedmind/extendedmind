048e83f1a49a14237d1bd86865d7cd0a9015361f
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2014-11-10 12:08:37 +0200

    Added another Android device to script

--- frontend/cordova/launch-android.sh
+++ frontend/cordova/launch-android.sh
@@ -3,5 +3,12 @@ mvn -o clean install -DskipTests=true
 cd ../cordova
 mvn -o clean install
 cd app
-cordova run android --target=4272b012
+
+if test "$1" == "tuomas"
+then
+  cordova run android --target=1299e503
+else
+  cordova run android --target=4272b012
+fi
+
 cd ..
