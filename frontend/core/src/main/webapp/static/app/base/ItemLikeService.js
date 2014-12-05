85ce8077c0fd0ef71bfba520e3c032a5eceeff98
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2014-12-05 15:58:10 +0200

    Added some ideas on how to use ItemLikeService to implement saving, deleting and undeleting in a consistent way

--- frontend/core/src/main/webapp/static/app/base/ItemLikeService.js
+++ frontend/core/src/main/webapp/static/app/base/ItemLikeService.js
@@ -16,19 +16,115 @@
  /*global angular */
  'use strict';
 
- function ItemLikeService(PersistentStorageService) {
+ function ItemLikeService($q, BackendClientService, PersistentStorageService, UserSessionService) {
 
   return {
-    isEdited: function(itemLike, ownerUUID, extraCompareFn){
+    createMetaItem: function(){
+      return {
+        'uuid': {
+          type: 'id',
+          maxlength: '36'
+        },
+        'title': {
+          type: 'string',
+          maxlength: '128',
+        },
+        'description': {
+          type: 'string',
+          maxlength: '1024'
+        },
+        'link': {
+          type: 'string',
+          maxlength: '512'
+        },
+        'created': {
+          type: 'timestamp',
+        },
+        'modified': {
+          type: 'timestamp',
+        },
+        'deleted': {
+          type: 'timestamp',
+        }
+      }
+    },
+    isEdited: function(itemLike, ownerUUID, itemFunctions){
 
     },
-    prepareTransport: function(itemLike, ownerUUID, extraPrepareFn){
+    generateTransportableItemLike: function(item, ownerUUID, itemFunctions){
 
     },
-    persistItemLike: function(save){
+    generatePersistableItemLike: function(item, ownerUUID, itemFunctions){
 
-    }
+    },
+    saveItemLike: function(item, itemType, ownerUUID, itemFunctions){
+      var deferred = $q.defer();
+
+      if (this.isEdited(item, ownerUUID, itemFunctions)){
+        // When item is not edited, just resolve without giving an item as parameter to indicate
+        // nothing was actually saved
+        deferred.resolve();
+      } else if (item.uuid) {
+        // Existing item
+
+        var transportItem = this.generateTransportItemLike(item, ownerUUID, itemFunctions);
+
+        if (UserSessionService.isOfflineEnabled()) {
+          // Push to offline buffer
+          params = {type: 'item', owner: ownerUUID, uuid: item.uuid};
+          BackendClientService.put('/api/' + params.owner + '/item/' + item.uuid,
+                                   this.putNewItemRegex, params, item);
+          item.modified = BackendClientService.generateFakeTimestamp();
+          item.trans = transientProperties;
+          itemFunctions.update(item, ownerUUID);
+          deferred.resolve(item);
+        } else {
+          // Online
+          BackendClientService.putOnline('/api/' + ownerUUID + '/item/' + item.uuid,
+                                         this.putExistingItemRegex, item)
+          .then(function(result) {
+            if (result.data) {
+              item.modified = result.data.modified;
+              item.trans = transientProperties;
+              itemFunctions.update(item, ownerUUID);
+              deferred.resolve(item);
+            }
+          });
+        }
+      } else {
+        // New item
+        if (UserSessionService.isOfflineEnabled()) {
+          // Push to offline queue with fake UUID
+          var fakeUUID = UUIDService.generateFakeUUID();
+          params = {type: 'item', owner: ownerUUID, fakeUUID: fakeUUID};
+          BackendClientService.put('/api/' + params.owner + '/item',
+                                   this.putNewItemRegex, params, item);
+          // Use the fake uuid and a fake modified that is far enough in the to make
+          // it to the end of the list
+          item.uuid = fakeUUID;
+          item.created = item.modified = BackendClientService.generateFakeTimestamp();
+          item.trans = {itemType: 'item'};
+          itemFunctions.set(item, ownerUUID);
+          deferred.resolve(item);
+        } else {
+          // Online
+          BackendClientService.putOnline('/api/' + ownerUUID + '/item',
+                                         this.putNewItemRegex, item)
+          .then(function(result) {
+            if (result.data) {
+              item.uuid = result.data.uuid;
+              item.created = result.data.created;
+              item.modified = result.data.modified;
+              item.trans = {itemType: 'item'};
+              itemFunctions.set(item, ownerUUID);
+              deferred.resolve(item);
+            }
+          });
+        }
+      }
+      return deferred.promise;
+    },
   };
 }
-ItemLikeService['$inject'] = ['PersistentStorageService'];
+ItemLikeService['$inject'] = ['$q', 'BackendClientService', 'PersistentStorageService', 'UserSessionService'];
 angular.module('em.main').factory('ItemLikeService', ItemLikeService);
