extendedmind/extendedmind

commit fd81c95b5fa841bb93fc659e0403b46f7af83904

Fixed feature change to compare new data with data from current feature instead of undefined.

frontend/core/src/main/webapp/static/app/main/MainController.js
@@ -231,7 +231,7 @@ function MainController($element, $controller, $filter, $q, $rootScope, $scope, 
   var featurePendingOpen;
   $scope.changeFeature = function(feature, data, toggleMenu, pending){
     var currentFeature = UISessionService.getCurrentFeatureName();
-    var currentData = UISessionService.getFeatureData();
+    var currentData = UISessionService.getFeatureData(currentFeature);
     var featureChanged = !(currentFeature === feature && currentData === data);
     var featureInfos = $scope.features[feature];
 
@@ -417,7 +417,7 @@ function MainController($element, $controller, $filter, $q, $rootScope, $scope, 
 
   // Start from tasks on onboarding, or later on, from focus
   if (!$scope.onboardingInProgress){
-    $scope.changeFeature('focus');
+    $scope.changeFeature('inbox');
   }else{
     $scope.changeFeature('tasks');
   }

commit e12aea3867fd9cb8cf8d6db763ed07ee2fcb8203

Set initial feature back to 'focus' from 'inbox'. Was broken since fd81c95b5fa841bb93fc659e0403b46f7af83904.

frontend/core/src/main/webapp/static/app/main/MainController.js
@@ -417,7 +417,7 @@ function MainController($element, $controller, $filter, $q, $rootScope, $scope, 
 
   // Start from tasks on onboarding, or later on, from focus
   if (!$scope.onboardingInProgress){
-    $scope.changeFeature('inbox');
+    $scope.changeFeature('focus');
   }else{
     $scope.changeFeature('tasks');
   }