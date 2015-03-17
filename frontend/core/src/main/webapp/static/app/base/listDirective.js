extendedmind/extendedmind

commit 667d83a8969791274df9170a751b98dc2174143b

Cache tasks in focus slides and filter in JS.

frontend/core/src/main/webapp/static/app/base/listDirective.js
@@ -29,10 +29,21 @@
         return listArrayFn($scope);
       };
 
-      $scope.getVisibleArrayLength = function() {
-        if ($scope.listInfos && $scope.listInfos.array) {
-          return $scope.listInfos.array.length;
+      $scope.getVisibleArray = function(getFullArrayFn, limitTo) {
+        var fullArray = getFullArrayFn();
+
+        if (customFilterItemVisible) {
+          $scope.listInfos.array = customFilterItemVisible(fullArray);  // Cache filtered full array
+          return $scope.listInfos.array.slice(0, limitTo);
         }
+
+        $scope.listInfos.array = fullArray; // Cache filtered full array
+        return $scope.listInfos.array.slice(0, limitTo);
+      };
+
+      $scope.getFilteredFullArrayLength = function() {
+        if ($scope.listInfos.array)
+          return $scope.listInfos.array.length;
       };
 
       this.notifyArrayVisible = function(/*array*/) {
@@ -86,7 +97,7 @@
         $scope.isNearListBottomCallback = callback;
         if ($scope.listInfos && $scope.listInfos.array) {
           // Return initial status to caller.
-          return $scope.currentListLimitTo < $scope.listInfos.array.length;
+          return $scope.currentListLimitTo < $scope.getFilteredFullArrayLength();
         }
       };
 
@@ -305,26 +316,26 @@
       function addMoreItemsToBottom(){
 
         function doAddMoreItemsToBottom(){
-          if (scope.maximumNumberOfItems + scope.itemIncreaseAmount >= visibleArrayLength) {
-            setLimits(visibleArrayLength - scope.maximumNumberOfItems);
+          if (scope.maximumNumberOfItems + scope.itemIncreaseAmount >= filteredFullArrayLength) {
+            setLimits(filteredFullArrayLength - scope.maximumNumberOfItems);
           } else {
             setLimits(scope.currentListStartIndex + scope.itemIncreaseAmount);
           }
         }
 
-        var visibleArrayLength = scope.getVisibleArrayLength();
-        if ((visibleArrayLength - scope.currentListStartIndex) > scope.maximumNumberOfItems) {
+        var filteredFullArrayLength = scope.getFilteredFullArrayLength();
+        if ((filteredFullArrayLength - scope.currentListStartIndex) > scope.maximumNumberOfItems) {
           if (preventListModify) {
             // List modifying is prevented.
             allowListModifyDeferred = $q.defer();
             allowListModifyDeferred.promise.then(function() {
               // List modifying is allowed again.
-              doAddMoreItemsToBottom(visibleArrayLength);
+              doAddMoreItemsToBottom(filteredFullArrayLength);
               allowListModifyDeferred = undefined;
             });
           } else {
             // Ok to to add more items.
-            doAddMoreItemsToBottom(visibleArrayLength);
+            doAddMoreItemsToBottom(filteredFullArrayLength);
             if (!$rootScope.$$phase && !scope.$$phase) scope.$digest(); // Update UI.
           }
         }
@@ -364,9 +375,9 @@
       }
 
       function setIsNearListBottom() {
-        if (scope.currentListLimitTo >= scope.getVisibleArrayLength()) {
+        if (scope.currentListLimitTo >= scope.getFilteredFullArrayLength()) {
           if (angular.isFunction(scope.isNearListBottomCallback)) scope.isNearListBottomCallback(false);
-        } else if (scope.getVisibleArrayLength()) {
+        } else if (scope.getFilteredFullArrayLength()) {
           if (angular.isFunction(scope.isNearListBottomCallback)) scope.isNearListBottomCallback(true);
         }
       }

frontend/core/src/main/webapp/static/app/focus/focusTasks.html
@@ -324,23 +324,21 @@
 
         <div class="swiper-slide" swiper-slide="focus/tasks/rightDuplicate" slide-index="0" swiper-slide-duplicate="true">
           <div class="container-content" ng-class="daySlides[2].pastDate && 'past-date'"
-            list="tasks | tasksByDate:daySlides[2].info:daySlides[2].pastDate" list-hide-item-default>
+            list="getTasksArray('date', {date: daySlides[2].info, pastDate: daySlides[2].pastDate})"
+            list-hide-item-default>
             <div ng-if="isFeatureActive('focus')">
 
               <div ng-hide="datepickerVisible" class="group-mixed heading">
                 <div class="group-mixed--middle" swiper-click="openDatepicker(daySlides[2].info)">
                   <h2 class="group-heading justified" ng-bind="daySlides[2].heading"></h2>
                 </div>
-                <div class="group-mixed--right" task-list-completed="rightDuplicateTasksDate"></div>
+                <div class="group-mixed--right" task-list-completed="{{daySlides[2].pastDate}}"></div>
               </div>
 
               <div list-item-add="getNewDayTask(2)" list-item-add-save="saveTask(newItem)"
                 ng-include="urlBase + 'app/tasks/taskListItemAdd.html'"></div>
               <div class="animate-list-item-leave"
-                ng-repeat="task in listInfos.array = (getFullArray()
-                | paramFilter:isListItemVisible:daySlides[2].pastDate)
-                | orderBy:'trans.created':true
-                | limitTo: currentListLimitTo track by task.trans.uuid"
+                ng-repeat="task in getVisibleArray(getFullArray, currentListLimitTo)"
                 list-item ng-include="urlBase + 'app/tasks/taskListItem.html'"></div>
               <div list-prompt="tasks" list-prompt-infos="listInfos"
                 list-prompt-hide="[listItemAddActive, onboardingInProgress, daySlidesInfosCleared]"></div>
@@ -350,23 +348,22 @@
 
         <div class="swiper-slide" swiper-slide="focus/tasks/left" slide-index="1" swiper-slide-loop-start="true">
           <div class="container-content" ng-class="daySlides[0].pastDate && 'past-date'"
-            list="tasks | tasksByDate:daySlides[0].info:daySlides[0].pastDate" list-hide-item-default>
+            list="getTasksArray('date', {date: daySlides[0].info, pastDate: daySlides[0].pastDate})"
+            list-hide-item-default>
             <div ng-if="isFeatureActive('focus')">
 
               <div ng-hide="datepickerVisible" class="group-mixed heading">
                 <div class="group-mixed--middle" swiper-click="openDatepicker(daySlides[0].info)">
                   <h2 class="group-heading justified" ng-bind="daySlides[0].heading"></h2>
                 </div>
-                <div class="group-mixed--right" task-list-completed></div>
+                <div class="group-mixed--right" task-list-completed="{{daySlides[0].pastDate}}"></div>
               </div>
 
               <div list-item-add="getNewDayTask(0)" list-item-add-save="saveTask(newItem)"
                 ng-include="urlBase + 'app/tasks/taskListItemAdd.html'"></div>
               <div class="animate-list-item-leave"
-                ng-repeat="task in listInfos.array = (getFullArray()
-                | paramFilter:isListItemVisible:daySlides[0].pastDate)
-                | orderBy:'trans.created':true
-                | limitTo: currentListLimitTo track by task.trans.uuid"
+                ng-repeat="task in getVisibleArray(getFullArray, currentListLimitTo)
+                track by task.trans.uuid"
                 list-item ng-include="urlBase + 'app/tasks/taskListItem.html'"></div>
               <div list-prompt="tasks" list-prompt-infos="listInfos"
                 list-prompt-hide="[listItemAddActive, onboardingInProgress, daySlidesInfosCleared]"></div>
@@ -376,23 +373,22 @@
 
         <div class="swiper-slide" swiper-slide="focus/tasks/middle" slide-index="2">
           <div class="container-content" ng-class="daySlides[1].pastDate && 'past-date'"
-            list="tasks | tasksByDate:daySlides[1].info:daySlides[1].pastDate" list-hide-item-default>
+            list="getTasksArray('date', {date: daySlides[1].info, pastDate: daySlides[1].pastDate}, daySlides[1].info)"
+            list-hide-item-default>
             <div ng-if="isFeatureActive('focus')">
 
               <div ng-hide="datepickerVisible" class="group-mixed heading">
                 <div class="group-mixed--middle" swiper-click="openDatepicker(daySlides[1].info)">
                   <h2 class="group-heading justified" ng-bind="daySlides[1].heading"></h2>
                 </div>
-                <div class="group-mixed--right" task-list-completed></div>
+                <div class="group-mixed--right" task-list-completed="{{daySlides[1].pastDate}}"></div>
               </div>
 
               <div list-item-add="getNewDayTask(1)" list-item-add-save="saveTask(newItem)"
                 ng-include="urlBase + 'app/tasks/taskListItemAdd.html'"></div>
               <div class="animate-list-item-leave"
-                ng-repeat="task in listInfos.array = (getFullArray()
-                | paramFilter:isListItemVisible:daySlides[1].pastDate)
-                | orderBy:'trans.created':true
-                | limitTo: currentListLimitTo track by task.trans.uuid"
+                ng-repeat="task in getVisibleArray(getFullArray, currentListLimitTo)
+                track by task.trans.uuid"
                 list-item ng-include="urlBase + 'app/tasks/taskListItem.html'"></div>
               <div list-prompt="tasks" list-prompt-infos="listInfos"
                 list-prompt-hide="[listItemAddActive, onboardingInProgress, daySlidesInfosCleared]"></div>
@@ -402,23 +398,22 @@
 
         <div class="swiper-slide" swiper-slide="focus/tasks/right" slide-index="3">
           <div class="container-content" ng-class="daySlides[2].pastDate && 'past-date'"
-            list="tasks | tasksByDate:daySlides[2].info:daySlides[2].pastDate" list-hide-item-default>
+            list="getTasksArray('date', {date: daySlides[2].info, pastDate: daySlides[2].pastDate})"
+            list-hide-item-default>
             <div ng-if="isFeatureActive('focus')">
 
               <div ng-hide="datepickerVisible" class="group-mixed heading">
                 <div class="group-mixed--middle" swiper-click="openDatepicker(daySlides[2].info)">
                   <h2 class="group-heading justified" ng-bind="daySlides[2].heading"></h2>
                 </div>
-                <div class="group-mixed--right" task-list-completed></div>
+                <div class="group-mixed--right" task-list-completed="{{daySlides[2].pastDate}}"></div>
               </div>
 
               <div list-item-add="getNewDayTask(2)" list-item-add-save="saveTask(newItem)"
                 ng-include="urlBase + 'app/tasks/taskListItemAdd.html'"></div>
               <div class="animate-list-item-leave"
-                ng-repeat="task in listInfos.array = (getFullArray()
-                | paramFilter:isListItemVisible:daySlides[2].pastDate)
-                | orderBy:'trans.created':true
-                | limitTo: currentListLimitTo track by task.trans.uuid"
+                ng-repeat="task in getVisibleArray(getFullArray, currentListLimitTo)
+                track by task.trans.uuid"
                 list-item ng-include="urlBase + 'app/tasks/taskListItem.html'"></div>
               <div list-prompt="tasks" list-prompt-infos="listInfos"
                 list-prompt-hide="[listItemAddActive, onboardingInProgress, daySlidesInfosCleared]"></div>
@@ -428,23 +423,22 @@
 
         <div class="swiper-slide" swiper-slide="focus/tasks/leftDuplicate" slide-index="4" swiper-slide-duplicate="true">
           <div class="container-content" ng-class="daySlides[0].pastDate && 'past-date'"
-            list="tasks | tasksByDate:daySlides[0].info:daySlides[0].pastDate" list-hide-item-default>
+            list="getTasksArray('date', {date: daySlides[0].info, pastDate: daySlides[0].pastDate})"
+            list-hide-item-default>
             <div ng-if="isFeatureActive('focus')">
 
               <div ng-hide="datepickerVisible" class="group-mixed heading">
                 <div class="group-mixed--middle" swiper-click="openDatepicker(daySlides[0].info)">
                   <h2 class="group-heading justified" ng-bind="daySlides[0].heading"></h2>
                 </div>
-                <div class="group-mixed--right" task-list-completed></div>
+                <div class="group-mixed--right" task-list-completed="{{daySlides[0].pastDate}}"></div>
               </div>
 
               <div list-item-add="getNewDayTask(0)" list-item-add-save="saveTask(newItem)"
                 ng-include="urlBase + 'app/tasks/taskListItemAdd.html'"></div>
               <div class="animate-list-item-leave"
-                ng-repeat="task in listInfos.array = (getFullArray()
-                | paramFilter:isListItemVisible:daySlides[0].pastDate)
-                | orderBy:'trans.created':true
-                | limitTo: currentListLimitTo track by task.trans.uuid"
+                ng-repeat="task in getVisibleArray(getFullArray, currentListLimitTo)
+                track by task.trans.uuid"
                 list-item ng-include="urlBase + 'app/tasks/taskListItem.html'"></div>
               <div list-prompt="tasks" list-prompt-infos="listInfos"
                 list-prompt-hide="[listItemAddActive, onboardingInProgress, daySlidesInfosCleared]"></div>

frontend/core/src/main/webapp/static/app/tasks/tasksAll.html
@@ -1,5 +1,5 @@
 <div class="container-content--outer" ng-if="isFeatureActive('tasks')" list-container drawer-handle="left">
-  <section class="container-content" list="allTasks" list-recent>
+  <section class="container-content" list="getTasksArray('all')" list-recent>
 
     <div ng-show="!onboardingInProgress || isOnboardingListItemAddActive()">
       <div class="group-mixed heading">
@@ -12,7 +12,7 @@
         list-item-add-save="saveTask(newItem)" ng-include="urlBase + 'app/tasks/taskListItemAdd.html'"></div>
       <div class="animate-list-item-leave"
         ng-repeat="task in listInfos.array = (getFullArray() | paramFilter:isListItemVisible)
-        | orderBy:getTaskModified:true | limitTo: currentListLimitTo track by task.trans.uuid"
+        | limitTo: currentListLimitTo track by task.trans.uuid"
         list-item ng-include="urlBase + 'app/tasks/taskListItem.html'">
       </div>
       <div list-prompt="tasks" list-prompt-infos="listInfos"

commit e06a3cbdc25a6c8c7892699d11a6b9ab2b5b1915

Fixed "Controller 'list', required by directive 'listItem', can't be found" as in a7598183bcaa39a502d0a167840fa377b7b938e7. Was broken since 667d83a8969791274df9170a751b98dc2174143b.

frontend/core/src/main/webapp/static/app/base/listDirective.js
@@ -23,6 +23,11 @@
     controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
       $scope.listInfos = {};
 
+      $scope.listOptions = {};
+      if ($attrs.listOptions) {
+        $scope.listOptions = $parse($attrs.listOptions)($scope);
+      }
+
       var listArrayFn = $parse($attrs.list);
       $scope.getFullArray = function(){
         return listArrayFn($scope);
@@ -33,10 +38,15 @@
         if (fullArray) {
           if (customFilterItemVisible) {
             $scope.listInfos.array = customFilterItemVisible(fullArray);  // Cache filtered full array
-            return $scope.listInfos.array.slice(0, limitTo);
+          } else if ($scope.listOptions.hideItemDefault) {
+            // Return empty array.
+            // NOTE:  This is intended to be working together with custom filter. While it is not visible,
+            //        but will be later, use this to avoid Angular error "Controller 'list', required by
+            //        directive 'listItem', can't be found!"
+            $scope.listInfos.array = [];
+          } else {
+            $scope.listInfos.array = fullArray; // Cache filtered full array
           }
-
-          $scope.listInfos.array = fullArray; // Cache filtered full array
           return $scope.listInfos.array.slice(0, limitTo);
         }
       };
@@ -175,14 +185,10 @@
         return listData;
       }
 
-      var listOptions;
-      if (attrs.listOptions) {
-        listOptions = $parse(attrs.listOptions)(scope);
-      }
-      initList(listOptions);
+      initList(scope.listOptions);
 
       function isListBounded() {
-        return listOptions && typeof listOptions.isBounded === 'function' && listOptions.isBounded();
+        return scope.listOptions.isBounded === 'function' && scope.listOptions.isBounded();
       }
 
       function listActive(){
@@ -230,7 +236,7 @@
           if (!scope.$$phase && !$rootScope.$$phase) scope.$digest(); // Update UI.
         }
 
-        var duplicateListData = controllers[0].getDuplicateListData(listOptions.id);
+        var duplicateListData = controllers[0].getDuplicateListData(scope.listOptions.id);
         if (duplicateListData) {
           if (duplicateListData.element.scrollTop) {
             duplicateListData.element.scrollTop = 0;
@@ -351,14 +357,14 @@
       }
 
       function modeChanged() {
-        reInitList(listOptions);
+        reInitList(scope.listOptions);
       }
 
       scope.addMoreItems = function() {
         var limit = scope.getFilteredFullArrayLength();
         setLimits(limit);
-        if (listOptions && listOptions.duplicate) {
-          var duplicateListData = controllers[0].getDuplicateListData(listOptions.id);
+        if (scope.listOptions.duplicate) {
+          var duplicateListData = controllers[0].getDuplicateListData(scope.listOptions.id);
           if (duplicateListData) {
             duplicateListData.setLimits(limit);
           }
@@ -477,8 +483,8 @@
 
           setLimits(limit);
 
-          if (listOptions && listOptions.duplicate) {
-            duplicateListData = controllers[0].getDuplicateListData(listOptions.id);
+          if (scope.listOptions.duplicate) {
+            duplicateListData = controllers[0].getDuplicateListData(scope.listOptions.id);
             if (duplicateListData) {
               duplicateListData.setLimits(limit);
               duplicateListData.updateUI();

frontend/core/src/main/webapp/static/app/focus/focusTasks.html
@@ -326,7 +326,8 @@
         swiper-slide-duplicate="true">
           <div class="container-content" ng-class="daySlides[2].pastDate && 'past-date'"
             list="getTasksArray('date', {date: daySlides[2].info, pastDate: daySlides[2].pastDate})"
-            list-options="{isBounded: isAgendaVisible, registerModeChanged: registerAgendaVisibilityChangedCallback,
+            list-options="{hideItemDefault: true, isBounded: isAgendaVisible,
+            registerModeChanged: registerAgendaVisibilityChangedCallback,
             id: 'focus/tasks/rightDuplicate', scrollInactiveTop: true, duplicate: 'focus/tasks/right'}">
 
             <div ng-hide="datepickerVisible" class="group-mixed heading">
@@ -375,7 +376,8 @@
         <div class="swiper-slide" swiper-slide="focus/tasks/left" slide-index="1" swiper-slide-loop-start="true">
           <div class="container-content" ng-class="daySlides[0].pastDate && 'past-date'"
             list="getTasksArray('date', {date: daySlides[0].info, pastDate: daySlides[0].pastDate})"
-            list-options="{isBounded: isAgendaVisible, registerModeChanged: registerAgendaVisibilityChangedCallback,
+            list-options="{hideItemDefault: true, isBounded: isAgendaVisible,
+            registerModeChanged: registerAgendaVisibilityChangedCallback,
             id: 'focus/tasks/left', scrollInactiveTop: true, duplicate: 'focus/tasks/leftDuplicate'}">
 
             <div ng-hide="datepickerVisible" class="group-mixed heading">
@@ -424,7 +426,8 @@
         <div class="swiper-slide" swiper-slide="focus/tasks/middle" slide-index="2">
           <div class="container-content" ng-class="daySlides[1].pastDate && 'past-date'"
             list="getTasksArray('date', {date: daySlides[1].info, pastDate: daySlides[1].pastDate})"
-            list-options="{isBounded: isAgendaVisible, registerModeChanged: registerAgendaVisibilityChangedCallback,
+            list-options="{hideItemDefault: true, isBounded: isAgendaVisible,
+            registerModeChanged: registerAgendaVisibilityChangedCallback,
             id: 'focus/tasks/middle', scrollInactiveTop: true}">
 
             <div ng-hide="datepickerVisible" class="group-mixed heading">
@@ -473,7 +476,8 @@
         <div class="swiper-slide" swiper-slide="focus/tasks/right" slide-index="3">
           <div class="container-content" ng-class="daySlides[2].pastDate && 'past-date'"
             list="getTasksArray('date', {date: daySlides[2].info, pastDate: daySlides[2].pastDate})"
-            list-options="{isBounded: isAgendaVisible, registerModeChanged: registerAgendaVisibilityChangedCallback,
+            list-options="{hideItemDefault: true, isBounded: isAgendaVisible,
+            registerModeChanged: registerAgendaVisibilityChangedCallback,
             id: 'focus/tasks/right', scrollInactiveTop: true, duplicate: 'focus/tasks/rightDuplicate'}">
 
             <div ng-hide="datepickerVisible" class="group-mixed heading">
@@ -522,7 +526,8 @@
         <div class="swiper-slide" swiper-slide="focus/tasks/leftDuplicate" slide-index="4" swiper-slide-duplicate="true">
           <div class="container-content" ng-class="daySlides[0].pastDate && 'past-date'"
             list="getTasksArray('date', {date: daySlides[0].info, pastDate: daySlides[0].pastDate})"
-            list-options="{isBounded: isAgendaVisible, registerModeChanged: registerAgendaVisibilityChangedCallback,
+            list-options="{hideItemDefault: true, isBounded: isAgendaVisible,
+            registerModeChanged: registerAgendaVisibilityChangedCallback,
             id: 'focus/tasks/leftDuplicate', scrollInactiveTop: true, duplicate: 'focus/tasks/left'}">
 
             <div ng-hide="datepickerVisible" class="group-mixed heading">

frontend/core/src/main/webapp/static/app/lists/listTasks.html
@@ -1,6 +1,7 @@
 <div class="container-content--outer" list-container drawer-handle="left"
   drawer-handle-register-activate="registerFeatureActivateCallback(activate, 'list', 'listTasks')">
-  <section class="container-content" list="getTasksArray('list', {uuid: list.trans.uuid})">
+  <section class="container-content" list="getTasksArray('list', {uuid: list.trans.uuid})"
+    list-options="{hideItemDefault: true}">
     <div class="group-mixed heading">
       <div class="group-mixed--middle">
         <h2 class="group-heading justified">

frontend/core/src/main/webapp/static/app/tasks/tasksAll.html
@@ -1,6 +1,7 @@
 <div class="container-content--outer" list-container drawer-handle="left"
   drawer-handle-register-activate="registerFeatureActivateCallback(activate, 'tasks', 'tasksAll')">
-  <section class="container-content" list="getTasksArray('all')" list-recent>
+  <section class="container-content" list="getTasksArray('all')" list-recent
+    list-options="{hideItemDefault: true}">
 
     <div ng-show="!onboardingInProgress || isOnboardingListItemAddActive()">
       <div class="group-mixed heading">

frontend/core/src/main/webapp/static/app/tasks/tasksContext.html
@@ -1,5 +1,6 @@
 <div class="container-content--outer" list-container>
-  <section class="container-content" list="getTasksArray('context', {id: context && context.trans.uuid})">
+  <section class="container-content" list="getTasksArray('context', {id: context && context.trans.uuid})"
+    list-options="{hideItemDefault: true}">
     <div class="group-mixed heading vertically-centered">
       <div class="group-mixed--middle">
         <h2 class="group-heading justified">