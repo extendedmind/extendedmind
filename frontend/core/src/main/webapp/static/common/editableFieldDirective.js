extendedmind/extendedmind

commit 48829e101e7d6ee172fce280b29ebf5dbdca2f44

Fixed nonworking first click on login button when the app is started from the login screen and set autofocus into the email input.

frontend/core/src/main/webapp/static/common/editableFieldDirective.js
@@ -37,6 +37,9 @@
       if (attrs.editableFieldBlur) {
         var blurCallback = $parse(attrs.editableFieldBlur).bind(undefined, scope);
       }
+      if (attrs.editableFieldAutofocus) {
+        focusElement();
+      }
 
       function focusElement() {
         function doFocusElement(){

frontend/core/src/main/webapp/static/app/entry/EntryController.js
@@ -23,9 +23,9 @@ function EntryController($http, $location, $rootScope, $routeParams, $scope,
 
   AnalyticsService.visitEntry('entry');
 
-  var directToLogin = false;
+  $scope.directToLogin = false;
   function initializeLogin(){
-    directToLogin = true;
+    $scope.directToLogin = true;
     $scope.entryState = 'login';
     $scope.user = {};
     SwiperService.setInitialSlidePath('entry', 'entry/main');
@@ -89,11 +89,11 @@ function EntryController($http, $location, $rootScope, $routeParams, $scope,
   }
 
   $scope.isHomeSlideEnabled = function() {
-    return !directToLogin;
+    return !$scope.directToLogin;
   };
 
   $scope.isEntryInputAutoFocusDisabled = function() {
-    return directToLogin;
+    return $scope.directToLogin;
   };
 
   var entryEmailMainInputFocusCallbackFunction;

frontend/core/src/main/webapp/static/app/entry/entryMain.html
@@ -15,7 +15,8 @@
     <div class="row-form">
       <input type="email" name="email" ng-model="user.username"
       placeholder="enter email&#8230;" maxlength="254" editable-field="getEditableFieldSticky()"
-      editable-field-register-callbacks="registerEntryMainEmailInputCallbacks(focus, blur)" required />
+      editable-field-autofocus="{{directToLogin}}"
+      editable-field-register-callbacks="registerEntryMainEmailInputCallbacks(focus, blur)" required/>
     </div>
     <div class="group-error row-form">
       <div ng-if="loginForm.email.$touched" ng-messages="loginForm.email.$error">
@@ -38,8 +39,7 @@
       <span ng-if="entryOffline" class="text-error">can't connect to server. are you online?</span>
     </div>
   </section>
-  <footer class="container-content--after group-btn"
-    editable-field-container="{{isEntryInputAutoFocusDisabled() ? '' : 'auto'}}">
+  <footer class="container-content--after group-btn" editable-field-container="auto">
     <button type="submit" class="btn-action" ng-disabled="!loginForm.$valid || loggingIn" ng-click="logIn()">
       <span class="text-btn" ng-bind="loggingIn && 'logging in' || 'done'"></span>
     </button>

commit b99042d4da14dda4e779a1c1c3f87ff5532539ac

Fixed swiper layout broken in 48829e101e7d6ee172fce280b29ebf5dbdca2f44 when going to login from main entry slide. Autofocus take effect only when app is started from the login screen, for real this time :)

frontend/core/src/main/webapp/static/common/editableFieldDirective.js
@@ -37,7 +37,7 @@
       if (attrs.editableFieldBlur) {
         var blurCallback = $parse(attrs.editableFieldBlur).bind(undefined, scope);
       }
-      if (attrs.editableFieldAutofocus) {
+      if (attrs.editableFieldAutofocus && $parse(attrs.editableFieldAutofocus)(scope)) {
         focusElement();
       }
 