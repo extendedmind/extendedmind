30ab28294b9e0f6371ceed8c87fb8f7172b4cedb
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2015-06-12 11:52:35 +0300

    Fixed isValidTokens return value

--- backend/src/main/scala/org/extendedmind/db/SecurityDatabase.scala
+++ backend/src/main/scala/org/extendedmind/db/SecurityDatabase.scala
@@ -41,19 +41,19 @@ import org.extendedmind.domain.SignUp
 import org.extendedmind.domain.UserPreferences
 
 trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
-  
+
   // Token is valid for twelve hours
   val TOKEN_DURATION: Long = 43200000
-  
+
   // If rememberMe is set, the token can be replaced for 7 days
   val TOKEN_REPLACEABLE: Long = 604800000
 
   // If rememberMe and extended are set, the token can be replaced for 90 days
   val TOKEN_REPLACEABLE_EXTENDED: Long = 7776000000l;
-  
+
   // Password reset is valid for one hour
   val PASSWORD_RESET_DURATION: Long = 3600000
-  
+
   // PUBLIC
 
   def generateToken(email: String, attemptedPassword: String, payload: Option[AuthenticatePayload]): Response[SecurityContext] = {
@@ -93,17 +93,17 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
 
   def authenticate(email: String, attemptedPassword: String): Response[SecurityContext] = {
     withTx{
-      implicit neo4j => 
+      implicit neo4j =>
         for {
           user <- getUserNode(email).right
           sc <- validatePassword(user, attemptedPassword).right
         } yield sc
     }
   }
-  
+
   def authenticate(email: String, attemptedPassword: String, ownerUUID: Option[UUID]): Response[SecurityContext] = {
     withTx{
-      implicit neo4j => 
+      implicit neo4j =>
         for {
           user <- getUserNode(email).right
           foreignOwnerUUID <- Right(getForeignOwnerUUID(user, ownerUUID)).right
@@ -113,9 +113,9 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
   }
 
   def authenticate(token: String, ownerUUID: Option[UUID], shareable: Boolean = false)(implicit log: LoggingContext): Response[SecurityContext] = {
-    val currentTime = System.currentTimeMillis()    
+    val currentTime = System.currentTimeMillis()
     withTx{
-      implicit neo4j => 
+      implicit neo4j =>
         for {
           token <- Token.decryptToken(token).right
           tokenNode <- getTokenNode(token).right
@@ -126,10 +126,10 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
         } yield sc
     }
   }
-  
+
   def validateEmailUniqueness(email: String): Response[Boolean] = {
     withTx{
-      implicit neo4j => 
+      implicit neo4j =>
         val userNodeList = findNodesByLabelAndProperty(OwnerLabel.USER, "email", email).toList
         if (!userNodeList.isEmpty){
           return fail(INVALID_PARAMETER, ERR_BASE_EMAIL_EXISTS, "User already exists with given email: " + email)
@@ -137,10 +137,10 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
         Right(true)
     }
   }
-  
+
   def logout(encryptedToken: String): Response[SecurityContext] = {
     withTx{
-      implicit neo4j => 
+      implicit neo4j =>
         for {
           token <- Token.decryptToken(encryptedToken).right
           tokenNode <- getTokenNode(token).right
@@ -150,17 +150,17 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
         } yield sc
     }
   }
-  
+
   def destroyTokens(userUUID: UUID): Response[CountResult] = {
     withTx{
-      implicit neo4j => 
+      implicit neo4j =>
         for {
           userNode <- getNode(userUUID, OwnerLabel.USER).right
           result <- destroyTokens(userNode, None).right
         } yield result
     }
   }
-  
+
   def changePassword(userUUID: UUID, newPassword: String): Response[Unit] = {
     withTx{
       implicit neo4j =>
@@ -170,7 +170,7 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
         } yield result
     }
   }
-  
+
   def changeUserType(userUUID: UUID, userType: Integer): Response[SetResult] = {
     for {
       userNode <- changeUserNodeType(userUUID, userType).right
@@ -185,12 +185,12 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
         val count = tokens.size
         tokens.foreach { token => {
          token.getRelationships() foreach {relationship => relationship.delete}
-         token.delete() 
+         token.delete()
         }}
         Right(CountResult(count))
     }
   }
-      
+
   def savePasswordResetInformation(userUUID: UUID, resetCode: Long, resetCodeValid: Long, emailId: String): Response[Unit] = {
     withTx {
       implicit neo4j =>
@@ -200,7 +200,7 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
 	    } yield result
     }
   }
-    
+
   def getPasswordResetExpires(code: Long, email: String): Response[Long] = {
     withTx {
       implicit neo4j =>
@@ -210,14 +210,14 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
 	    } yield result
     }
   }
-  
+
   def resetPassword(code: Long, signUp: SignUp): Response[SetResult] = {
     for {
       userNode <- resetPasswordNode(code, signUp).right
       result <- Right(getSetResult(userNode, true)).right
     } yield result
   }
-  
+
   def verifyEmail(code: Long, email: String): Response[SetResult] = {
     for {
       userNode <- verifyEmailNode(code, email).right
@@ -225,9 +225,9 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
     } yield result
   }
 
-  
+
   // PRIVATE
-  
+
   protected def getTokenNode(token: Token, acceptDeleted: Boolean = false)
         (implicit log: LoggingContext): Response[Node] = {
     val response = getNode("accessKey", token.accessKey: java.lang.Long, MainLabel.TOKEN, None, acceptDeleted)
@@ -237,7 +237,7 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
       response
     }
   }
-  
+
   protected def validateToken(tokenNode: Node, currentTime: Long): Response[Unit] = {
     if (tokenNode.hasProperty("expires")) {
       val tokenValid = tokenNode.getProperty("expires").asInstanceOf[Long];
@@ -246,7 +246,7 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
       } else fail(INVALID_AUTHENTICATION, ERR_BASE_TOKEN_EXPIRED, "Token has expired")
     } else fail(INTERNAL_SERVER_ERROR, ERR_BASE_TOKEN_MISSING_EXPIRED, "Token " + tokenNode.getId() + " is missing expired property")
   }
-  
+
   protected def validateTokenReplacable(tokenNode: Node, currentTime: Long)(implicit neo4j: DatabaseService): Response[Unit] = {
     if (tokenNode.hasProperty("replaceable")) {
       val replaceable = tokenNode.getProperty("replaceable").asInstanceOf[Long];
@@ -255,8 +255,8 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
       } else fail(INVALID_AUTHENTICATION, ERR_BASE_TOKEN_NO_LONGER_REPLACEABLE, "Token no longer replaceable")
     } else fail(INVALID_PARAMETER, ERR_BASE_TOKEN_NOT_REPLACEABLE, "Token not replaceable")
   }
-  
-  protected def validateSubscription(sc: SecurityContext)(implicit neo4j: DatabaseService): Response[Unit] = {      
+
+  protected def validateSubscription(sc: SecurityContext)(implicit neo4j: DatabaseService): Response[Unit] = {
     if (settings.signUpMode == MODE_NORMAL
         && (sc.user.hasLabel(UserLabel.NORMAL) || sc.user.hasLabel(UserLabel.BETA))
         && !sc.user.hasLabel(SubscriptionLabel.PREMIUM)
@@ -266,10 +266,10 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
       Right(Unit)
     }
   }
-  
+
   protected def clearExtraLogins(userNode: Node, tokenNode: Node)(implicit neo4j: DatabaseService): Response[CountResult] = {
     if (settings.signUpMode == MODE_NORMAL
-        && (userNode.hasLabel(UserLabel.NORMAL) || userNode.hasLabel(UserLabel.BETA)) 
+        && (userNode.hasLabel(UserLabel.NORMAL) || userNode.hasLabel(UserLabel.BETA))
         && !userNode.hasLabel(SubscriptionLabel.PREMIUM)){
       // When a normal or beta user who does not have a valid subscription swaps token,
       // all other tokens are deleted
@@ -292,10 +292,10 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
             false
           }
         }
-      })) 
+      }))
     }
   }
-  
+
   protected def hasValidOrReplaceableToken(userNode: Node)(implicit neo4j: DatabaseService): Boolean = {
     val tokenTraversal = neo4j.gds.traversalDescription()
       .breadthFirst()
@@ -306,16 +306,16 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
       .traverse(userNode)
     val tokenList = tokenTraversal.nodes().toList
     val currentTime = System.currentTimeMillis
-    tokenList.forall(tokenNode => {
+    tokenList.find(tokenNode => {
       val authInfo = getAuthenticationInfo(tokenNode)
       if (authInfo._2 >= currentTime || (authInfo._3.isDefined && authInfo._3.get >= currentTime)){
         true
       }else{
         false
       }
-    })
+    }).isDefined
   }
-  
+
   protected def createNewAccessKey(tokenNode: Node, sc: SecurityContext, payload: Option[AuthenticatePayload])(implicit neo4j: DatabaseService): SecurityContext = {
     // Make new token and set properties to the token node
     val token = Token(sc.userUUID)
@@ -341,11 +341,11 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
     tokenNode --> SecurityRelationship.IDS --> userNode
     tokenInfo
   }
-  
+
   protected def destroyTokens(userNode: Node)(implicit neo4j: DatabaseService): Response[CountResult] = {
     destroyTokens(userNode, None)
   }
-  
+
   protected def destroyTokens(userNode: Node, destroyCondition: Option[(Node => Boolean)])(implicit neo4j: DatabaseService): Response[CountResult] = {
     val tokenTraversal = neo4j.gds.traversalDescription()
       .breadthFirst()
@@ -364,7 +364,7 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
     })
     Right(CountResult(deleteCount))
   }
-  
+
   protected def destroyToken(tokenNode: Node)(implicit neo4j: DatabaseService) {
     // Remove all relationships
     val relationShipList = tokenNode.getRelationships().toList
@@ -384,7 +384,7 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
 					      currentTime + TOKEN_REPLACEABLE_EXTENDED
 					    else
 					      currentTime + TOKEN_REPLACEABLE
-					    
+
       tokenNode.setProperty("replaceable", replaceable)
       return (currentTime, expires, Some(replaceable))
     }else if (tokenNode.hasProperty("replaceable")){
@@ -407,14 +407,14 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
       sc <- Right(getSecurityContext(user)).right
     } yield sc
   }
-  
+
   private def validatePassword(user: Node, attemptedPassword: String, collectiveUUID: Option[UUID])(implicit neo4j: DatabaseService): Response[SecurityContext] = {
     for{
       validPassword <- validatePassword(attemptedPassword, getStoredPassword(user)).right
       sc <- getLimitedSecurityContext(user, collectiveUUID).right
     } yield sc
   }
-  
+
   private def validatePassword(attemptedPassword: String, storedPassword: Password): Response[Boolean] = {
     // Check password
     if (PasswordService.authenticate(attemptedPassword, storedPassword)) {
@@ -423,7 +423,7 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
       fail(INVALID_PARAMETER, ERR_BASE_INVALID_PASSWORD, "Invalid password")
     }
   }
-    
+
   private def getForeignOwnerUUID(user: Node, ownerUUID: Option[UUID]): Option[UUID] = {
     if (ownerUUID.isDefined && (getUUID(user) == ownerUUID.get)) None
     else ownerUUID
@@ -432,7 +432,7 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
   private def getSecurityContext(user: Node)(implicit neo4j: DatabaseService): SecurityContext = {
     getCompleteSecurityContext(user, getUserType(user), getSubscription(user))
   }
-  
+
   private def getLimitedSecurityContext(user: Node, foreignOwnerUUID: Option[UUID], shareable: Boolean = false)(implicit neo4j: DatabaseService): Response[SecurityContext] = {
     getLimitedSecurityContextWithSubscription(user, getUserType(user), getSubscription(user), foreignOwnerUUID, shareable)
   }
@@ -451,7 +451,7 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
     sc.user = user
     sc
   }
-  
+
   private def getLimitedSecurityContextWithSubscription(user: Node, userType: Byte, subscription: Option[String],
                                         foreignOwnerUUID: Option[UUID], shareable: Boolean = false)
                                        (implicit neo4j: DatabaseService): Response[SecurityContext] = {
@@ -459,13 +459,13 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
     if (foreignOwnerUUID.isEmpty){
       val sc = getSecurityContextSkeleton(user, userType, subscription)
       sc.user = user
-      Right(sc)      
+      Right(sc)
     }else{
       val sharingRelationshipsList = sharingTraversalDescription.traverse(user).relationships().toList
       val collectives = {
         // See if one of the collectives the user has access to is the foreign UUID
         val collectiveRelationshipList = sharingRelationshipsList filter {relationship => {
-          relationship.getEndNode.hasLabel(OwnerLabel.COLLECTIVE) && getUUID(relationship.getEndNode) == foreignOwnerUUID.get 
+          relationship.getEndNode.hasLabel(OwnerLabel.COLLECTIVE) && getUUID(relationship.getEndNode) == foreignOwnerUUID.get
         }}
         if (collectiveRelationshipList.size > 0){
           getCollectiveAccess(collectiveRelationshipList)
@@ -496,11 +496,11 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
       val sc = getSecurityContextSkeleton(user, userType, subscription).copy(
         collectives = collectives,
         sharedLists = sharedLists)
-      sc.user = user      
+      sc.user = user
       Right(sc)
     }
   }
-  
+
   private def getAuthenticationInfo(tokenNode: Node): (Long, Long, Option[Long]) = {
     val authenticated = tokenNode.getProperty("modified").asInstanceOf[Long]
     val expires = tokenNode.getProperty("expires").asInstanceOf[Long]
@@ -527,7 +527,7 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
       getUserPreferences(user)
     )
   }
-  
+
   private def changeUserNodeType(userUUID: UUID, userType: Integer): Response[Node] = {
     withTx {
       implicit neo4j =>
@@ -537,10 +537,10 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
         } yield userNode
     }
   }
-  
+
   private def changeUserType(userNode: Node, userType: Integer)(implicit neo4j: DatabaseService) = {
     val currentUserType = getUserType(userNode)
-    
+
     if (userType.intValue() != currentUserType){
       if (userType == Token.ADMIN.intValue){
         setUserTypeLabel(userNode, UserLabel.ADMIN)
@@ -552,7 +552,7 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
         removeUserTypeLabel(userNode)      }
     }
   }
-  
+
   private def getUserType(user: Node): Byte = {
     val labels = user.getLabels().asScala
     if (labels.find(p => p.name() == "ADMIN").isDefined)
@@ -564,17 +564,17 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
     else
       Token.NORMAL
   }
-  
+
   private def getSubscription(user: Node): Option[String] = {
     if (user.hasLabel(SubscriptionLabel.PREMIUM)) Some("premium")
     else None
   }
-  
+
   private def setUserTypeLabel(userNode: Node, label: Label)(implicit neo4j: DatabaseService){
     removeUserTypeLabel(userNode)
     userNode.addLabel(label)
   }
-  
+
   private def removeUserTypeLabel(userNode: Node)(implicit neo4j: DatabaseService){
     // Remove existing user type
     if (userNode.hasLabel(UserLabel.ADMIN))
@@ -584,26 +584,26 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
     else if (userNode.hasLabel(UserLabel.BETA))
       userNode.removeLabel(UserLabel.BETA)
   }
-  
+
   private def savePasswordResetInformation(userNode: Node, resetCode: Long, resetCodeExpires: Long, emailId: String)(implicit neo4j: DatabaseService){
     userNode.setProperty("passwordResetCode", resetCode)
     userNode.setProperty("passwordResetCodeExpires", resetCodeExpires)
     userNode.setProperty("passwordResetEmailId", emailId)
   }
-  
+
   private def getPasswordResetExpires(code: Long,userNode: Node)(implicit neo4j: DatabaseService): Response[Long] = {
     if (userNode.hasProperty("passwordResetCode") && userNode.getProperty("passwordResetCode").asInstanceOf[Long] == code){
       val currentTime = System.currentTimeMillis
       if (userNode.hasProperty("passwordResetCodeExpires") && userNode.getProperty("passwordResetCodeExpires").asInstanceOf[Long] > currentTime){
         Right(userNode.getProperty("passwordResetCodeExpires").asInstanceOf[Long])
       }else{
-        fail(INVALID_PARAMETER, ERR_BASE_PASSWORD_NOT_RESETABLE_ANYMORE, "Password not resetable anymore")       
+        fail(INVALID_PARAMETER, ERR_BASE_PASSWORD_NOT_RESETABLE_ANYMORE, "Password not resetable anymore")
       }
     }else{
       fail(INVALID_PARAMETER, ERR_BASE_PASSWORD_NOT_RESETABLE, "Password not resetable")
     }
   }
-  
+
   private def resetPasswordNode(code: Long, signUp: SignUp): Response[Node] = {
     withTx {
       implicit neo =>
@@ -615,7 +615,7 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
         } yield userNode
     }
   }
-  
+
   private def verifyEmailNode(code: Long, email: String): Response[Node] = {
     withTx {
       implicit neo =>
@@ -625,7 +625,7 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
         } yield userNode
     }
   }
-  
+
   private def verifyEmail(code: Long, userNode: Node)(implicit neo4j: DatabaseService): Response[Long] = {
     if (userNode.hasProperty("emailVerificationCode")){
       if (userNode.getProperty("emailVerificationCode").asInstanceOf[Long] == code){
@@ -639,9 +639,9 @@ trait SecurityDatabase extends AbstractGraphDatabase with UserDatabase {
       fail(INVALID_PARAMETER, ERR_BASE_PASSWORD_NOT_VERIFIABLE, "email not verifiable")
     }
   }
-  
+
   private def clearPasswordResetExpires(userNode: Node)(implicit neo4j: DatabaseService){
     if (userNode.hasProperty("passwordResetCodeExpires")) userNode.removeProperty("passwordResetCodeExpires")
   }
-  
+
 }
