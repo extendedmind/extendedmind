01c9e2d1d18520229ba99669fbdbc4e7738c18ff
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2015-06-12 11:58:26 +0300

    Some more work on backend docker, now creates backend container with the right libraries

--- backend/src/main/command/em-backend.sh
+++ backend/src/main/command/em-backend.sh
@@ -3,6 +3,5 @@ if [ $# -eq 0 ]
   then
     echo "No arguments supplied"
 else
-  export LOGBACK_CONFIG_FILE_LOCATION=$2
-  java -Dconfig.file=$1 -Dlogback.configurationFile$
+  java -Dconfig.file=$1 -Dlogback.configurationFile=$2 -cp "/usr/src/extendedmind/lib/*" org.extendedmind.Server
 fi
