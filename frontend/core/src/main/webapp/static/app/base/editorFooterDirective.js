extendedmind/extendedmind

commit 05807e7e7352f748f870e584a470454850d7f3cb

Updated AngularJS from 1.3.8 to 1.4.1 and fixed all known breaking changes. NOTE: Fast toggling editor footer (keywords/later) sometimes opens the footer without animation.

frontend/core/src/main/webapp/static/app/base/editorFooterDirective.js
@@ -15,11 +15,11 @@
  'use strict';
 
  /*
- * WHAT'S NOT WORKING:
+ * NOTE: what's not working
  *  - enter omnibar text, enter keywords search, unfocus, clear omnibar text, focus keywords search
  */
 
- function editorFooterDirective($animate, $document, $parse, $rootScope, $timeout, packaging) {
+ function editorFooterDirective($animateCss, $document, $parse, $rootScope, $timeout, packaging) {
   var footerHeight = 44;  // NOTE: Match with @grid-vertical in LESS.
   return {
     restrict: 'A',
@@ -179,28 +179,21 @@
         element[0].style.height = footerHeight + 'px';
         element[0].style.bottom = 0;
 
-        // Remove animation class from opening.
-        element[0].classList.remove('animate-editor-footer-open');
-
         // Start shrink animation.
-        shrinkPromise = $animate.addClass(element, 'animate-editor-footer-close', {
+        shrinkPromise = $animateCss(element, {
           // Remove flicker during open caused by leftover transform property in element style from previous
           // position.
           to: {
             transform: 'translate3d(0, 0, 0)' // Reset element style.
-          }
-        }).then(function() {
-          if (!$rootScope.$$phase && !scope.$$phase)
-            scope.$apply(function(){
-              scope.footerExpanded = false;   // Remove expandable DOM.
-              scope.footerExpandOpen = false; // Footer is now closed.
-              scope.footerExpandedToMaxHeight = false;  // Reset variable.
-            });
-          else {
-            scope.footerExpanded = false;   // Remove expandable DOM.
-            scope.footerExpandOpen = false; // Footer is now closed.
-            scope.footerExpandedToMaxHeight = false;  // Reset variable.
-          }
+          },
+          easing: $rootScope.ANIMATION_TIMING_FUNCTION,
+          duration: $rootScope.EDITOR_FOOTER_ANIMATION_SPEED
+        });
+
+        shrinkPromise.start().done(function() {
+          scope.footerExpanded = false;   // Remove expandable DOM.
+          scope.footerExpandOpen = false; // Footer is now closed.
+          scope.footerExpandedToMaxHeight = false;  // Reset variable.
           shrinkPromise = undefined;
           resetStates = {}; // Clear reset states
         });
@@ -215,42 +208,30 @@
         element[0].style.height = expandedHeight + footerHeight + 'px';
         element[0].style.bottom = -expandedHeight + 'px';
 
-        // Remove animation class from previous toggling.
-        element[0].classList.remove('animate-editor-footer-close');
-        element[0].classList.remove('animate-editor-footer-open');
-
-        var editorFooterOpenClass = 'animate-editor-footer-open';
-        if (noAnimation) {
-          // Prevent animation.
-          editorFooterOpenClass += ' no-animate';
-        }
-
-        expandPromise = $animate.addClass(element, editorFooterOpenClass, {
+        expandPromise = $animateCss(element, {
           from: {
             transform: 'translate3d(0, ' + -oldTranslateYPosition + 'px' + ', 0)'
           },
           to: {
             transform: 'translate3d(0, ' + -expandedHeight + 'px, 0)'
-          }
-        }).then(function() {
+          },
+          easing: $rootScope.ANIMATION_TIMING_FUNCTION,
+          duration: noAnimation ? 0 : $rootScope.EDITOR_FOOTER_ANIMATION_SPEED
+        });
+
+        expandPromise.start().done(function() {
           // Shrink promise exists if footer is shrinked before it is fully expanded. In that case,
           // this resolve callback is more like a rejection so let's do nothing here and let shrink promise
           // do its thing instead.
           if (!shrinkPromise) {
             // No shrink promise -> footer expanded.
 
-            scope.$apply(function() {
-              // Set padding-bottom for container to make content scrollable.
-              containerInfos.footerHeight = containerInfos.oldFooterHeight = expandedHeight + footerHeight;
-              contentElement.style.paddingBottom = containerInfos.footerHeight + 'px';
-              scope.footerExpandOpen = true;  // Footer is now opened.
-            });
+            // Set padding-bottom for container to make content scrollable.
+            containerInfos.footerHeight = containerInfos.oldFooterHeight = expandedHeight + footerHeight;
+            contentElement.style.paddingBottom = containerInfos.footerHeight + 'px';
+            scope.footerExpandOpen = true;  // Footer is now opened.
           }
           expandPromise = undefined;
-
-          if (noAnimation) {
-            element[0].classList.remove('no-animate');
-          }
         });
         // Need to digest ot be sure that class is applied
         if (!$rootScope.$$phase && !scope.$$phase){
@@ -467,5 +448,6 @@
     }
   };
 }
-editorFooterDirective['$inject'] = ['$animate', '$document', '$parse', '$rootScope', '$timeout', 'packaging'];
+editorFooterDirective['$inject'] = ['$animateCss', '$document', '$parse', '$rootScope', '$timeout',
+'packaging'];
 angular.module('em.base').directive('editorFooter', editorFooterDirective);

frontend/core/src/main/webapp/static/app/root/rootViewDirective.js
@@ -46,6 +46,8 @@
       // NOTE: These need to be in sync with their counterparts in LESS and/or JS.
       $rootScope.MENU_ANIMATION_SPEED = 200;
       $rootScope.EDITOR_ANIMATION_SPEED = 350;
+      $rootScope.EDITOR_FOOTER_ANIMATION_SPEED = 0.3;
+      $rootScope.ANIMATION_TIMING_FUNCTION = 'cubic-bezier(0.250, 0.460, 0.450, 0.940)';
       $rootScope.EDITOR_CLOSED_FAILSAFE_TIME = 2*$rootScope.EDITOR_ANIMATION_SPEED;
       $rootScope.CHECKBOX_CHECKING_ANIMATION_TIME = 3000;
       $rootScope.LIST_ITEM_LEAVE_ANIMATION_SPEED = 1000;

frontend/core/src/main/webapp/static/less/animations.less
@@ -314,35 +314,7 @@ input[type="checkbox"]:checked + label:before {
   opacity: 1;
 }
 
-@speed-animate-editor-footer-toggle: .3s;
-
-// 1. Animate editor to expanded position.
-.animate-editor-footer-open-add:not(.no-animate) {
-  -webkit-transition: -webkit-transform @speed-animate-editor-footer-toggle @timing-animate;
-  -moz-transition: -moz-transform @speed-animate-editor-footer-toggle @timing-animate;
-  -o-transition: -o-transform @speed-animate-editor-footer-toggle @timing-animate;
-  -ms-transition: -ms-transform @speed-animate-editor-footer-toggle @timing-animate;
-  transition: transform @speed-animate-editor-footer-toggle @timing-animate;
-}
-
-// 1. Override transform style applied directly into element by $animate.addClass options.
-//    NOTE: Do not use during ng-hide since it will use delay set here.
-.animate-editor-footer-close:not(.ng-hide-animate) {
-  -webkit-transition: -webkit-transform @speed-animate-editor-footer-toggle @timing-animate;
-  -moz-transition: -moz-transform @speed-animate-editor-footer-toggle @timing-animate;
-  -o-transition: -o-transform @speed-animate-editor-footer-toggle @timing-animate;
-  -ms-transition: -ms-transform @speed-animate-editor-footer-toggle @timing-animate;
-  transition: transform @speed-animate-editor-footer-toggle @timing-animate;
-
-  -webkit-transform: translate3d(0, 0, 0) !important; // [1]
-  -moz-transform: translate3d(0, 0, 0) !important; // [1]
-  -o-transform: translate3d(0, 0, 0) !important; // [1]
-  -ms-transform: translate3d(0, 0, 0) !important; // [1]
-  transform: translate3d(0, 0, 0) !important; // [1]
-}
-
 @speed-animation-editor: .35s;
-
 
 // 1. Darken container background with animation when editor is opening.
 //    Reset container backgroung with animation when editor is closing.

frontend/core/src/main/webapp/static/app/base/listItemDirective.js
@@ -96,21 +96,26 @@ angular.module('em.base').directive('listItem', listItemDirective);
 * See: https://docs.angularjs.org/api/ngAnimate
 */
 
-function listItemLeaveAnimation(UISessionService) {
+function listItemLeaveAnimation($animateCss, UISessionService) {
 
   return {
     /*
     *  Cancel animation when leave animation is locked.
     */
     leave: function(element, leaveDone) {
-      leaveDone();
-      if (!UISessionService.isAllowed('leaveAnimation')) {
-        // Second leaveDone call will cancel animation.
+      var runner = $animateCss(element, {
+        event: 'leave',
+        structural: true
+      }).start();
+
+      if (UISessionService.isAllowed('leaveAnimation')) {
+        runner.done(leaveDone);
+      } else {
+        runner.end();
         leaveDone();
-        return;
       }
     }
   };
 }
-listItemLeaveAnimation['$inject'] = ['UISessionService'];
+listItemLeaveAnimation['$inject'] = ['$animateCss', 'UISessionService'];
 angular.module('em.tasks').animation('.animate-list-item-leave', listItemLeaveAnimation);

frontend/core/src/main/webapp/static/common/calendarDirective.js
@@ -80,10 +80,8 @@
             // Add little animation to indicate the user that he/she succesfully clicked a date.
             $animate.addClass(activeDate.firstElementChild, 'animate-calendar-selected-date')
             .then(function() {
-              scope.$apply(function() {
-                // Good to return the date.
-                scope.returnDate({date: calendar.getDate()});
-              });
+              // Good to return the date.
+              scope.returnDate({date: calendar.getDate()});
             });
           });
         }

commit fd2ddfa3b5031c8081c9f5a3793951cad20e6fdd

Probably fixed fast toggling editor footer (keywords/later) sometimes opens the footer without animation which was broken since 05807e7e7352f748f870e584a470454850d7f3cb.

frontend/core/src/main/webapp/static/app/base/editorFooterDirective.js
@@ -183,14 +183,16 @@
         shrinkPromise = $animateCss(element, {
           // Remove flicker during open caused by leftover transform property in element style from previous
           // position.
+          addClass: 'animate-editor-footer-close',
+          removeClass: 'animate-editor-footer-open',
           to: {
             transform: 'translate3d(0, 0, 0)' // Reset element style.
           },
-          easing: $rootScope.ANIMATION_TIMING_FUNCTION,
-          duration: $rootScope.EDITOR_FOOTER_ANIMATION_SPEED
-        });
+          duration: true,
+          easing: true
+        }).start();
 
-        shrinkPromise.start().done(function() {
+        shrinkPromise.done(function() {
           scope.footerExpanded = false;   // Remove expandable DOM.
           scope.footerExpandOpen = false; // Footer is now closed.
           scope.footerExpandedToMaxHeight = false;  // Reset variable.
@@ -209,17 +211,19 @@
         element[0].style.bottom = -expandedHeight + 'px';
 
         expandPromise = $animateCss(element, {
+          addClass: 'animate-editor-footer-open',
+          removeClass: 'animate-editor-footer-close',
           from: {
             transform: 'translate3d(0, ' + -oldTranslateYPosition + 'px' + ', 0)'
           },
           to: {
             transform: 'translate3d(0, ' + -expandedHeight + 'px, 0)'
           },
-          easing: $rootScope.ANIMATION_TIMING_FUNCTION,
-          duration: noAnimation ? 0 : $rootScope.EDITOR_FOOTER_ANIMATION_SPEED
-        });
+          duration: noAnimation ? 0 : true,
+          easing: true
+        }).start();
 
-        expandPromise.start().done(function() {
+        expandPromise.done(function() {
           // Shrink promise exists if footer is shrinked before it is fully expanded. In that case,
           // this resolve callback is more like a rejection so let's do nothing here and let shrink promise
           // do its thing instead.

frontend/core/src/main/webapp/static/app/root/rootViewDirective.js
@@ -46,8 +46,6 @@
       // NOTE: These need to be in sync with their counterparts in LESS and/or JS.
       $rootScope.MENU_ANIMATION_SPEED = 200;
       $rootScope.EDITOR_ANIMATION_SPEED = 350;
-      $rootScope.EDITOR_FOOTER_ANIMATION_SPEED = 0.3;
-      $rootScope.ANIMATION_TIMING_FUNCTION = 'cubic-bezier(0.250, 0.460, 0.450, 0.940)';
       $rootScope.EDITOR_CLOSED_FAILSAFE_TIME = 2*$rootScope.EDITOR_ANIMATION_SPEED;
       $rootScope.CHECKBOX_CHECKING_ANIMATION_TIME = 3000;
       $rootScope.LIST_ITEM_LEAVE_ANIMATION_SPEED = 1000;

frontend/core/src/main/webapp/static/less/animations.less
@@ -852,3 +852,21 @@ a.link-menu.onboarding:not(.highlighted) span {
   65% {transform: translateY(0);}
   100% {transform: translateY(0);}
 }
+
+@speed-animate-editor-footer-toggle: .3s;
+
+.animate-editor-footer-open {
+  -webkit-transition: -webkit-transform @speed-animate-editor-footer-toggle @timing-animate;
+  -moz-transition: -moz-transform @speed-animate-editor-footer-toggle @timing-animate;
+  -o-transition: -o-transform @speed-animate-editor-footer-toggle @timing-animate;
+  -ms-transition: -ms-transform @speed-animate-editor-footer-toggle @timing-animate;
+  transition: transform @speed-animate-editor-footer-toggle @timing-animate;
+}
+
+.animate-editor-footer-close {
+  -webkit-transition: -webkit-transform @speed-animate-editor-footer-toggle @timing-animate;
+  -moz-transition: -moz-transform @speed-animate-editor-footer-toggle @timing-animate;
+  -o-transition: -o-transform @speed-animate-editor-footer-toggle @timing-animate;
+  -ms-transition: -ms-transform @speed-animate-editor-footer-toggle @timing-animate;
+  transition: transform @speed-animate-editor-footer-toggle @timing-animate;
+}