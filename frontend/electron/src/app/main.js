bd45e46e2505c570304e5aae15ede9a3a12b6fc6
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2015-09-23 17:41:03 +0300

    Added View menu to application which includes developer tools

--- frontend/electron/src/app/main.js
+++ frontend/electron/src/app/main.js
@@ -3,9 +3,6 @@ var BrowserWindow = require('browser-window');  // Module to create native brows
 var Menu = require('menu');
 var MenuItem = require('menu-item');
 
-// Report crashes to our server.
-require('crash-reporter').start();
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is GCed.
 var mainWindow = null;
@@ -90,6 +87,45 @@ app.on('ready', function() {
         accelerator: 'Command+A',
         selector: 'selectAll:'
       },
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click: function(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.reload();
+        }
+      },
+      {
+        label: 'Toggle Full Screen',
+        accelerator: (function() {
+          if (process.platform == 'darwin')
+            return 'Ctrl+Command+F';
+          else
+            return 'F11';
+        })(),
+        click: function(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+        }
+      },
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: (function() {
+          if (process.platform == 'darwin')
+            return 'Alt+Command+I';
+          else
+            return 'Ctrl+Shift+I';
+        })(),
+        click: function(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.toggleDevTools();
+        }
+      }
     ]
   }];
 
