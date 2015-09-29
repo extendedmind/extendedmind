92c3c5770990aa1cec4b413fde735d9309cb880d
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2015-09-29 10:59:57 +0300

    Fixed common collective not editable for admin users due to data arrays not being initialized

--- frontend/app/src/static/app/base/ItemLikeService.js
+++ frontend/app/src/static/app/base/ItemLikeService.js
@@ -270,7 +270,8 @@
           if (itemsMap.hasOwnProperty(ownerUUID)){
             if (ownerUUID !== userUUID){
               // Not the user, check if not in collectives
-              if (collectives && collectives[ownerUUID] && collectives[ownerUUID][2] === false){
+              if (collectives && collectives[ownerUUID] &&
+                  (collectives[ownerUUID][2] === false || UserSessionService.getUserType() === 0)){
                 BackendClientService.notifyOwnerAccess(ownerUUID, collectives[ownerUUID][1]);
               }else{
                 // Not in collectives, are they in shared lists?
