5cb82ebde6128d709eefaa26b93f451e69fac527
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2015-10-07 13:37:18 +0300

    Create a share extension for iOS as first example of native code mixed with Cordova.

--- frontend/cordova/app/hooks/after_platform_add/add_plugins.js
+++ frontend/cordova/app/hooks/after_platform_add/add_plugins.js
@@ -3,19 +3,19 @@
 //this hook installs all your plugins
 
 var pluginlist = [
-    "https://github.com/extendedmind/ionic-plugin-keyboard",
-    "cordova-plugin-vibration",
-    "cordova-plugin-media",
-    "https://github.com/apache/cordova-plugin-splashscreen.git",
+    "https://github.com/extendedmind/ionic-plugin-keyboard.git",
+    "cordova-plugin-vibration@1.2.0",
+    "cordova-plugin-media@1.0.1",
+    "https://github.com/apache/cordova-plugin-splashscreen.git#r2.1.0",
     "https://github.com/extendedmind/Calendar-PhoneGap-Plugin.git",
-    "https://github.com/extendedmind/cordova-plugin-local-notifications",
-    "https://github.com/leohenning/KeepScreenOnPlugin",
-    "https://github.com/extendedmind/cordova-webintent",
-    "https://github.com/extendedmind/phonegap-backbutton-plugin",
-    "cordova-plugin-device",
+    "https://github.com/extendedmind/cordova-plugin-local-notifications.git",
+    "https://github.com/extendedmind/KeepScreenOnPlugin.git",
+    "https://github.com/extendedmind/cordova-webintent.git",
+    "https://github.com/extendedmind/phonegap-backbutton-plugin.git",
+    "cordova-plugin-device@1.0.1",
     "cordova-plugin-whitelist@1.0.0",
-    "cordova-plugin-inappbrowser",
-    "cordova-plugin-globalization"
+    "cordova-plugin-inappbrowser@1.0.1",
+    "cordova-plugin-globalization@1.0.1"
 ];
 
 // no need to configure below
--- frontend/cordova/prepare-ios.sh
+++ frontend/cordova/prepare-ios.sh
@@ -2,7 +2,7 @@
 
 source setup-environment.sh
 
-if [ -d "app/platforms/ios" ]
+if [ -d "app/platforms/ios/extmd" ]
 then
   echo "iOS already prepared, skipping."
 else
@@ -13,6 +13,14 @@ else
   mkdir -p platforms
   mkdir -p plugins
 
+  # move existing iOS source code temporarily elsewhere
+  mv platforms/ios platforms/ios-tmp
+
   # add iOS platform
   cordova platform add ios
+
+  # copy sourced code back
+  cp -R platforms/ios-tmp/* platforms/ios
+  rm -rf platforms/ios-tmp
+
 fi
