d2acfcf8ec4fce315283db38e30d69b5be79cc7e
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2016-05-05 20:34:21 +0300

    Totally untested version MariaDB and Piwik Docker instead of Cube and Mongodb.

--- frontend/site/docker/nginx-run.sh
+++ frontend/site/docker/nginx-run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cp /etc/confd/templates/nginx/nginx.conf /etc/nginx/nginx.conf
 sed -i 's/BACKEND_IP/'"$BACKEND_PORT_8081_TCP_ADDR"'/g' /etc/nginx/sites-enabled/*.conf
-sed -i 's/COLLECT_IP/'"$COLLECTOR_PORT_1080_TCP_ADDR"'/g' /etc/nginx/sites-enabled/*.conf
-sed -i 's/EVALUATE_IP/'"$EVALUATOR_PORT_1081_TCP_ADDR"'/g' /etc/nginx/sites-enabled/*.conf
+sed -i 's/PIWIK_IP/'"$PIWIK_PORT_9000_TCP_ADDR"'/g' /etc/nginx/sites-enabled/*.conf
 sed -i 's/SITE_IP/'"$SITE_PORT_3000_TCP_ADDR"'/g' /etc/nginx/sites-enabled/*.conf
 sed -i 's/SITE2_IP/'"$SITE2_PORT_3001_TCP_ADDR"'/g' /etc/nginx/sites-enabled/*.conf
 service nginx start
