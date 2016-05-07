f37c59ee2f2ca11d1a0026e7c8e44d75f6c6abca
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2016-05-07 11:34:39 +0300

    Change to using link name as hostname, as it is possible with Docker 1.9

--- frontend/site/docker/nginx-run.sh
+++ frontend/site/docker/nginx-run.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 cp /etc/confd/templates/nginx/nginx.conf /etc/nginx/nginx.conf
-sed -i 's/BACKEND_IP/'"$BACKEND_PORT_8081_TCP_ADDR"'/g' /etc/nginx/sites-enabled/*.conf
-sed -i 's/PIWIK_IP/'"$PIWIK_PORT_9000_TCP_ADDR"'/g' /etc/nginx/sites-enabled/*.conf
-sed -i 's/SITE_IP/'"$SITE_PORT_3000_TCP_ADDR"'/g' /etc/nginx/sites-enabled/*.conf
-sed -i 's/SITE2_IP/'"$SITE2_PORT_3001_TCP_ADDR"'/g' /etc/nginx/sites-enabled/*.conf
 service nginx start
