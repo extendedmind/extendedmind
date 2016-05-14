42c936dc0671ebabca397f8aa98f10c952c964db
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2016-05-14 13:29:02 +0300

    Much simpler test, as we now wait for backend to be ready before commencing testing

--- e2e/src/test/loggedInBasicFunctionalitySpec.js
+++ e2e/src/test/loggedInBasicFunctionalitySpec.js
@@ -11,33 +11,14 @@ describe('extended mind', function() {
     helpers.waitForBackendReady(40000).then(function () {
       var doneButton = element(by.buttonText('done'));
       var errorSearch = by.css('.text-error');
-      doneButton.click().then(function(){
-        browser.driver.isElementPresent(errorSearch).then(function(errorIsPresent){
-          if (errorIsPresent){
-            return doneButton.click().then(function(){
-              return browser.driver.wait(function() {
-                browser.driver.sleep(800);
-                return doneButton.click().then(function(){
-                  return browser.driver.isElementPresent(errorSearch).then(function(errorIsPresent){
-                    return !errorIsPresent;
-                  });
-                });
-              }, 10000);
-            });
-          }
-        });
-      });
+      doneButton.click();
       helpers.waitForUrlToChangeTo('http://localhost:8008/my', 10000).then(function () {
         // Assert that we are in the today page
         var availableActiveSlide = by.xpath('//div[@swiper-slide="focus/tasks/left"]');
-        browser.driver.wait(function() {
-          return browser.driver.isElementPresent(availableActiveSlide);
-        }, 10000).then(function(){
-          var activeSlide = element(availableActiveSlide);
-          expect(activeSlide.isDisplayed()).toBeTruthy();
-          var activeSlideHeading = activeSlide.element(by.css('h2.group-heading'));
-          expect(activeSlideHeading.getText()).toBe('today');
-        });
+        var activeSlide = element(availableActiveSlide);
+        expect(activeSlide.isDisplayed()).toBeTruthy();
+        var activeSlideHeading = activeSlide.element(by.css('h2.group-heading'));
+        expect(activeSlideHeading.getText()).toBe('today');
       });
     });
   });
