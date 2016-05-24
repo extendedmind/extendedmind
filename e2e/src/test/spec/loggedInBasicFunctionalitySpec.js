547e604772b44e4330ac132778ecef2a630c6ec4
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2016-05-24 18:32:44 +0300

    Full visual test of every main location (with the exception of the date picker, which depends on the current time, and thus would need Date object mocking to work.

--- e2e/src/test/spec/loggedInBasicFunctionalitySpec.js
+++ e2e/src/test/spec/loggedInBasicFunctionalitySpec.js
@@ -11,7 +11,6 @@ describe('extended mind', function() {
     h.waitForBackendReady(40000).then(function () {
       vr.takeScreenshot('login-ready');
       const doneButton = element(by.buttonText('done'));
-      const errorSearch = by.css('.text-error');
       doneButton.click();
       h.waitForUrlToChangeTo('http://localhost:8008/my', 10000).then(function () {
 
@@ -25,7 +24,6 @@ describe('extended mind', function() {
         menuButton.click();
         browser.driver.sleep(h.MENU_ANIMATION_SPEED);
         vr.takeScreenshot('focus-tasks-today-menu');
-        const errorSearch = by.css('.text-error');
         const focusToNotesLink = element(by.xpath(h.XPATH_FOCUS_TASKS_SLIDE +
                                                   h.XPATH_FOOTER_NAVIGATION_LINK.replace('${linkText}', 'notes')));
         focusToNotesLink.click();
@@ -44,12 +42,30 @@ describe('extended mind', function() {
         const activeTaskEditorSlide = element(by.xpath(h.XPATH_TASK_EDITOR_BASIC_SLIDE));
         expect(activeTaskEditorSlide.isDisplayed()).toBeTruthy();
         vr.takeScreenshot('focus-tasks-today-menu-editor-basic');
+        const listPickerLink = element(by.xpath(h.XPATH_TASK_EDITOR_BASIC_SLIDE + h.XPATH_LINK_SWIPER_CLICK.replace('${clickMethod}', 'openListPicker()')));
+        listPickerLink.click();
+        const listPickerDone = element(by.xpath(h.XPATH_EDITOR_HIGHLIGHTED_LINK.replace('${linkText}', 'done')));
+        expect(listPickerDone.isDisplayed()).toBeTruthy();
+        vr.takeScreenshot('focus-tasks-today-menu-editor-basic-list');
+        listPickerDone.click();
+        const contextPickerLink = element(by.xpath(h.XPATH_TASK_EDITOR_BASIC_SLIDE + h.XPATH_LINK_SWIPER_CLICK.replace('${clickMethod}', 'openContextPicker()')));
+        contextPickerLink.click();
+        const contextPickerDone = element(by.xpath(h.XPATH_EDITOR_HIGHLIGHTED_LINK.replace('${linkText}', 'done')));
+        expect(contextPickerDone.isDisplayed()).toBeTruthy();
+        vr.takeScreenshot('focus-tasks-today-menu-editor-basic-context');
+        contextPickerDone.click();
         const taskEditorAdvancedLink = element(by.xpath(h.XPATH_EDITOR_FOOTER_NAVIGATION_LINK.replace('${linkText}', 'advanced')));
         taskEditorAdvancedLink.click();
         browser.driver.sleep(h.SWIPER_ANIMATION_SPEED);
         let taskEditorCloseLink = element(by.xpath(h.XPATH_EDITOR_CLOSE.replace('${ItemType}', 'Task')));
         expect(taskEditorCloseLink.isDisplayed()).toBeTruthy();
         vr.takeScreenshot('focus-tasks-today-menu-editor-advanced');
+        const repeatingPickerLink = element(by.xpath(h.XPATH_TASK_EDITOR_ADVANCED_SLIDE + h.XPATH_LINK_SWIPER_CLICK.replace('${clickMethod}', 'openRepeatingPicker()')));
+        repeatingPickerLink.click();
+        const repeatingPickerDone = element(by.xpath(h.XPATH_EDITOR_HIGHLIGHTED_LINK.replace('${linkText}', 'done')));
+        expect(repeatingPickerDone.isDisplayed()).toBeTruthy();
+        vr.takeScreenshot('focus-tasks-today-menu-editor-repeating');
+        repeatingPickerDone.click();
         taskEditorCloseLink.click();
 
         // USER
@@ -68,6 +84,24 @@ describe('extended mind', function() {
         expect(closeChangePasswordEditor.isDisplayed()).toBeTruthy();
         vr.takeScreenshot('user-details-menu-change-password-editor');
         closeChangePasswordEditor.click();
+        const changeEmailLink = element(by.xpath(h.XPATH_USER_DETAILS_SLIDE + h.XPATH_LINK_ITEM.replace('${linkText}', 'change email')));
+        changeEmailLink.click();
+        const closeChangeEmailEditor = element(by.xpath(h.XPATH_EDITOR_HEADER_NAVIGATION_LINK.replace('${linkText}', 'back')));
+        expect(closeChangeEmailEditor.isDisplayed()).toBeTruthy();
+        vr.takeScreenshot('user-details-menu-change-email-editor');
+        closeChangeEmailEditor.click();
+        const updatePublishingLink = element(by.xpath(h.XPATH_USER_DETAILS_SLIDE + h.XPATH_LINK_ITEM.replace('${linkText}', 'update publishing information')));
+        updatePublishingLink.click();
+        const closeUpdatePublishingEditor = element(by.xpath(h.XPATH_EDITOR_HEADER_NAVIGATION_LINK.replace('${linkText}', 'back')));
+        expect(closeUpdatePublishingEditor.isDisplayed()).toBeTruthy();
+        vr.takeScreenshot('user-details-menu-update-publishing-editor');
+        closeUpdatePublishingEditor.click();
+        const deleteAccountLink = element(by.xpath(h.XPATH_USER_DETAILS_SLIDE + h.XPATH_LINK_ITEM.replace('${linkText}', 'delete account')));
+        deleteAccountLink.click();
+        const closeDeleteAccountEditor = element(by.xpath(h.XPATH_EDITOR_HEADER_NAVIGATION_LINK.replace('${linkText}', 'back')));
+        expect(closeDeleteAccountEditor.isDisplayed()).toBeTruthy();
+        vr.takeScreenshot('user-details-menu-delete-account-editor');
+        closeDeleteAccountEditor.click();
 
         // INBOX
         const inboxLink = element(by.xpath(h.XPATH_MENU_LINK.replace('${linkText}', 'inbox')));
@@ -90,8 +124,25 @@ describe('extended mind', function() {
         writeEssayBodyLink.click();
         taskEditorCloseLink = element(by.xpath(h.XPATH_EDITOR_CLOSE.replace('${ItemType}', 'Task')));
         expect(taskEditorCloseLink.isDisplayed()).toBeTruthy();
-        vr.takeScreenshot('tasks-all-menu-editor');
+        vr.takeScreenshot('tasks-all-menu-editor-basic');
         taskEditorCloseLink.click();
+        const tasksToContextsLink = element(by.xpath(h.XPATH_TASKS_ALL_SLIDE +
+                                                     h.XPATH_FOOTER_NAVIGATION_LINK.replace('${linkText}', 'contexts')));
+        tasksToContextsLink.click();
+        browser.driver.sleep(h.SWIPER_ANIMATION_SPEED);
+        vr.takeScreenshot('tasks-contexts-menu');
+        const computerContextLink = element(by.xpath(h.XPATH_TASKS_CONTEXTS_SLIDE + h.XPATH_LINK_LIST_ITEM.replace('${linkText}', '@computer')));
+        computerContextLink.click();
+        browser.driver.sleep(h.SWIPER_ANIMATION_SPEED);
+        vr.takeScreenshot('tasks-context-menu');
+
+        // CONTEXT EDITOR
+        const computerContextHeadingLink = element(by.xpath(h.XPATH_TASKS_CONTEXT_SLIDE + h.XPATH_LINK_SWIPER_CLICK.replace('${clickMethod}', 'openContextEditor(context)')));
+        computerContextHeadingLink.click();
+        const contextEditorCloseLink = element(by.xpath(h.XPATH_EDITOR_CLOSE.replace('${ItemType}', 'Tag')));
+        expect(contextEditorCloseLink.isDisplayed()).toBeTruthy();
+        vr.takeScreenshot('tasks-context-menu-editor');
+        contextEditorCloseLink.click();
 
         // NOTES
         const notesLink = element(by.xpath(h.XPATH_MENU_LINK.replace('${linkText}', 'notes')));
@@ -99,10 +150,24 @@ describe('extended mind', function() {
         const notesOnProductivityLink = element(by.xpath(h.XPATH_NOTES_CONTENT + h.XPATH_LINK_LIST_ITEM.replace('${linkText}', 'notes on productivity')));
         expect(notesOnProductivityLink.isDisplayed()).toBeTruthy();
         vr.takeScreenshot('notes-menu');
+
+        // NOTE EDITOR
         notesOnProductivityLink.click();
         let noteEditorCloseLink = element(by.xpath(h.XPATH_EDITOR_CLOSE.replace('${ItemType}', 'Note')));
         expect(noteEditorCloseLink.isDisplayed()).toBeTruthy();
-        vr.takeScreenshot('notes-menu-editor');
+        vr.takeScreenshot('notes-menu-editor-basic');
+        const noteListPickerLink = element(by.xpath(h.XPATH_NOTE_EDITOR_BASIC_SLIDE + h.XPATH_LINK_SWIPER_CLICK.replace('${clickMethod}', 'openListPicker()')));
+        noteListPickerLink.click();
+        const noteListPickerDone = element(by.xpath(h.XPATH_EDITOR_HIGHLIGHTED_LINK.replace('${linkText}', 'done')));
+        expect(noteListPickerDone.isDisplayed()).toBeTruthy();
+        vr.takeScreenshot('notes-menu-editor-basic-list');
+        noteListPickerDone.click();
+        const noteKeywordsPickerLink = element(by.xpath(h.XPATH_NOTE_EDITOR_BASIC_SLIDE + h.XPATH_LINK_SWIPER_CLICK.replace('${clickMethod}', 'openKeywordsPicker()')));
+        noteKeywordsPickerLink.click();
+        const noteKeywordsPickerDone = element(by.xpath(h.XPATH_EDITOR_HIGHLIGHTED_LINK.replace('${linkText}', 'done')));
+        expect(noteKeywordsPickerDone.isDisplayed()).toBeTruthy();
+        vr.takeScreenshot('notes-menu-editor-basic-keywords');
+        noteKeywordsPickerDone.click();
         noteEditorCloseLink.click();
 
         // LISTS
@@ -116,13 +181,85 @@ describe('extended mind', function() {
         tripToDublinLink.click();
         expect(element(by.xpath(h.XPATH_LIST_TASKS_SLIDE + h.XPATH_LINK_LIST_ITEM.replace('${linkText}', 'print tickets'))).isDisplayed()).toBeTruthy();
         vr.takeScreenshot('list-tasks-menu');
+        const listToNotesLink = element(by.xpath(h.XPATH_LIST_TASKS_SLIDE +
+                                                 h.XPATH_FOOTER_NAVIGATION_LINK.replace('${linkText}', 'notes')));
+        listToNotesLink.click();
+        browser.driver.sleep(h.SWIPER_ANIMATION_SPEED);
+        vr.takeScreenshot('list-notes-menu');
 
-/*        let noteEditorCloseLink = element(by.xpath(h.XPATH_EDITOR_CLOSE.replace('${ItemType}', 'Note')));
-        expect(noteEditorCloseLink.isDisplayed()).toBeTruthy();
-        vr.takeScreenshot('notes-menu-editor');
-        noteEditorCloseLink.click();
-*/
+        // LIST EDITOR
+        const listHeadingLink = element(by.xpath(h.XPATH_TOOLBAR_HEADING_BUTTON));
+        listHeadingLink.click();
+        let listEditorCloseLink = element(by.xpath(h.XPATH_EDITOR_CLOSE.replace('${ItemType}', 'List')));
+        expect(listEditorCloseLink.isDisplayed()).toBeTruthy();
+        vr.takeScreenshot('list-notes-menu-editor-basic');
+        const parentListPickerLink = element(by.xpath(h.XPATH_LIST_EDITOR_BASIC_SLIDE + h.XPATH_LINK_SWIPER_CLICK.replace('${clickMethod}', 'openListPicker()')));
+        parentListPickerLink.click();
+        const parentListPickerDone = element(by.xpath(h.XPATH_EDITOR_HIGHLIGHTED_LINK.replace('${linkText}', 'done')));
+        expect(parentListPickerDone.isDisplayed()).toBeTruthy();
+        vr.takeScreenshot('list-notes-menu-editor-basic-parent');
+        parentListPickerDone.click();
+        const listEditorAdvancedLink = element(by.xpath(h.XPATH_EDITOR_FOOTER_NAVIGATION_LINK.replace('${linkText}', 'advanced')));
+        listEditorAdvancedLink.click();
+        browser.driver.sleep(h.SWIPER_ANIMATION_SPEED);
+        vr.takeScreenshot('list-notes-menu-editor-advanced');
+        const listShareEditorLink = element(by.xpath(h.XPATH_LIST_EDITOR_ADVANCED_SLIDE + h.XPATH_LINK_SWIPER_CLICK.replace('${clickMethod}', 'openListShareEditor()')));
+        listShareEditorLink.click();
+        const listShareEditorDone = element(by.xpath(h.XPATH_EDITOR_HIGHLIGHTED_LINK.replace('${linkText}', 'done')));
+        expect(listShareEditorDone.isDisplayed()).toBeTruthy();
+        vr.takeScreenshot('list-notes-menu-editor-advanced-share');
+        listShareEditorDone.click();
+        listEditorCloseLink.click();
 
+        // TRASH
+        const trashLink = element(by.xpath(h.XPATH_MENU_LINK.replace('${linkText}', 'trash')));
+        trashLink.click();
+        const trashHeading = element(by.xpath(h.XPATH_HEADING.replace('${headingText}', '0 deleted')));
+        expect(trashHeading.isDisplayed()).toBeTruthy();
+        vr.takeScreenshot('trash-menu');
+
+        // SETTINGS
+        const settingsLink = element(by.xpath(h.XPATH_MENU_LINK.replace('${linkText}', 'settings')));
+        settingsLink.click();
+        const settingsHeading = element(by.xpath(h.XPATH_HEADING.replace('${headingText}', 'settings')));
+        expect(settingsHeading.isDisplayed()).toBeTruthy();
+        vr.takeScreenshot('settings-menu-top');
+
+        // OMNIBAR
+        const omnibarButton = element(by.xpath(h.XPATH_OMNIBAR_BUTTON));
+        omnibarButton.click();
+        const omnibarCloseLink = element(by.xpath(h.XPATH_LINK_SWIPER_CLICK.replace('${clickMethod}', 'closeEditor()')));
+        expect(omnibarCloseLink.isDisplayed()).toBeTruthy();
+        vr.takeScreenshot('settings-menu-omnibar');
+        const expandLink = element(by.xpath(h.XPATH_LINK_SWIPER_CLICK.replace('${clickMethod}', 'toggleExpand()')));
+        expandLink.click();
+        const editLink = element(by.xpath(h.XPATH_EDITOR_HIGHLIGHTED_LINK.replace('${linkText}', 'edit')));
+        expect(editLink.isDisplayed()).toBeTruthy();
+        vr.takeScreenshot('settings-menu-omnibar-keywords');
+        editLink.click();
+        const secretKeywordLink = element(by.xpath(h.XPATH_KEYWORD_LINK.replace('${linkText}', '#secret')));
+        expect(secretKeywordLink.isDisplayed()).toBeTruthy();
+        vr.takeScreenshot('settings-menu-omnibar-keywords-edit');
+        secretKeywordLink.click();
+        const keywordEditorCloseLink = element(by.xpath(h.XPATH_EDITOR_CLOSE.replace('${ItemType}', 'Tag')));
+        expect(keywordEditorCloseLink.isDisplayed()).toBeTruthy();
+        vr.takeScreenshot('settings-menu-keyword-editor');
+        const parentKeywordPickerLink = element(by.xpath(h.XPATH_LINK_SWIPER_CLICK.replace('${clickMethod}', 'openParentPicker()')));
+        parentKeywordPickerLink.click();
+        const parentKeywordPickerDone = element(by.xpath(h.XPATH_TAG_EDITOR_HIGHLIGHTED_LINK.replace('${linkText}', 'done')));
+        expect(parentKeywordPickerDone.isDisplayed()).toBeTruthy();
+        vr.takeScreenshot('settings-menu-keyword-editor-parent');
+        parentKeywordPickerDone.click();
+        keywordEditorCloseLink.click();
+
+        // LOGOUT
+        userLink.click();
+        const logoutButton = element(by.xpath(h.XPATH_FOOTER_BUTTON.replace('${clickMethod}', 'logOut()')));
+        logoutButton.click();
+        h.waitForUrlToChangeTo('http://localhost:8008/entry', 10000).then(function () {
+          const entryMainSlide = element(by.xpath(h.XPATH_ENTRY_MAIN_SLIDE));
+          expect(entryMainSlide.isDisplayed()).toBeTruthy();
+        });
       });
     });
   });
