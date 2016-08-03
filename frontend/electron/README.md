2889db51c304a1403121569e4d4ac594a505489c
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2016-08-03 11:23:13 +0300

    Upgraded to latest Electron. Use electron-builder to also make Linux packages, add support for RPM.

--- frontend/electron/README.md
+++ frontend/electron/README.md
@@ -6,10 +6,10 @@ Extended Mind - Electron builds for OSX and Windows
 Setup on OSX
 ------------
 
-Install Wine for Windows and dpkg and fakeroot for Linux:
+Install Xquartz, Wine and Mono for Windows and gnu-tar, libicns, graphicsmagick, rpm and xz for Linux:
 
 ```
-brew install wine makensis fakeroot dpkg
+brew install Caskroom/cask/xquartz wine mono gnu-tar libicns graphicsmagick rpm xz
 ```
 
 Building
--- frontend/electron/src/app/main.js
+++ frontend/electron/src/app/main.js
@@ -1,26 +1,106 @@
-var app = require('app');  // Module to control application life.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
-var Menu = require('menu');
-var MenuItem = require('menu-item');
+const {app, BrowserWindow, Menu} = require('electron')
+
+// Create the Application's main menu
+
+let template = [
+{
+  label: 'Edit',
+  submenu: [
+    {
+      label: 'Undo',
+      accelerator: 'Command+Z',
+      selector: 'undo:'
+    },
+    {
+      label: 'Redo',
+      accelerator: 'Shift+Command+Z',
+      selector: 'redo:'
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Cut',
+      accelerator: 'Command+X',
+      selector: 'cut:'
+    },
+    {
+      label: 'Copy',
+      accelerator: 'Command+C',
+      selector: 'copy:'
+    },
+    {
+      label: 'Paste',
+      accelerator: 'Command+V',
+      selector: 'paste:'
+    },
+    {
+      label: 'Select All',
+      accelerator: 'Command+A',
+      selector: 'selectAll:'
+    },
+  ]
+},
+{
+  label: 'View',
+  submenu: [
+    {
+      label: 'Reload',
+      accelerator: 'CmdOrCtrl+R',
+      click: function(item, focusedWindow) {
+        if (focusedWindow)
+          focusedWindow.reload();
+      }
+    },
+    {
+      label: 'Toggle Full Screen',
+      accelerator: (function() {
+        if (process.platform == 'darwin')
+          return 'Ctrl+Command+F';
+        else
+          return 'F11';
+      })(),
+      click: function(item, focusedWindow) {
+        if (focusedWindow)
+          focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+      }
+    },
+    {
+      label: 'Toggle Developer Tools',
+      accelerator: (function() {
+        if (process.platform == 'darwin')
+          return 'Alt+Command+I';
+        else
+          return 'Ctrl+Shift+I';
+      })(),
+      click: function(item, focusedWindow) {
+        if (focusedWindow)
+          focusedWindow.toggleDevTools();
+      }
+    }
+  ]
+}];
+if (process.platform === 'darwin') {
+  const name = app.getName().replace(new RegExp('-', 'g'), ' ');
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        label: 'Quit ' + name,
+        role: 'quit'
+      }
+    ]
+  });
+}
+const menu = Menu.buildFromTemplate(template);
 
 // Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is GCed.
-var mainWindow = null;
+// be closed automatically when the JavaScript object is garbage collected.
+let win
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
-    app.quit();
-  }else{
-    mainWindow = null;
-  }
-});
-
-function createMainWindow(){
+function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({
+  win = new BrowserWindow({
     width: 1600,
     height: 1200,
     useContentSize: true,
@@ -28,124 +108,38 @@ function createMainWindow(){
     minHeight: 600});
 
   // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  win.loadURL(`file://${__dirname}/index.html`)
 
-  // Create the Application's main menu
-  var template = [
-  {
-    label: 'extended mind',
-    submenu: [
-      {
-        label: 'Quit extended mind',
-        accelerator: 'Command+Q',
-        click: function() { app.quit(); }
-      },
-    ]
-  },
-  {
-    label: 'Edit',
-    submenu: [
-      {
-        label: 'Undo',
-        accelerator: 'Command+Z',
-        selector: 'undo:'
-      },
-      {
-        label: 'Redo',
-        accelerator: 'Shift+Command+Z',
-        selector: 'redo:'
-      },
-      {
-        type: 'separator'
-      },
-      {
-        label: 'Cut',
-        accelerator: 'Command+X',
-        selector: 'cut:'
-      },
-      {
-        label: 'Copy',
-        accelerator: 'Command+C',
-        selector: 'copy:'
-      },
-      {
-        label: 'Paste',
-        accelerator: 'Command+V',
-        selector: 'paste:'
-      },
-      {
-        label: 'Select All',
-        accelerator: 'Command+A',
-        selector: 'selectAll:'
-      },
-    ]
-  },
-  {
-    label: 'View',
-    submenu: [
-      {
-        label: 'Reload',
-        accelerator: 'CmdOrCtrl+R',
-        click: function(item, focusedWindow) {
-          if (focusedWindow)
-            focusedWindow.reload();
-        }
-      },
-      {
-        label: 'Toggle Full Screen',
-        accelerator: (function() {
-          if (process.platform == 'darwin')
-            return 'Ctrl+Command+F';
-          else
-            return 'F11';
-        })(),
-        click: function(item, focusedWindow) {
-          if (focusedWindow)
-            focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
-        }
-      },
-      {
-        label: 'Toggle Developer Tools',
-        accelerator: (function() {
-          if (process.platform == 'darwin')
-            return 'Alt+Command+I';
-          else
-            return 'Ctrl+Shift+I';
-        })(),
-        click: function(item, focusedWindow) {
-          if (focusedWindow)
-            focusedWindow.toggleDevTools();
-        }
-      }
-    ]
-  }];
-
-  menu = Menu.buildFromTemplate(template);
+  // Set menu
   Menu.setApplicationMenu(menu);
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', function() {
-  createMainWindow();
-});
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
 
-/* NOTE: This should be the right event for this version of Electron, but it isn't.
-         The latter is fired even though it should be deprecated.
-app.on('activate', function(event, hasVisibleWindows){
-  if (!hasVisibleWindows){
-    if (!mainWindow){
-      createMainWindow();
-    }else{
-      mainWindow.show();
-    }
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
-});*/
+})
 
-app.on('activate-with-no-open-windows', function(){
-  if (!mainWindow){
-    createMainWindow();
-  }else{
-    mainWindow.show();
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
   }
-});
+})
