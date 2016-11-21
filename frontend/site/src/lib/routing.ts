91a9c005b3ff16dbb1cc9549033930b6814598e9
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2016-11-21 20:00:29 +0200

    Added infinite scrolling to headers using waypoints

--- frontend/site/src/lib/routing.ts
+++ frontend/site/src/lib/routing.ts
@@ -23,8 +23,21 @@ export class Routing {
 
   private async headers(ctx: Router.IRouterContext, next: () => Promise<any>) {
     console.info("GET ", ctx.path);
+    const HEADERS_PER_PAGE: number = 10;
     const publicHeaders = await ctx.state.backendClient.getPublicHeaders();
-    let renderContext: any = {};
+    const headers = publicHeaders.getNotes();
+    // How many items were indicated as being not shown previously. If first query, everything is not shown
+    const previousRemaining: number = ctx.query.remaining === undefined ? headers.length : ctx.query.remaining;
+    // Because new headers might be added to the top of the array, we use remaining to count the index from
+    // the end.
+    const firstHeaderIndex = headers.length - previousRemaining;
+    const headersPortion = headers.slice(firstHeaderIndex, firstHeaderIndex + HEADERS_PER_PAGE);
+    const remaining: number = headers.length - (firstHeaderIndex + HEADERS_PER_PAGE) < 0
+      ? 0 : headers.length - (firstHeaderIndex + HEADERS_PER_PAGE);
+    let renderContext: any = {
+      headers: headersPortion,
+      remaining: remaining,
+    };
     // TODO: add support for basic info of owner from .content field of publicItemsResponse.json
     ctx.body = ctx.state.render.template("pages/headers", renderContext);
   }
