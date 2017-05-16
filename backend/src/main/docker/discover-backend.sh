20ed6ce1bde5db59e4a78dc9e82278ce660bbece
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2017-05-16 09:09:32 +0300

    Only use one init container based on busybox that also chowns.

--- backend/src/main/docker/discover-backend.sh
+++ backend/src/main/docker/discover-backend.sh
@@ -1,16 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
 SERVER_ID=$(($(hostname | sed s/.*-//) + 1))
 echo "Guessed server id: $SERVER_ID"
 
 if [ -z "$2" ]
 then
-  echo "Usage: generate-neo4j-properties.sh [POD_NAME] [TO_DIRECTORY] (OPERATION_MODE)"
+  echo "Usage: discover-backend.sh [CONF_DIRECTORY] [DATA_DIRECTORY] (OPERATION_MODE)"
   exit 1
 fi
 
-POD_NAME=$1
-OUTPUT=$2/neo4j.properties
+CONF_DIRECTORY=$1
+DATA_DIRECTORY=$2
+NEO4J_PROPS=$CONF_DIRECTORY/neo4j.properties
 HA=false
 if [ "$3" = "HA" ] || [ "$3" = "HA_BOOTSTRAP" ]; then
   HA=true
@@ -20,17 +21,21 @@ fi
 trap 'echo exiting generate neo4j properties script..; exit' SIGINT SIGQUIT SIGTERM
 
 # Create/empty the properties file
-> $OUTPUT
+> $NEO4J_PROPS
 
 if [ "$HA" = false ]; then 
-  echo "dbms.allow_format_migration=true" >> $OUTPUT
+  echo "dbms.allow_format_migration=true" >> $NEO4J_PROPS
 else
   # TODO: Use actual "replicas" value from kubernetes #31218
   echo "HA: assuming exactly 3 nodes"
   HOSTS=backend-0.backend.default.svc.cluster.local:5001,backend-1.backend.default.svc.cluster.local:5001,backend-2.backend.default.svc.cluster.local:5001
   PF=2
-  echo "dbms.mode=ha" >> $OUTPUT
-  echo "ha.server_id=$SERVER_ID" >> $OUTPUT
-  echo "ha.initial_hosts=$HOSTS" >> $OUTPUT
-  echo "ha.tx_push_factor=$PF" >> $OUTPUT
+  echo "dbms.mode=ha" >> $NEO4J_PROPS
+  echo "ha.server_id=$SERVER_ID" >> $NEO4J_PROPS
+  echo "ha.initial_hosts=$HOSTS" >> $NEO4J_PROPS
+  echo "ha.tx_push_factor=$PF" >> $NEO4J_PROPS
 fi
+
+# Fix permissions
+chown -R 1000:1000 $CONF_DIRECTORY
+chown -R 1000:1000 $DATA_DIRECTORY
