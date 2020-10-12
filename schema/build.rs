908de1de8e6ddd03d68613478f2154ed44e3e051
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2020-10-12 16:39:44 +0300

    Move temp cargo sources under target

--- schema/build.rs
+++ schema/build.rs
@@ -2,11 +2,15 @@ use std::env;
 use std::process;
 use std::process::{Command, Stdio};
 
+
 fn main() {
-    let current_dir = env::var("CARGO_MANIFEST_DIR").ok().expect("Cargo should have set the CARGO_MANIFEST_DIR environment variable");
+    let current_dir = env::var("CARGO_MANIFEST_DIR").expect("Cargo should have set the CARGO_MANIFEST_DIR environment variable");
+    let bazel_build_dir = &format!("{}/../bazel-extendedmind/bazel-out/darwin-fastbuild/bin/schema", current_dir);
     if Command::new("sh")
         .arg("-c")
-        .arg(&format!("cp -r {}/../bazel-extendedmind/bazel-out/darwin-fastbuild/bin/schema/models {}/src/", current_dir, current_dir))
+        .arg(&format!("mkdir -p target/src \
+                      && cp -r {}/models {}/target/src/ \
+                      && cp {}/lib.rs {}/target/src/", bazel_build_dir, current_dir, bazel_build_dir, current_dir))
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .status().unwrap().code().unwrap() != 0
