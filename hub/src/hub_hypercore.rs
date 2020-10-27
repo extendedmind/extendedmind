dd686b92f7954cbbbe8bce28e719b28ed8559beb
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2020-10-27 11:23:48 +0200

    Almost compiling but not quite

--- hub/src/hub_hypercore.rs
+++ hub/src/hub_hypercore.rs
@@ -1,17 +1,17 @@
 use actix::prelude::*;
-use async_pipe;
+use async_pipe::{PipeReader, PipeWriter};
 use bytes::Bytes;
 use futures::io::AsyncReadExt;
 use futures::io::AsyncWriteExt;
-use hypercore_protocol::ProtocolBuilder;
+use futures::TryFutureExt;
+use hypercore_protocol::{Protocol, ProtocolBuilder};
 // TODO:
 // use hypercore_protocol::Message as HypercoreMessage;
 
 pub struct HubHypercore {
-    incoming_writer: async_pipe::PipeWriter,
-    incoming_reader: async_pipe::PipeReader,
-    outgoing_writer: async_pipe::PipeWriter,
-    outgoing_reader: async_pipe::PipeReader,
+    incoming_writer: PipeWriter,
+    outgoing_reader: PipeReader,
+    protocol: Protocol<PipeReader, PipeWriter>,
 }
 impl HubHypercore {
     pub fn new() -> Self {
@@ -19,9 +19,8 @@ impl HubHypercore {
         let (outgoing_writer, mut outgoing_reader) = async_pipe::pipe();
         Self {
             incoming_writer,
-            incoming_reader,
-            outgoing_writer,
             outgoing_reader,
+            protocol: ProtocolBuilder::new(true).connect_rw(incoming_reader, outgoing_writer),
         }
     }
 }
@@ -49,19 +48,24 @@ pub struct HubHypercoreMessage {
     pub payload: Bytes,
 }
 impl Message for HubHypercoreMessage {
-    type Result = Result<Bytes, HubHypercoreError>;
+    type Result = Result<Bytes, std::io::Error>;
 }
 impl Handler<HubHypercoreMessage> for HubHypercore {
-    type Result = ResponseActFuture<HubHypercore, Result<Bytes, HubHypercoreError>>;
+    type Result = ResponseActFuture<HubHypercore, Result<Bytes, std::io::Error>>;
     fn handle(&mut self, msg: HubHypercoreMessage, _ctx: &mut Context<Self>) -> Self::Result {
         debug!("Got hub hypercore message");
         // TODO: Somehow use slef.incoming_writer! instead of these
-        let (mut incoming_writer, incoming_reader) = async_pipe::pipe();
-        let (outgoing_writer, mut outgoing_reader) = async_pipe::pipe();
+        // let (mut incoming_writer, incoming_reader) = async_pipe::pipe();
+        // let (outgoing_writer, mut outgoing_reader) = async_pipe::pipe();
 
-        let mut protocol = ProtocolBuilder::new(true).connect_rw(incoming_reader, outgoing_writer);
-
-        let fut = futures::future::ok(msg.payload);
+        // let mut protocol = ProtocolBuilder::new(true).connect_rw(incoming_reader, outgoing_writer);
+        let mut v = Vec::new();
+        let fut = Box::new(self.incoming_writer.write_all(&msg.payload)).and_then(|_| {
+            self.outgoing_reader
+                .read_to_end(&mut v)
+                .map_ok(|_| Bytes::from(v))
+        });
+        // let fut = futures::future::ok(msg.payload);
         Box::pin(fut.into_actor(self))
     }
 }
