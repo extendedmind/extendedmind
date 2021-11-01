b685acc2bfbf6488ac3767e7647024bb3cbe573d
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2021-11-01 12:46:53 +0200

    Use a more descriptive id for embedded use

--- ui/web/svelte.config.js
+++ ui/web/svelte.config.js
@@ -24,7 +24,7 @@ const config = {
         },
         ssr: false,
         // hydrate the <div id="svelte"> element in src/app.html
-        target: '#svelte',
+        target: '#extendedmind',
     },
 };
 
