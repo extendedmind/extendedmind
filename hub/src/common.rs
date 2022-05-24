ffa12c0045387c0eba853ba48c6af0c77d6aeb8a
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2022-05-24 09:09:49 +0300

    Refactor one big file hub file, part 1

---
+++ hub/src/common.rs
@@ -0,0 +1,40 @@
+use async_std::channel::{Receiver, Sender};
+use extendedmind_engine::{Bytes, Engine, RandomAccessDisk};
+use std::io;
+use std::path::PathBuf;
+
+#[derive(Clone, Copy)]
+#[repr(u8)]
+pub enum SystemCommand {
+    WakeUp = 1,
+    Disconnect = 2,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum ReceiverType {
+    System,
+    Hypercore,
+    Client,
+}
+
+#[derive(derivative::Derivative)]
+#[derivative(Clone(bound = ""))]
+pub struct State {
+    // Engine
+    pub engine: Engine<RandomAccessDisk>,
+    // System command receiver
+    pub system_commands: Receiver<Result<Bytes, io::Error>>,
+    // Directory for data files
+    pub data_root_dir: PathBuf,
+    // Directory for static files
+    pub static_root_dir: Option<PathBuf>,
+    // Port to listen for HTTP traffic
+    pub http_port: Option<u16>,
+    // Port to listen for TCP hypercore protocol traffic
+    pub tcp_port: Option<u16>,
+}
+
+pub type ChannelSenderReceiver = (
+    Sender<Result<Bytes, io::Error>>,
+    Receiver<Result<Bytes, io::Error>>,
+);
--- hub/src/main.rs
+++ hub/src/main.rs
@@ -1,240 +1,23 @@
 use anyhow::Result;
 use async_ctrlc::CtrlC;
-use async_std::channel::{bounded, Receiver, Sender};
+use async_std::channel::bounded;
 use async_std::sync::{Arc, Mutex};
 use async_std::task;
 use clap::Parser;
+use extendedmind_engine::{tcp, Bytes, Engine};
 use futures::stream::StreamExt;
-use log::{debug, info};
-use std::io;
+use log::info;
 use std::path::PathBuf;
 use std::process;
 use std::sync::atomic::AtomicBool;
-use std::time::{Duration, Instant};
-use tide::{Body, Response, StatusCode};
-use tide_websockets::{Message, WebSocket};
+use std::time::Duration;
 
-use extendedmind_engine::{tcp, Bytes, ChannelWriter, Engine, EngineEvent, RandomAccessDisk};
+mod common;
+mod http;
+mod routing;
 
-#[derive(Clone, Copy)]
-#[repr(u8)]
-enum SystemCommand {
-    WakeUp = 1,
-    Disconnect = 2,
-}
-
-#[derive(Copy, Clone, Debug)]
-enum ReceiverType {
-    System,
-    Hypercore,
-    Client,
-}
-
-#[derive(derivative::Derivative)]
-#[derivative(Clone(bound = ""))]
-struct State {
-    // Engine
-    engine: Engine<RandomAccessDisk>,
-    // System command receiver
-    system_commands: Receiver<Result<Bytes, io::Error>>,
-    // Directory for data files
-    data_root_dir: PathBuf,
-    // Directory for static files
-    static_root_dir: Option<PathBuf>,
-    // Port to listen for HTTP traffic
-    http_port: Option<u16>,
-    // Port to listen for TCP hypercore protocol traffic
-    tcp_port: Option<u16>,
-}
-
-#[derive(Clone)]
-struct WrappedData {
-    data: Bytes,
-    receiver_type: ReceiverType,
-}
-
-type WrappedBytesClosure =
-    Box<dyn Fn(Result<Bytes, io::Error>) -> Result<WrappedData, io::Error> + Send + Sync>;
-
-type ChannelSenderReceiver = (
-    Sender<Result<Bytes, io::Error>>,
-    Receiver<Result<Bytes, io::Error>>,
-);
-
-async fn index(req: tide::Request<State>) -> tide::Result<Response> {
-    let static_root_dir = &req.state().static_root_dir;
-    if let Some(static_root_dir) = static_root_dir {
-        let mut res = Response::new(StatusCode::Ok);
-        res.set_body(
-            Body::from_file(
-                static_root_dir
-                    .join(req.url().path().get(1..).unwrap())
-                    .join("index.html"),
-            )
-            .await
-            .unwrap(),
-        );
-
-        Ok(res)
-    } else {
-        let res = Response::new(StatusCode::NotFound);
-        Ok(res)
-    }
-}
-
-async fn async_main(initial_state: State) -> Result<()> {
-    let http_port = initial_state.http_port;
-    let tcp_port = initial_state.tcp_port;
-    let engine = initial_state.engine.clone();
-
-    if let Some(http_port) = http_port {
-        let http_server = http_server(initial_state).unwrap();
-        let http_listener = http_server.listen("0.0.0.0:".to_owned() + &http_port.to_string());
-        if let Some(tcp_port) = tcp_port {
-            let tcp_listener = tcp::listen(format!("0.0.0.0:{}", tcp_port), engine);
-            futures::try_join!(http_listener, tcp_listener)?;
-        } else {
-            http_listener.await?;
-        }
-    } else {
-        if let Some(tcp_port) = tcp_port {
-            tcp::listen(format!("0.0.0.0:{}", tcp_port), engine).await?;
-        } else {
-            anyhow::bail!("Either tcp_port or http_port has to be provided");
-        };
-    }
-
-    Ok(())
-}
-
-fn http_server(initial_state: State) -> Result<tide::Server<State>> {
-    let static_root_dir = initial_state.static_root_dir.clone();
-    let mut app = tide::with_state(initial_state);
-
-    if let Some(static_root_dir) = static_root_dir {
-        app.at("").get(index);
-        app.at("/extendedmind").get(index);
-        app.at("/").serve_dir(static_root_dir.to_str().unwrap())?;
-    }
-
-    app.at("/extendedmind/hypercore").get(WebSocket::new(
-        |req: tide::Request<State>, stream| async move {
-            debug!("WS connect to hypercore");
-            // Get handle to async-tungstenite WebSocketStream
-            let ws_writer = stream.clone();
-            let mut ws_reader = stream.clone();
-
-            // Create engine and hypercore channels for this connection
-            let (engine_sender, hypercore_receiver): ChannelSenderReceiver = bounded(1000);
-            let (hypercore_sender, engine_receiver): ChannelSenderReceiver = bounded(1000);
-
-            // Create the engine event channel
-            let (engine_event_sender, engine_event_receiver): (
-                Sender<EngineEvent>,
-                Receiver<EngineEvent>,
-            ) = async_std::channel::bounded(1000);
-
-            // Get passable handle on engine
-            let engine = req.state().engine.clone();
-
-            // Launch a task for the protocol
-            task::spawn(async move {
-                engine
-                    .connect_passive(
-                        ChannelWriter::new(engine_sender),
-                        engine_receiver,
-                        engine_event_sender,
-                        engine_event_receiver,
-                    )
-                    .await
-                    .ok();
-            });
-
-            // Create channel for client data
-            let (client_sender, client_receiver): ChannelSenderReceiver = bounded(1000);
-
-            // Launch a second task for listening to receivers
-            task::spawn(async move {
-                let receivers = collect_receivers(req.state(), hypercore_receiver, client_receiver);
-                let mut merged_receivers = futures::stream::select_all(receivers);
-                let mut now = Instant::now();
-                while let Some(Ok(wrapped_value)) = merged_receivers.next().await {
-                    match wrapped_value.receiver_type {
-                        ReceiverType::System => {
-                            if wrapped_value.data.as_ref().get(0)
-                                == Some(&(SystemCommand::Disconnect as u8))
-                            {
-                                debug!("got disconnect");
-                                ws_writer.send(Message::Close(None)).await.unwrap();
-                                break;
-                            }
-                            // Send ping message if enough time has elapsed
-                            if now.elapsed().as_secs() > 30 {
-                                debug!("sending ping");
-                                ws_writer.send(Message::Ping(Vec::new())).await.unwrap();
-                                now = Instant::now();
-                            }
-                        }
-                        ReceiverType::Client => {
-                            debug!(
-                                "got client request, forwarding to hypercore {}",
-                                wrapped_value.data.len()
-                            );
-                            hypercore_sender
-                                .clone()
-                                .send(Ok(wrapped_value.data))
-                                .await
-                                .unwrap();
-                        }
-                        ReceiverType::Hypercore => {
-                            let msg = wrapped_value.data.as_ref().to_vec();
-                            debug!("got hypercore message, sending to client, {:?}", msg.len());
-                            ws_writer.send(Message::Binary(msg)).await.unwrap();
-                        }
-                    }
-                }
-            });
-
-            // Block loop on incoming messages
-            while let Some(Ok(Message::Binary(msg))) = ws_reader.next().await {
-                debug!("got incoming client message {:?}", msg.len());
-                client_sender.send(Ok(Bytes::from(msg))).await.unwrap();
-            }
-            Ok(())
-        },
-    ));
-    return Ok(app);
-}
-
-fn collect_receivers(
-    state: &State,
-    hypercore_receiver: Receiver<Result<Bytes, io::Error>>,
-    client_receiver: Receiver<Result<Bytes, io::Error>>,
-) -> Vec<futures::stream::Map<Receiver<Result<Bytes, io::Error>>, WrappedBytesClosure>> {
-    vec![
-        state
-            .system_commands
-            .clone()
-            .map(Box::new(|read_result: Result<Bytes, io::Error>| {
-                read_result.map(|data| WrappedData {
-                    data,
-                    receiver_type: ReceiverType::System,
-                })
-            }) as WrappedBytesClosure),
-        hypercore_receiver.map(Box::new(move |read_result: Result<Bytes, io::Error>| {
-            read_result.map(|data| WrappedData {
-                data,
-                receiver_type: ReceiverType::Hypercore,
-            })
-        }) as WrappedBytesClosure),
-        client_receiver.map(Box::new(|read_result: Result<Bytes, io::Error>| {
-            read_result.map(|data| WrappedData {
-                data,
-                receiver_type: ReceiverType::Client,
-            })
-        }) as WrappedBytesClosure),
-    ]
-}
+use common::{ChannelSenderReceiver, State, SystemCommand};
+use http::http_server;
 
 #[derive(Parser)]
 #[clap(version = "0.1.0", author = "Timo Tiuraniemi <timo.tiuraniemi@iki.fi>")]
@@ -270,6 +53,31 @@ fn setup_logging(log_to_stderr: bool) {
     };
 
     base_config.chain(std_config).apply().unwrap();
+}
+
+async fn async_main(initial_state: State) -> Result<()> {
+    let http_port = initial_state.http_port;
+    let tcp_port = initial_state.tcp_port;
+    let engine = initial_state.engine.clone();
+
+    if let Some(http_port) = http_port {
+        let http_server = http_server(initial_state).unwrap();
+        let http_listener = http_server.listen("0.0.0.0:".to_owned() + &http_port.to_string());
+        if let Some(tcp_port) = tcp_port {
+            let tcp_listener = tcp::listen(format!("0.0.0.0:{}", tcp_port), engine);
+            futures::try_join!(http_listener, tcp_listener)?;
+        } else {
+            http_listener.await?;
+        }
+    } else {
+        if let Some(tcp_port) = tcp_port {
+            tcp::listen(format!("0.0.0.0:{}", tcp_port), engine).await?;
+        } else {
+            anyhow::bail!("Either tcp_port or http_port has to be provided");
+        };
+    }
+
+    Ok(())
 }
 
 fn main() -> Result<()> {
