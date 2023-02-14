3bcb77d1a0a3251e376147e8092af6c3e0e016ee
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2023-02-14 13:38:53 +0200

    Setup peermerge listening

--- hub/src/main.rs
+++ hub/src/main.rs
@@ -1,11 +1,15 @@
 use anyhow::Result;
+use async_std::task;
 use clap::{Parser, Subcommand};
+use extendedmind_engine::{NameDescription, Peermerge, StateEvent};
+use futures::channel::mpsc::{unbounded, UnboundedReceiver, UnboundedSender};
+use futures::stream::StreamExt;
 use peermerge_tcp::connect_tcp_server_disk;
 use std::{path::PathBuf, process};
 
 use crate::{
     admin::execute_admin_command,
-    common::{BackupOpts, GlobalOpts},
+    common::{AdminCommand, BackupOpts, GlobalOpts},
     init::{initialize, InitializeResult},
 };
 
@@ -75,10 +79,11 @@ fn main() -> Result<()> {
             backup_opts,
         } => {
             // Initialize
-            let initialize_result = initialize(admin_socket_file, backup_opts, vec![data_root_dir]);
+            let initialize_result =
+                initialize(admin_socket_file, backup_opts, vec![data_root_dir.clone()]);
 
             // Block on server
-            futures::executor::block_on(start_server(initialize_result, tcp_port))?;
+            futures::executor::block_on(start_server(initialize_result, tcp_port, data_root_dir))?;
         }
         Command::Register { peermerge_doc_url } => {
             let result = futures::executor::block_on(execute_admin_command(
@@ -91,8 +96,56 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-async fn start_server(initialize_result: InitializeResult, tcp_port: u16) -> Result<()> {
-    async_std::task::sleep(std::time::Duration::from_secs(100)).await;
-    // tcp::listen(format!("0.0.0.0:{}", tcp_port), engine).await?;
+async fn start_server(
+    initialize_result: InitializeResult,
+    tcp_port: u16,
+    data_root_dir: PathBuf,
+) -> Result<()> {
+    // Create channels
+    let (mut peermerge_state_event_sender, mut peermerge_state_event_receiver): (
+        UnboundedSender<StateEvent>,
+        UnboundedReceiver<StateEvent>,
+    ) = unbounded();
+
+    // Create a peermerge
+    let peermerge = Peermerge::create_new_disk(NameDescription::new("hub"), &data_root_dir).await;
+
+    // Listen to admin commands
+    let mut admin_command_receiver = initialize_result.admin_command_receiver;
+    let admin_result_sender = initialize_result.admin_result_sender;
+    let mut peermerge_for_task = peermerge.clone();
+    task::spawn(async move {
+        while let Some(event) = admin_command_receiver.next().await {
+            match event {
+                AdminCommand::Register { peermerge_doc_url } => {
+                    peermerge_for_task
+                        .attach_proxy_document_disk(&peermerge_doc_url)
+                        .await;
+                    admin_result_sender.try_send(Ok(())).unwrap();
+                }
+                AdminCommand::BustCache { .. } => {
+                    // Nothing to do, just exit
+                    admin_result_sender.try_send(Ok(())).unwrap();
+                }
+            }
+        }
+    });
+
+    // Listen to peermerge state events, even if just to log them
+    task::spawn(async move {
+        while let Some(event) = peermerge_state_event_receiver.next().await {
+            log::debug!("Received peermerge event {:?}", event);
+        }
+    });
+
+    // Start server
+    connect_tcp_server_disk(
+        peermerge,
+        "0.0.0.0",
+        tcp_port,
+        &mut peermerge_state_event_sender,
+    )
+    .await
+    .unwrap();
     Ok(())
 }
