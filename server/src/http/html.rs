1fce0c84257385c74637a0e01a4303181d3d3280
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2023-03-07 14:35:49 +0200

    Support CSS inlining where the rel is after the href

--- server/src/http/html.rs
+++ server/src/http/html.rs
@@ -9,7 +9,8 @@ use tokio::io::AsyncReadExt;
 
 use crate::common::{is_inline_css, log_access, ServerState};
 
-const CSS_NEEDLE: &str = "<link rel=\"stylesheet\" href=\"/";
+const CSS_NEEDLE_1: &str = "<link rel=\"stylesheet\" href=\"/";
+const CSS_NEEDLE_2: &str = ".css\" rel=\"stylesheet\"";
 const CSS_ELEMENT_START: &str = "<style>";
 const CSS_ELEMENT_END: &str = "</style>";
 
@@ -85,36 +86,30 @@ impl ServerState {
         {
             log::debug!("Inlining CSS for path {}", url_path);
             let body_as_string = String::from_utf8(contents).unwrap();
-            // Locate styleheet links, only absolute paths are supported for now
-            let css_links: Vec<_> = body_as_string.match_indices(CSS_NEEDLE).collect();
-            let css_links_iter = css_links.iter();
+            let css_paths = css_paths(&body_as_string);
             let mut inlined_css_body: String = "".to_string();
             let mut last_processed_index = 0;
-            for val in css_links_iter {
-                let element_start_index = val.0;
+            for (css_path, element_start_index, element_end_index) in css_paths {
                 inlined_css_body += &body_as_string[last_processed_index..element_start_index];
-                let path_start_index = val.0 + CSS_NEEDLE.len() - 1;
-                let path_end_index =
-                    path_start_index + body_as_string[path_start_index..].find("\"").unwrap_or(0);
-                let element_end_index =
-                    path_end_index + body_as_string[path_end_index..].find(">").unwrap_or(0);
-
-                if path_end_index > 0 && element_end_index > 0 {
-                    let css_path = &body_as_string[path_start_index..path_end_index];
-                    log::debug!("Inlining CSS in path: {}", css_path);
-                    let css_file_path = self.get_file_path_from_url_path(css_path).await.unwrap();
-                    let mut file = File::open(css_file_path).await.unwrap();
-                    let mut contents: Vec<u8> = Vec::new();
-                    file.read_to_end(&mut contents).await.unwrap();
-                    let css_file_content =
-                        html_escape::encode_style(std::str::from_utf8(&contents).unwrap());
-                    inlined_css_body += CSS_ELEMENT_START;
-                    inlined_css_body += &css_file_content;
-                    inlined_css_body += CSS_ELEMENT_END;
+                let css_path = if css_path.starts_with("/") {
+                    css_path.to_string()
                 } else {
-                    // Something went wrong, just add the element like it is to the string
-                    inlined_css_body += &body_as_string[element_start_index..element_end_index];
-                }
+                    // Need to combine the url and path
+                    format!(
+                        "{}{}",
+                        url_path[..url_path.rfind('/').unwrap() + 1].to_string(),
+                        css_path
+                    )
+                };
+                let css_file_path = self.get_file_path_from_url_path(&css_path).await.unwrap();
+                let mut file = File::open(css_file_path).await.unwrap();
+                let mut contents: Vec<u8> = Vec::new();
+                file.read_to_end(&mut contents).await.unwrap();
+                let css_file_content =
+                    html_escape::encode_style(std::str::from_utf8(&contents).unwrap());
+                inlined_css_body += CSS_ELEMENT_START;
+                inlined_css_body += &css_file_content;
+                inlined_css_body += CSS_ELEMENT_END;
                 last_processed_index = element_end_index + 1;
             }
 
@@ -181,6 +176,46 @@ fn get_path_with_extension(path: &PathBuf, extension: &str) -> PathBuf {
     return path_with_extension;
 }
 
+fn css_paths(html: &str) -> Vec<(String, usize, usize)> {
+    let mut css_links: Vec<(usize, &str)> = html
+        .match_indices(CSS_NEEDLE_1)
+        .map(|val| (val.0, CSS_NEEDLE_1))
+        .collect();
+    let mut css_links_2 = html
+        .match_indices(CSS_NEEDLE_2)
+        .map(|val| (val.0, CSS_NEEDLE_2))
+        .collect();
+    css_links.append(&mut css_links_2);
+    css_links.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+    let css_links_iter = css_links.iter();
+    let mut css_paths: Vec<(String, usize, usize)> = vec![];
+    for val in css_links_iter {
+        let (path_start_index, path_end_index, element_start_index) = if val.1 == CSS_NEEDLE_1 {
+            let element_start_index = val.0;
+            let path_start_index = val.0 + CSS_NEEDLE_1.len() - 1;
+            let path_end_index =
+                path_start_index + html[path_start_index..].find("\"").unwrap_or(0);
+            (path_start_index, path_end_index, element_start_index)
+        } else if val.1 == CSS_NEEDLE_2 {
+            let path_end_index = val.0 + 4;
+            let path_start_index = html[..path_end_index].rfind("\"").unwrap_or(0) + 1;
+            let element_start_index = html[..path_start_index].rfind("<").unwrap_or(0);
+            (path_start_index, path_end_index, element_start_index)
+        } else {
+            unreachable!();
+        };
+        let element_end_index = path_end_index + html[path_end_index..].find(">").unwrap_or(0);
+        if path_end_index > 0 && element_end_index > 0 {
+            css_paths.push((
+                html[path_start_index..path_end_index].to_string(),
+                element_start_index,
+                element_end_index,
+            ));
+        }
+    }
+    css_paths
+}
+
 pub async fn handle_static_files(
     axum::extract::State(state): axum::extract::State<Arc<ServerState>>,
     request: Request<Body>,
@@ -229,5 +264,35 @@ pub async fn handle_static_files(
             .status(StatusCode::FORBIDDEN)
             .body(Full::default())
             .unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_css_paths() {
+        let html: &str = "<html>\n\
+             <body>\n\
+             </body>\n\
+             </html>";
+        let expected: Vec<(String, usize, usize)> = vec![];
+        assert_eq!(css_paths(html), expected);
+        let html: &str = "<html>\n\
+             <body>\n\
+             <link href=\"../_app/test1.css\" rel=\"stylesheet\">
+             <link rel=\"stylesheet\" href=\"/_app/test2.css\"/>
+             <link href=\"/_app/test3.css\" rel=\"stylesheet\"/>
+             <link rel=\"stylesheet\" href=\"/_app/test4.css\"/>
+             </body>\n\
+             </html>";
+        let expected: Vec<(String, usize, usize)> = vec![
+            ("../_app/test1.css".to_string(), 14, 61),
+            ("/_app/test2.css".to_string(), 76, 122),
+            ("/_app/test3.css".to_string(), 137, 183),
+            ("/_app/test4.css".to_string(), 198, 244),
+        ];
+        assert_eq!(css_paths(html), expected);
     }
 }
